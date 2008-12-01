bedafa82fcdc2db986889f28f6f36afcb87ec855
IamSempr <IamSempr@19ab8b60-bd6c-11dd-baed-41e496641fac>
2008-12-01 12:54:29 +0000

    it's ok
    git-svn-id: http://hustoj.googlecode.com/svn@5 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -456,6 +456,7 @@ int main(int argc, char** argv) {
 				if (cleft[reg.orig_eax]==0){
 					ACflg=OJ_RE;
 					syslog(LOG_ERR|LOG_DAEMON,"Error @ %s %d\n",argv[1],reg.orig_eax);
+					fprintf(stderr,"Error @ %s %d\n",argv[1],reg.orig_eax);
 					ptrace(PTRACE_KILL,pidApp,NULL,NULL);
 				}else{
 					if (sub==1) cleft[reg.orig_eax]--;

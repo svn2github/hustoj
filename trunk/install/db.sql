2ef7b75868d6ace7da282b7c4484e2637b9accd2
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2009-09-21 06:07:56 +0000

    added install scripts and README modified web admin interface to use HTMLFrame modified problem list to prevent non-authed user from accessing private contest problems after its finished
    first time on this project  zhblue(newsclan@gmail.com)
    
    git-svn-id: http://hustoj.googlecode.com/svn@11 19ab8b60-bd6c-11dd-baed-41e496641fac

---
+++ trunk/install/db.sql
@@ -0,0 +1,507 @@
+set names utf8; 
+create database jol;
+use jol;
+
+-- phpMyAdmin SQL Dump
+
+-- version 2.9.1.1-Debian-10
+
+-- http://www.phpmyadmin.net
+
+-- 
+
+-- Ö÷»ú: localhost
+
+-- Éú³ÉÈÕÆÚ: 2009 Äê 04 ÔÂ 24 ÈÕ 12:08
+
+-- ·þÎñÆ÷°æ±Ÿ: 5.0.32
+
+-- PHP °æ±Ÿ: 5.2.0-8+etch13
+
+-- 
+
+-- ÊýŸÝ¿â: `judge`
+
+-- 
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `compileinfo`
+
+-- 
+
+ 
+
+CREATE TABLE `compileinfo` (
+
+  `solution_id` int(11) NOT NULL default '0',
+
+  `error` text,
+
+  PRIMARY KEY  (`solution_id`)
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `contest`
+
+-- 
+
+ 
+
+CREATE TABLE `contest` (
+
+  `contest_id` int(11) NOT NULL auto_increment,
+
+  `title` varchar(255) default NULL,
+
+  `start_time` datetime default NULL,
+
+  `end_time` datetime default NULL,
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  `description` text,
+
+  `private` tinyint(4) NOT NULL default '0',
+
+  PRIMARY KEY  (`contest_id`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `contest_problem`
+
+-- 
+
+ 
+
+CREATE TABLE `contest_problem` (
+
+  `problem_id` int(11) NOT NULL default '0',
+
+  `contest_id` int(11) default NULL,
+
+  `title` char(200) NOT NULL default '',
+
+  `num` int(11) NOT NULL default '0'
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `loginlog`
+
+-- 
+
+ 
+
+CREATE TABLE `loginlog` (
+
+  `user_id` varchar(20) NOT NULL default '',
+
+  `password` varchar(40) default NULL,
+
+  `ip` varchar(100) default NULL,
+
+  `time` datetime default NULL
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `mail`
+
+-- 
+
+ 
+
+CREATE TABLE `mail` (
+
+  `mail_id` int(11) NOT NULL auto_increment,
+
+  `to_user` varchar(20) NOT NULL default '',
+
+  `from_user` varchar(20) NOT NULL default '',
+
+  `title` varchar(200) NOT NULL default '',
+
+  `content` text,
+
+  `new_mail` tinyint(1) NOT NULL default '1',
+
+  `reply` tinyint(4) default '0',
+
+  `in_date` datetime default NULL,
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  PRIMARY KEY  (`mail_id`),
+
+  KEY `uid` (`to_user`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `message`
+
+-- 
+
+ 
+
+CREATE TABLE `message` (
+
+  `message_id` int(11) NOT NULL auto_increment,
+
+  `problem_id` int(11) NOT NULL default '0',
+
+  `parent_id` int(11) NOT NULL default '0',
+
+  `thread_id` int(11) NOT NULL default '0',
+
+  `depth` int(11) NOT NULL default '0',
+
+  `orderNum` int(11) NOT NULL default '0',
+
+  `user_id` varchar(20) NOT NULL default '',
+
+  `title` varchar(200) NOT NULL default '',
+
+  `content` text,
+
+  `in_date` datetime default NULL,
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  PRIMARY KEY  (`message_id`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `news`
+
+-- 
+
+ 
+
+CREATE TABLE `news` (
+
+  `news_id` int(11) NOT NULL auto_increment,
+
+  `user_id` varchar(20) NOT NULL default '',
+
+  `title` varchar(200) NOT NULL default '',
+
+  `content` text NOT NULL,
+
+  `time` datetime NOT NULL default '0000-00-00 00:00:00',
+
+  `importance` tinyint(4) NOT NULL default '0',
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  PRIMARY KEY  (`news_id`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1004 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `online`
+
+-- 
+
+ 
+
+CREATE TABLE `online` (
+
+  `hash` varchar(32) collate utf8_unicode_ci NOT NULL,
+
+  `ip` varchar(20) character set utf8 NOT NULL default '',
+
+  `ua` varchar(255) character set utf8 NOT NULL default '',
+
+  `refer` varchar(255) collate utf8_unicode_ci default NULL,
+
+  `lastmove` int(10) NOT NULL,
+
+  `firsttime` int(10) default NULL,
+
+  `uri` varchar(255) collate utf8_unicode_ci default NULL,
+
+  PRIMARY KEY  (`hash`),
+
+  UNIQUE KEY `hash` (`hash`)
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `privilege`
+
+-- 
+
+ 
+
+CREATE TABLE `privilege` (
+
+  `user_id` char(20) NOT NULL default '',
+
+  `rightstr` char(30) NOT NULL default '',
+
+  `defunct` char(1) NOT NULL default 'N'
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `problem`
+
+-- 
+
+ 
+
+CREATE TABLE `problem` (
+
+  `problem_id` int(11) NOT NULL auto_increment,
+
+  `title` varchar(200) NOT NULL default '',
+
+  `description` text,
+
+  `input` text,
+
+  `output` text,
+
+  `sample_input` text,
+
+  `sample_output` text,
+
+  `spj` char(1) NOT NULL default '0',
+
+  `hint` text,
+
+  `source` varchar(100) default NULL,
+
+  `sample_Program` varchar(255) default NULL,
+
+  `in_date` datetime default NULL,
+
+  `time_limit` int(11) NOT NULL default '0',
+
+  `memory_limit` int(11) NOT NULL default '0',
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  `accepted` int(11) default '0',
+
+  `submit` int(11) default '0',
+
+  `ratio` tinyint(4) NOT NULL default '0',
+
+  `error` int(11) default '0',
+
+  `difficulty` tinyint(4) NOT NULL default '0',
+
+  `submit_user` int(11) default '0',
+
+  `solved` int(11) default '0',
+
+  `case_time_limit` int(11) NOT NULL default '0',
+
+  PRIMARY KEY  (`problem_id`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `solution`
+
+-- 
+
+ 
+
+CREATE TABLE `solution` (
+
+  `solution_id` int(11) NOT NULL auto_increment,
+
+  `problem_id` int(11) NOT NULL default '0',
+
+  `user_id` varchar(20) NOT NULL default '',
+
+  `time` int(11) NOT NULL default '0',
+
+  `memory` int(11) NOT NULL default '0',
+
+  `in_date` datetime NOT NULL default '0000-00-00 00:00:00',
+
+  `className` varchar(20) NOT NULL default '',
+
+  `result` smallint(6) NOT NULL default '0',
+
+  `language` tinyint(4) NOT NULL default '0',
+
+  `ip` varchar(20) NOT NULL default '',
+
+  `contest_id` int(11) default NULL,
+
+  `valid` tinyint(4) NOT NULL default '1',
+
+  `num` tinyint(4) NOT NULL default '-1',
+
+  `code_length` int(11) NOT NULL default '0',
+
+  `judgetime` datetime default NULL,
+
+  PRIMARY KEY  (`solution_id`),
+
+  KEY `uid` (`user_id`),
+
+  KEY `pid` (`problem_id`),
+
+  KEY `res` (`result`),
+
+  KEY `cid` (`contest_id`)
+
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22930 ;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `source_code`
+
+-- 
+
+ 
+
+CREATE TABLE `source_code` (
+
+  `solution_id` int(11) NOT NULL,
+
+  `source` text NOT NULL,
+
+  PRIMARY KEY  (`solution_id`)
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
+
+-- --------------------------------------------------------
+
+ 
+
+-- 
+
+-- ±íµÄœá¹¹ `users`
+
+-- 
+
+ 
+
+CREATE TABLE `users` (
+
+  `user_id` varchar(20) NOT NULL default '',
+
+  `email` varchar(100) default NULL,
+
+  `submit` int(11) default '0',
+
+  `solved` int(11) default '0',
+
+  `defunct` char(1) NOT NULL default 'N',
+
+  `ip` varchar(20) NOT NULL default '',
+
+  `accesstime` datetime default NULL,
+
+  `volume` int(11) NOT NULL default '1',
+
+  `language` int(11) NOT NULL default '1',
+
+  `password` varchar(32) default NULL,
+
+  `reg_time` datetime default NULL,
+
+  `nick` varchar(100) NOT NULL default '',
+
+  `school` varchar(100) NOT NULL default '',
+
+  PRIMARY KEY  (`user_id`)
+
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ 
---
+++ trunk/install/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#before install check DB setting in 
+#	judge.conf 
+#	hustoj-read-only/web/include/db_info.inc.php
+#	and down here
+#and run this with root
+
+WEBBASE=/var/www/
+DBUSER=root
+DBPASS=root
+
+svn checkout http://hustoj.googlecode.com/svn/trunk/ hustoj-read-only
+
+#create user and homedir
+sudo useradd -u 1536 judge
+
+#try install tools
+sudo apt-get install g++ libmysql++-dev
+sudo yum install g++ libmysql++-dev
+
+#compile and install the core
+cd hustoj-read-only/core/judged/
+sudo make
+sudo cp judged /usr/bin
+cd ../../../hustoj-read-only/core/judge_client
+sudo make
+sudo cp judge_client /usr/bin
+cd ../../../
+#install web and db
+sudo cp -R hustoj-read-only/web $WEBBASE/JudgeOnline
+sudo mysql -h localhost -u$DBUSER -p$DBPASS < db.sql
+
+#create work dir set default conf
+sudo    mkdir /home/judge/etc
+sudo    mkdir /home/judge/data
+sudo    mkdir /home/judge/log
+sudo    mkdir /home/judge/run0
+sudo cp java0.policy  judge.conf /home/judge/etc
+sudo chown -R judge /home/judge
+
+#boot up judged
+sudo echo "LANG=C /usr/bin/judged" > /etc/init.d/judged
+sudo chmod +x  /etc/init.d/judged
+sudo ln -s /etc/init.d/judged /etc/rc3.d/S93judged
+sudo /etc/init.d/judged

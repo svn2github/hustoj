faf66076672b7e76feea8b2a9c6645341480e9e5
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2009-09-27 03:29:48 +0000

    force to use utf-8 as db conn encode
    git-svn-id: http://hustoj.googlecode.com/svn@19 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -7,6 +7,7 @@
 	if(mysql_pconnect($DB_HOST,$DB_USER,$DB_PASS));
 	else die('Could not connect: ' . mysql_error());
 	// use db
+	mysql_query("set names utf8");
 	if(mysql_select_db($DB_NAME));
 	else die('Can\'t use foo : ' . mysql_error());
 	@session_start();

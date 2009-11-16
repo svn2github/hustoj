c9c06da53a8cd52d97a77c65ac21f817192aa521
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2009-11-16 12:50:42 +0000

    alert submiter of void main and input promopt
    git-svn-id: http://hustoj.googlecode.com/svn@39 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/submitpage.php
+++ trunk/web/submitpage.php
@@ -20,7 +20,33 @@ else{
 }
 ?>
 <center>
-<form action="submit.php" method="post">
+<script>
+	function checksource(src){
+		var keys=new Array();
+		var errs=new Array();
+		var msg="";
+		keys[0]="void main";
+		errs[0]="main函数返回值不能为void,否则会编译出错。\n很多人甚至市面上的一些书籍，都使用了void main( ) ，其实这是错误的。\nC/C++ 中从来没有定义过void main( ) 。\nC++ 之父 Bjarne Stroustrup 在他的主页上的 FAQ 中明确地写着\n The definition void main( ) { /* ... */ } is not and never has been C++,\n nor has it even been C.\n（ void main( ) 从来就不存在于 C++ 或者 C ）。";
+		keys[1]="Please";
+		errs[1]="除非题目要求，否则不要使用类似‘Please input’这样的提示，";		
+		keys[2]="请";
+		errs[2]="除非题目要求，否则不要使用类似‘请输入’这样的提示，";		
+		keys[3]="输入";
+		errs[3]="除非题目要求，否则不要使用类似‘请输入’这样的提示，";		
+		keys[3]="input";
+		errs[3]="除非题目要求，否则不要使用类似‘Please input’这样的提示，";		
+		for(var i=0;i<keys.length;i++){
+			if(src.indexOf(keys[i])!=-1){
+				msg+=errs[i]+"\n";
+			}
+		}
+		if(msg.length>0)
+			return confirm(msg+"\n 代码可能有错误，确定要提交么？");					
+		else
+			return true;
+}
+</script>
+<form action="submit.php" method="post"  onsubmit="return checksource(document.getElementById('source').value);">
 <?if (isset($id)){?>
 Problem <font color=blue><b><?=$id?></b></font><br>
 <input type='hidden' value='<?=$id?>' name="id">
@@ -41,7 +67,7 @@ Language:
 	<option value=3>Java</option>
 </select>
 <br>
-<textarea cols=80 rows=35 name="source"></textarea><br>
+<textarea cols=80 rows=35 id="source" name="source"></textarea><br>
 <input type=hidden name=testquote value='"'>
 <input type=submit value="Submit">
 <input type=reset value="Reset">

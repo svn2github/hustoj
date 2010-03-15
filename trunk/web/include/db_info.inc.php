b3e60939b02f7709d8b448c632696ab4ea4505e6
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-03-15 14:56:10 +0000

    use db config file for customer OJ Names etc
    git-svn-id: http://hustoj.googlecode.com/svn@75 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -1,14 +1,17 @@
 <?
-	$DB_HOST="";
-	$DB_NAME="";
-	$DB_USER="";
+	$DB_HOST="localhost";
+	$DB_NAME="jol";
+	$DB_USER="root";
 	$DB_PASS="";
 	// connect db
 	$OJ_NAME="Huazhong Univ. of Sci. & Tech";
+	$OJ_HOME="/";
+	$OJ_ADMIN="root@localhost"
 	if(mysql_pconnect($DB_HOST,$DB_USER,$DB_PASS));
 	else die('Could not connect: ' . mysql_error());
 	// use db
 	mysql_query("set names utf8");
+	mysql_set_charset("utf8");
 	if(mysql_select_db($DB_NAME));
 	else die('Can\'t use foo : ' . mysql_error());
 	@session_start();

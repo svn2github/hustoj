c793ea89168becc355c487307a21ea4e4617a21c
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-03-23 09:54:15 +0000

    fix problem with not  showing more than 1 page
    git-svn-id: http://hustoj.googlecode.com/svn@89 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/problemset.php
+++ trunk/web/problemset.php
@@ -46,10 +46,10 @@ if (!isset($_SESSION['administrator'])){
 	$sql="SELECT `problem_id`,`title`,`submit`,`accepted` FROM `problem` ".
 	"WHERE `defunct`='N' AND `problem_id` NOT IN(
 		SELECT `problem_id` FROM `contest_problem` WHERE `contest_id` IN (
-			SELECT `contest_id` FROM `contest` WHERE `end_time`>NOW() or private='1'
+			SELECT `contest_id` FROM `contest` WHERE `end_time`>NOW() or private=1
 		)
-	) or problem_id=1000";
-	$sql=$sql." AND `problem_id`>='".strval($pstart)."' AND `problem_id`<'".strval($pend)."' ";
+	)";
+	$sql=$sql." AND `problem_id`>='".strval($pstart)."' AND `problem_id`<'".strval($pend)."'  or problem_id=1000 ";
 }
 else{
 	$sql="SELECT `problem_id`,`title`,`submit`,`accepted` FROM `problem` ";

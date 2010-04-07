32e03396452e6c8b2d5963eaebfb7ccf1d7aa7dc
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-04-07 14:48:28 +0000

    Import funtion on the way
    git-svn-id: http://hustoj.googlecode.com/svn@110 19ab8b60-bd6c-11dd-baed-41e496641fac

---
+++ trunk/web/admin/problem_import.php
@@ -0,0 +1,8 @@
+<?require_once("admin-header.php");?>
+
+<form action='problem_import_xml.php' method=post enctype="multipart/form-data">
+	<b>Import Problem:</b><br />
+	
+	<input type=file name=fps >
+    <input type=submit value='Import'>
+</form>
---
+++ trunk/web/admin/problem_import_xml.php
@@ -0,0 +1,3 @@
+<?require_once("admin-header.php");?>
+
+Import Function is On the way.

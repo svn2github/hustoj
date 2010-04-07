19a0ce6692c5142f6da90f24a682314c864cc344
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-04-07 07:29:52 +0000

    this one can provide FPS(XML-based FreeProblemSet) output for other OJ to import.
    defect: test-data is not included
    
    git-svn-id: http://hustoj.googlecode.com/svn@106 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/problem_export.php
+++ trunk/web/admin/problem_export.php
@@ -1,19 +1,9 @@
 <?require_once("admin-header.php");?>
-<?
-if($_POST['do']=='do'){
-	$start=addslashes($_POST['start']);
-	$end=addslashes($_POST['end']);
-	$sql="update `solution` set `user_id`='$to' where `user_id`='$from' and problem_id>=$start and problem_id<=$end and result=4";
-	echo $sql;
-	//mysql_query($sql);
-	//echo mysql_affected_rows()." source file given!";
-	
-}
-?>
+
 <form action='problem_export_xml.php' method=post>
 	<b>Export Problem:</b><br />
-	start pid:<input type=text size=10 name="start"><br />
-	end pid:<input type=text size=10 name="end"><br />
+	start pid:<input type=text size=10 name="start" value=1000><br />
+	end pid:<input type=text size=10 name="end" value=1000><br />
 	<input type='hidden' name='do' value='do'>
 	<input type=submit value='Export'>
 </form>

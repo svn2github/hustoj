9d22b467d3e4e01a6c40c53d52175d80b6e69585
IamSempr <IamSempr@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-05-18 04:16:03 +0000

    Add disscuss from HOJ
    git-svn-id: http://hustoj.googlecode.com/svn@251 19ab8b60-bd6c-11dd-baed-41e496641fac

---
+++ trunk/web/include/discuss_func.inc.php
@@ -0,0 +1,28 @@
+<?php
+function problem_exist($pid,$cid){
+	require_once("./include/db_info.inc.php");
+	if ($pid=='') $pid=0;
+	if ($cid!='')
+		$cid="'".mysql_escape_string($cid)."'";
+	else
+		$cid='NULL';
+	if($pid!=0)
+		if($cid!='NULL')
+			$sql="SELECT 1 FROM `contest_problem` WHERE `contest_id` = $cid AND `problem_id` = '".mysql_escape_string($pid)."'";
+		else
+			$sql="SELECT 1 FROM `problem` WHERE `problem_id` = '".mysql_escape_string($pid)."'";
+	else if($cid!='NULL')
+		$sql="SELECT 1 FROM `contest` WHERE `contest_id` = $cid";
+	else
+		return true;
+	$sql.=" LIMIT 1";
+	$result=mysql_query($sql) or die(mysql_error());
+	return mysql_num_rows($result)>0;
+}
+function err_msg($msg){
+	require_once("oj-header.php");
+	echo $msg;
+	require_once("oj-footer.php");
+	exit(0);
+}
+?>

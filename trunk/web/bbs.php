72aa015198bdc4f088750c9383ccf9ae70b4e410
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-05-18 03:13:03 +0000

    add a simple link to phpbb
    git-svn-id: http://hustoj.googlecode.com/svn@250 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/bbs.php
+++ trunk/web/bbs.php
@@ -1,1 +1,9 @@
-<?header("Location:http://algorithm.byhh.net");?>
+<?
+$pid=(int)$_GET['id'];
+if($pid>0){
+$pid="p".$pid;
+    header("Location:bbs/search.php?fid[]=2&keywords=".$pid); //chenge this to your own phpBB search link
+}else{
+    header("Location:bbs/");
+}
+?>

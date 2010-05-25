efc9fa639561c049d1d9a4e651424008b3ed4461
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-05-25 03:16:24 +0000

    add a switch $OJ_BBS for user to choose which bbs module shoul work
    bbs=phpBB3 
    discuss=mini-forum like the one on POJ
    
    git-svn-id: http://hustoj.googlecode.com/svn@264 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/bbs.php
+++ trunk/web/bbs.php
@@ -1,12 +1,19 @@
 <?
-/* header("Location:discuss.php?id=".$_GET['id']);
+require_once("./include/db_info.inc.php");
+/* 
 */
 $pid=(int)$_GET['id'];
-if($pid>0){
-$pid="p".$pid;
-    header("Location:bbs/search.php?fid[]=2&keywords=".$pid); //chenge this to your own phpBB search link
-}else{
-    header("Location:bbs/");
-}
 
+if($OJ_BBS=="discuss"){
+   header("Location:discuss/discuss.php?id=".$pid);
+
+}else{
+
+	if($pid>0){
+	$pid="p".$pid;
+	    header("Location:bbs/search.php?fid[]=2&keywords=".$pid); //chenge this to your own phpBB search link
+	}else{
+	    header("Location:bbs/");
+	}
+}
 ?>
--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -8,11 +8,13 @@
 	$OJ_HOME="http://code.google.com/p/freeproblemset/";
 	$OJ_ADMIN="root@localhost";
 	$OJ_DATA="/home/judge/data/";
+	$OJ_BBS="bbs";//"bbs" for phpBB3 bridge or "discuss" for mini-forum
 	if(mysql_pconnect($DB_HOST,$DB_USER,$DB_PASS));
 	else die('Could not connect: ' . mysql_error());
 	// use db
 	mysql_query("set names utf8");
 	mysql_set_charset("utf8");
+	
 	if(mysql_select_db($DB_NAME));
 	else die('Can\'t use foo : ' . mysql_error());
 	@session_start();

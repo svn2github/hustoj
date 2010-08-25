8b0204f337d6992d89eb5fe79e7aa62d69aaff76
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-08-25 09:59:56 +0000

    let hustoj back2thefuture
    git-svn-id: http://hustoj.googlecode.com/svn@315 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/oj-footer.php
+++ trunk/web/oj-footer.php
@@ -5,7 +5,7 @@
 <center>
 	<p>
 		All Copyright Reserved 2008-2010 <a href='<?=$OJ_HOME?>'><?=$OJ_NAME?></a> TEAM<br>
-		GPL Source 2003-2009 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
+		GPL Source 2003-2010 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?=$OJ_ADMIN?>">admin</a><br>
 
 	</p>

85026f05f98e5ed93011e0e8ba8cd2c649759cc1
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-09-05 15:34:00 +0000

    promopt solution when import error happens for authority
    git-svn-id: http://hustoj.googlecode.com/svn@345 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/problem.php
+++ trunk/web/include/problem.php
@@ -43,9 +43,14 @@ function mkdata($pid,$filename,$input,$OJ_DATA){
 	
 	$basedir = "$OJ_DATA/$pid";
 	
-	$fp = fopen ( $basedir . "/$filename", "w" );
-	fputs ( $fp, preg_replace ( "(\r\n)", "\n", $input ) );
-	fclose ( $fp );
+	$fp = @fopen ( $basedir . "/$filename", "w" );
+	if($fp){
+		fputs ( $fp, preg_replace ( "(\r\n)", "\n", $input ) );
+		fclose ( $fp );
+	}else{
+		echo "Error while opening".$basedir . "/$filename ,try [chgrp -R www-data $OJ_DATA] and [chmod -R 771 $OJ_DATA ] "
+		
+	}
 	
 	
 	

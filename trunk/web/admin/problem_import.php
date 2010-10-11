f899c68e19467f04dbf2bc42e4e342d35d30180e
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-10-11 13:58:46 +0000

    promopt max filesize and open_basedir settings
    git-svn-id: http://hustoj.googlecode.com/svn@424 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/problem_import.php
+++ trunk/web/admin/problem_import.php
@@ -2,8 +2,17 @@
 if (!(isset($_SESSION['administrator']))){
 	echo "<a href='../loginpage.php'>Please Login First!</a>";
 	exit(1);
-}?>
-Import FPS data ,please make sure you file is smaller than the [upload_max_filesize] in PHP.ini
+}
+   $maxfile=min(ini_get("upload_max_filesize"),ini_get("post_max_size"));
+
+?>
+Import FPS data ,please make sure you file is smaller than [<?=$maxfile?>] <br/>
+or set upload_max_filesize and post_max_size in PHP.ini
+
+<?php 
+   if(strstr(ini_get("open_basedir"),$OJ_DATA)){
+
+?>
 <br>
 <form action='problem_import_xml.php' method=post enctype="multipart/form-data">
 	<b>Import Problem:</b><br />
@@ -11,6 +20,12 @@ Import FPS data ,please make sure you file is smaller than the [upload_max_files
 	<input type=file name=fps >
     <input type=submit value='Import'>
 </form>
-
+<?php 
+   }else{
+   	 echo $OJ_DATA." is not in your open_basedir setting of php.ini, you can't use import function"; 
+   	}
+   
+?>
+<br>
 
 free problem set FPS-xml can be download at <a href=http://code.google.com/p/freeproblemset/downloads/list>FPS-Googlecode</a>

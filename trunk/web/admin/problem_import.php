2b22ec8fefae8bcc84d4ff3e5b3015d70e01428b
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-10-13 05:40:59 +0000

    more pre-import check and promopt
    git-svn-id: http://hustoj.googlecode.com/svn@426 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/problem_import.php
+++ trunk/web/admin/problem_import.php
@@ -1,4 +1,13 @@
-<?require_once("admin-header.php");
+<?
+function writable($path){
+	$ret=false;
+	$fp=fopen($path."/testifwritable.tst","w");
+	$ret=!($fp===false);
+	fclose($fp);
+	unlink($path."/testifwritable.tst");
+	return $ret;
+}
+require_once("admin-header.php");
 if (!(isset($_SESSION['administrator']))){
 	echo "<a href='../loginpage.php'>Please Login First!</a>";
 	exit(1);
@@ -7,11 +16,14 @@ if (!(isset($_SESSION['administrator']))){
 
 ?>
 Import FPS data ,please make sure you file is smaller than [<?=$maxfile?>] <br/>
-or set upload_max_filesize and post_max_size in PHP.ini
-
+or set upload_max_filesize and post_max_size in PHP.ini<br/>
+if you fail on import big files[10M+],try enlarge your [memory_limit]  setting in php.ini.<br>
 <?php 
-   if(strstr(ini_get("open_basedir"),$OJ_DATA)){
-
+   if(!strstr(ini_get("open_basedir"),$OJ_DATA)){
+ 	   echo $OJ_DATA." is not in your open_basedir setting of php.ini, you can't use import function.<br>"; 
+   }else if(!writable("../upload")){
+       echo "../upload is not writable, chmod 770 to it.<br>";
+   }else{
 ?>
 <br>
 <form action='problem_import_xml.php' method=post enctype="multipart/form-data">
@@ -21,9 +33,8 @@ or set upload_max_filesize and post_max_size in PHP.ini
     <input type=submit value='Import'>
 </form>
 <?php 
-   }else{
-   	 echo $OJ_DATA." is not in your open_basedir setting of php.ini, you can't use import function"; 
-   	}
+  
+   		}
    
 ?>
 <br>

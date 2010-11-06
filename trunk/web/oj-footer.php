c3518c91fa161f5628524c0d00de105dd5d87943
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-11-06 11:38:28 +0000

    add gpl-2.0 lisence as asked
    git-svn-id: http://hustoj.googlecode.com/svn@499 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/oj-footer.php
+++ trunk/web/oj-footer.php
@@ -7,7 +7,7 @@
 <center>
 	<p>
 		All Copyright Reserved 2008-2010 <a href='<?=$OJ_HOME?>'><?=$OJ_NAME?></a> TEAM<br>
-		<span class=green>GPL Source</span> 2003-2010 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2010 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?=$OJ_ADMIN?>">admin</a><br>
 		
 	</p>

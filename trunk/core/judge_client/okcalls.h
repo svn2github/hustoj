9b9407ced1ca33a4880b57403ed4cb001d9e45dd
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-11-10 01:13:01 +0000

    add GPL declare in header
    git-svn-id: http://hustoj.googlecode.com/svn@505 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/okcalls.h
+++ trunk/core/judge_client/okcalls.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2008 sempr <iamsempr@gmail.com>
+ *
+ * This file is part of HUSTOJ.
+ *
+ * HUSTOJ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HUSTOJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HUSTOJ. if not, see <http://www.gnu.org/licenses/>.
+ */
 #include <sys/syscall.h>
 
 int LANG_JV[256]={SYS_fcntl64,SYS_getdents64 , SYS_ugetrlimit, SYS_rt_sigprocmask, SYS_futex, SYS_read, SYS_mmap2, SYS_stat64, SYS_open, SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_readlink, SYS_munmap, SYS_close, SYS_uname, SYS_clone, SYS_uname, SYS_mprotect, SYS_rt_sigaction, SYS_sigprocmask, SYS_getrlimit, SYS_fstat64, SYS_getuid32, SYS_getgid32, SYS_geteuid32, SYS_getegid32, SYS_set_thread_area, SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 0};

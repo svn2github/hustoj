cc1dad3744b2da46369da6838ec751d039ada35e
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2010-11-21 15:03:54 +0000

    rejudge retest sim
    git-svn-id: http://hustoj.googlecode.com/svn@549 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/rejudge.php
+++ trunk/web/admin/rejudge.php
@@ -8,12 +8,16 @@ if(isset($_POST['do'])){
 	if (isset($_POST['rjpid'])){
 		$sql="UPDATE `solution` SET `result`=1 WHERE `problem_id`=".$_POST['rjpid'];
 		mysql_query($sql) or die(mysql_error());
+		$sql="delete from `sim` WHERE `s_id` in select solution_id from solution where `problem_id`=".$_POST['rjpid'];
+		mysql_query($sql) or die(mysql_error());
 		$url="../status.php?problem_id=".$_POST['rjpid'];
 		echo "Rejudged Problem ".$_POST['rjpid'];
 		echo "<script>location.href='$url';</script>";
 	}
 	else if (isset($_POST['rjsid'])){
 		$sql="UPDATE `solution` SET `result`=1 WHERE `solution_id`=".$_POST['rjsid'];
+		mysql_query($sql) or die(mysql_error());
+		$sql="delete from `sim` WHERE `s_id`=".$_POST['rjpid'];
 		mysql_query($sql) or die(mysql_error());
 		$url="../status.php?top=".(intval($_POST['rjsid'])+1);
 		echo "Rejudged Runid ".$_POST['rjsid'];

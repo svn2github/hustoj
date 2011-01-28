1ab02cca02d0ded6ac3dcf587c1ea4b902a6f427
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-01-28 04:56:20 +0000

    happy new year 2011
    git-svn-id: http://hustoj.googlecode.com/svn@736 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/oj-footer.php
+++ trunk/web/oj-footer.php
@@ -14,8 +14,8 @@
 		<a href=setlang.php?lang=th>ไทย</a>
 	<?}?>
 		<br>
-		All Copyright Reserved 2008-2010 <a href='<?=$OJ_HOME?>'><?=$OJ_NAME?></a> TEAM<br>
-		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2010 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
+		All Copyright Reserved 2010-2011 <a href='<?=$OJ_HOME?>'><?=$OJ_NAME?></a> TEAM<br>
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2011 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?=$OJ_ADMIN?>">admin</a><br>
 		
 	</p>

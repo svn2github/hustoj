898123234c692c447406daef53680006c2ab74d8
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-02-08 05:53:14 +0000

    add python run command in run_solution()
    git-svn-id: http://hustoj.googlecode.com/svn@755 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -778,12 +778,12 @@ void run_solution(int & lang, char * work_dir, int & time_lmt, int & usedtime,
 		//execl("./ruby", "Main.rb", NULL);
 		break;
 	case 5: //bash
-
 		system("/Main.sh<data.in");
+		break;
 	}
 	case 6: //Python
-
 		system("/python Main.py<data.in");
+		break;
 	}
 	//sleep(1);
 	exit(0);

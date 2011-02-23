900f84d50137c19b3dea5fabcbd9bb5e311fd371
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-02-23 08:14:24 +0000

    add header to non-login
    git-svn-id: http://hustoj.googlecode.com/svn@783 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/submitpage.php
+++ trunk/web/submitpage.php
@@ -2,6 +2,7 @@
 <title>Submit Code</title>
 <?
 if (!isset($_SESSION['user_id'])){
+	require_once("oj-header.php");
 	echo "<a href=loginpage.php>Please Login First</a>";
 	require_once("oj-footer.php");
 	exit(0);

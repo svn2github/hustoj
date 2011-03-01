c8b0e0166852d1bb8391a0cd5304454adc41e570
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-03-01 01:59:32 +0000

    iconv -f gbk -t utf8
    git-svn-id: http://hustoj.googlecode.com/svn@792 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/faqs.cn.php
+++ trunk/web/faqs.cn.php
@@ -7,8 +7,8 @@
   <font size="+3"><?=$OJ_NAME?> Online Judge FAQ</font>
 </center>
 <hr>
-<p><font color=green>Q</font>:������߲���ϵͳʹ��ʲô���ı������ͱ���ѡ��?<br>
-  <font color=red>A</font>:ϵͳ������<a href="http://www.debian.org/">Debian</a>/<a href="http://www.ubuntu.com">Ubuntu</a> Linux. ʹ��<a href="http://gcc.gnu.org/">GNU GCC/G++</a> ��ΪC/C++������, <a href="http://www.freepascal.org">Free Pascal</a> ��Ϊpascal ������ ���� <a href="http://www.oracle.com/technetwork/java/index.html">sun-java-jdk1.6</a> ���� Java. ��Ӧ�ı���ѡ������:<br>
+<p><font color=green>Q</font>:这个在线裁判系统使用什么样的编译器和编译选项?<br>
+  <font color=red>A</font>:系统运行于<a href="http://www.debian.org/">Debian</a>/<a href="http://www.ubuntu.com">Ubuntu</a> Linux. 使用<a href="http://gcc.gnu.org/">GNU GCC/G++</a> 作为C/C++编译器, <a href="http://www.freepascal.org">Free Pascal</a> 作为pascal 编译器 ，用 <a href="http://www.oracle.com/technetwork/java/index.html">sun-java-jdk1.6</a> 编译 Java. 对应的编译选项如下:<br>
 </p>
 <table border="1">
   <tr>
@@ -31,18 +31,18 @@
     </td>
   </tr>
 </table>
-<p>  �������汾Ϊ��ϵͳ���������������汾������ֱ���ο���:<br>
+<p>  编译器版本为（系统可能升级编译器版本，这里直供参考）:<br>
   <font color=blue>gcc (Ubuntu/Linaro 4.4.4-14ubuntu5) 4.4.5</font><br>
   <font color=blue>glibc 2.3.6</font><br>
 <font color=blue>Free Pascal Compiler version 2.4.0-2 [2010/03/06] for i386<br>
 java version "1.6.0_22"<br>
 </font></p>
 <hr>
-<p><font color=green>Q</font>:��������ȡ�����롢�������?<br>
-  <font color=red>A</font>:��ĳ���Ӧ�ôӱ�׼���� stdin('Standard Input')��ȡ��� ��������������׼��� stdout('Standard Output').����,��C���Կ���ʹ�� 'scanf' ����C++����ʹ��'cin' �������룻��Cʹ�� 'printf' ����C++ʹ��'cout'�������.</p>
-<p>�û���������ֱ�Ӷ�д�ļ�, ������������ܻ���Ϊ����ʱ���� "<font color=green>Runtime Error</font>"��<br>
+<p><font color=green>Q</font>:程序怎样取得输入、进行输出?<br>
+  <font color=red>A</font>:你的程序应该从标准输入 stdin('Standard Input')获取输出 并将结果输出到标准输出 stdout('Standard Output').例如,在C语言可以使用 'scanf' ，在C++可以使用'cin' 进行输入；在C使用 'printf' ，在C++使用'cout'进行输出.</p>
+<p>用户程序不允许直接读写文件, 如果这样做可能会判为运行时错误 "<font color=green>Runtime Error</font>"。<br>
   <br>
-������ 1000��Ĳο���</p>
+下面是 1000题的参考答案</p>
 <p> C++:<br>
 </p>
 <pre><font color="blue">
@@ -95,47 +95,47 @@ public class Main{
 }</font></pre>
 
 <hr>
-<font color=green>Q</font>:Ϊʲô�ҵĳ������Լ��ĵ������������룬��ϵͳ�����ұ������!<br>
-<font color=red>A</font>:GCC�ı����׼��VC6��Щ��ͬ�����ӷ���c/c++��׼:<br>
+<font color=green>Q</font>:为什么我的程序在自己的电脑上正常编译，而系统告诉我编译错误!<br>
+<font color=red>A</font>:GCC的编译标准与VC6有些不同，更加符合c/c++标准:<br>
 <ul>
-  <li><font color=blue>main</font> �������뷵��<font color=blue>int</font>, <font color=blue>void main</font> �ĺ��������ᱨ�������<br> 
-  <li><font color=green>i</font> ��ѭ����ʧȥ���� "<font color=blue>for</font>(<font color=blue>int</font> <font color=green>i</font>=0...){...}"<br>
-  <li><font color=green>itoa</font> ����ansi��׼����.<br>
-  <li><font color=green>__int64</font> ����ANSI��׼���壬ֻ����VCʹ��, ���ǿ���ʹ��<font color=blue>long long</font>����64λ������<br>
+  <li><font color=blue>main</font> 函数必须返回<font color=blue>int</font>, <font color=blue>void main</font> 的函数声明会报编译错误。<br> 
+  <li><font color=green>i</font> 在循环外失去定义 "<font color=blue>for</font>(<font color=blue>int</font> <font color=green>i</font>=0...){...}"<br>
+  <li><font color=green>itoa</font> 不是ansi标准函数.<br>
+  <li><font color=green>__int64</font> 不是ANSI标准定义，只能在VC使用, 但是可以使用<font color=blue>long long</font>声明64位整数。<br>
 </ul>
 <hr>
-<font color=green>Q</font>:ϵͳ������Ϣ����ʲô��˼?<br>
-<font color=red>A</font>:�������:<br>
-<p><font color=blue>Pending</font> : ϵͳæ����Ĵ����Ŷӵȴ�. </p>
-<p><font color=blue>Pending Rejudge</font>: ��Ϊ���ݸ��»�����ԭ��ϵͳ����������Ĵ�.</p>
-<p><font color=blue>Compiling</font> : ���ڱ���.<br>
+<font color=green>Q</font>:系统返回信息都是什么意思?<br>
+<font color=red>A</font>:详见下述:<br>
+<p><font color=blue>Pending</font> : 系统忙，你的答案在排队等待. </p>
+<p><font color=blue>Pending Rejudge</font>: 因为数据更新或其他原因，系统将重新判你的答案.</p>
+<p><font color=blue>Compiling</font> : 正在编译.<br>
 </p>
-<p><font color="blue">Running &amp; Judging</font>: �������к��ж�.<br>
+<p><font color="blue">Running &amp; Judging</font>: 正在运行和判断.<br>
 </p>
-<p><font color=blue>Accepted</font> : ����ͨ��!<br>
+<p><font color=blue>Accepted</font> : 程序通过!<br>
   <br>
-  <font color=blue>Presentation Error</font> : �𰸻�����ȷ�����Ǹ�ʽ���ԡ�<br>
+  <font color=blue>Presentation Error</font> : 答案基本正确，但是格式不对。<br>
   <br>
-  <font color=blue>Wrong Answer</font> : �𰸲��ԣ�����ͨ���������ݵĲ��Բ���һ������ȷ�𰸣�һ��������û�뵽�ĵط�.<br>
+  <font color=blue>Wrong Answer</font> : 答案不对，仅仅通过样例数据的测试并不一定是正确答案，一定还有你没想到的地方.<br>
   <br>
-  <font color=blue>Time Limit Exceeded</font> : ���г���ʱ�����ƣ�������Ƿ�����ѭ��������Ӧ���и���ļ��㷽����<br>
+  <font color=blue>Time Limit Exceeded</font> : 运行超出时间限制，检查下是否有死循环，或者应该有更快的计算方法。<br>
   <br>
-  <font color=blue>Memory Limit Exceeded</font> : �����ڴ����ƣ����ݿ�����Ҫѹ��������ڴ��Ƿ���й¶��<br>
+  <font color=blue>Memory Limit Exceeded</font> : 超出内存限制，数据可能需要压缩，检查内存是否有泄露。<br>
   <br>
-  <font color=blue>Output Limit Exceeded</font>: ����������ƣ�����������ȷ�𰸳�������.<br>
+  <font color=blue>Output Limit Exceeded</font>: 输出超过限制，你的输出比正确答案长了两倍.<br>
   <br>
-  <font color=blue>Runtime Error</font> : ����ʱ���󣬷Ƿ����ڴ���ʣ�����Խ�磬ָ��Ư�ƣ����ý��õ�ϵͳ������<br>
+  <font color=blue>Runtime Error</font> : 运行时错误，非法的内存访问，数组越界，指针漂移，调用禁用的系统函数。<br>
 </p>
-<p>  <font color=blue>Compile Error</font> : ��������������ñ���������ϸ�����<br>
+<p>  <font color=blue>Compile Error</font> : 编译错误，请点击后获得编译器的详细输出。<br>
   <br>
 </p>
 <hr>
-<font color=green>Q</font>:��βμ����߱���?<br>
-<font color=red>A</font>:<a href=registerpage.php>ע��</a> һ���ʺţ�Ȼ��Ϳ�����ϰ����������б�Contests���Կ������ڽ��еı������μӡ�<br>
+<font color=green>Q</font>:如何参加在线比赛?<br>
+<font color=red>A</font>:<a href=registerpage.php>注册</a> 一个帐号，然后就可以练习，点击比赛列表Contests可以看到正在进行的比赛并参加。<br>
 <br>
 <hr>
 <center>
-  <font color=green size="+2">�������������<a href="bbs.php"><?=$OJ_NAME?>��̳ϵͳ</a></font>
+  <font color=green size="+2">其他问题请访问<a href="bbs.php"><?=$OJ_NAME?>论坛系统</a></font>
 </center>
 <hr>
 <center>

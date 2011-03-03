ce0b23f4a96edd0a58df705a585b4c9a9e3d8f11
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-03-03 12:45:38 +0000

    fix
    http://hi.baidu.com/kashifs/blog/item/e47fc4195b2578eeaf5133be.html
    
    git-svn-id: http://hustoj.googlecode.com/svn@800 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/setlang.php
+++ trunk/web/setlang.php
@@ -1,6 +1,10 @@
 <?
 	require_once("./include/db_info.inc.php");
-	$_SESSION['OJ_LANG']=$_GET['lang'];
+	require_once("./include/my_func.inc.php");
+	$newlang=strval($_GET['lang']);
+	if(is_valid_user_name($newlang)){
+		$_SESSION['OJ_LANG']=$newlang;
+	}
 		echo "<script language='javascript'>\n";
 		echo "history.go(-1);\n";
 		echo "</script>";

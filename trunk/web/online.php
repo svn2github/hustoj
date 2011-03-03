a10a7922d7fa7844af79c247895ebc184780346f
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-03-03 13:06:17 +0000

    add from xiao@sdibtacm
    git-svn-id: http://hustoj.googlecode.com/svn@801 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/online.php
+++ trunk/web/online.php
@@ -30,4 +30,42 @@
 endforeach;?>
 </tbody>
 </table>
+
+
+<?
+if (isset($_SESSION['administrator'])){
+
+echo "<center>";
+echo "<td width='100%' colspan='5'><form>IP<input type='text' name='search'><input type='submit' value='$MSG_SEARCH' ></form></td></tr>";
+if(isset($_GET['search'])){
+
+    $sql="SELECT * FROM `loginlog`";
+    $search=trim(mysql_real_escape_string($_GET['search']));
+    if ($search!='')
+    	$sql=$sql." WHERE ip like '%$search%' ";
+     else
+        $sql=$sql." where user_id<>'".$_SESSION['user_id']."' ";
+    $sql=$sql."  order by `time` desc LIMIT 0,50";
+
+$result=mysql_query($sql) or die(mysql_error());
+echo "<table border=1>";
+echo "<tr align=center><td>UserID<td>Password<td>IP<td>Time</tr>";
+for (;$row=mysql_fetch_row($result);){
+        echo "<tr align=center>";
+        echo "<td><a href='userinfo.php?user=".$row[0]."'>".$row[0]."</a>";
+        echo "<td>".$row[1];
+        echo "<td>".$row[2];
+        echo "<td>".$row[3];
+        echo "</tr>";
+}
+echo "</table>";
+
+echo "</center>";
+mysql_free_result($result);
+}
+
+}
+?>
+
+
 <?php require_once('oj-footer.php')?>

7238872f5e017dbd7a23107d27c4c2f89500b242
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-04-06 14:59:25 +0000

    add AUTO_SHARE
    git-svn-id: http://hustoj.googlecode.com/svn@864 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -16,6 +16,7 @@ static  $OJ_SIM=true;
 static  $OJ_DICT=true;
 static  $OJ_LANGMASK=0; //1mC 2mCPP 4mPascal 8mJava 16mRuby 32mBash 
 static  $OJ_EDITE_AREA=true;//true: syntax highlighting is active
+static  $OJ_AUTO_SHARE=false;//true: One can view all AC submit if he/she has ACed it onece.
 if (isset($_SESSION['OJ_LANG'])) $OJ_LANG=$_SESSION['OJ_LANG'];
 	if(mysql_pconnect($DB_HOST,$DB_USER,$DB_PASS));
 	else die('Could not connect: ' . mysql_error());

abfe6865c001254ce3fb6c3a6e0889b19e0f3868
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-04-13 14:14:08 +0000

    rank scope
    git-svn-id: http://hustoj.googlecode.com/svn@943 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/ranklist.php
+++ trunk/web/ranklist.php
@@ -56,8 +56,8 @@
 					right join 
 					(select count(distinct problem_id) solved ,user_id from solution where in_date>now()-'$s' and result=4 group by user_id order by solved desc limit " . strval ( $rank ) . ",$page_size) s on users.user_id=s.user_id
 					right join 
-					(select count(distinct problem_id) sumbit ,user_id from solution where in_date>now()-'$s' group by user_id order by submit desc limit " . strval ( $rank ) . ",".($page_size*2).") t on users.user_id=t.user_id
-				ORDER BY s.`solved` DESC,submit,reg_time  LIMIT  0,50
+					(select count(distinct problem_id) submit ,user_id from solution where in_date>now()-'$s' group by user_id order by submit desc limit " . strval ( $rank ) . ",".($page_size*2).") t on users.user_id=t.user_id
+				ORDER BY s.`solved` DESC,t.submit,reg_time  LIMIT  0,50
 			 ";
 			 echo $sql;
 		}

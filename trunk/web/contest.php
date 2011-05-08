52a6b9dc248350958bb73bfa09f17294552e281a
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-05-08 00:19:27 +0000

    ie9 39xx reported by xiao
    git-svn-id: http://hustoj.googlecode.com/svn@1005 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/contest.php
+++ trunk/web/contest.php
@@ -119,6 +119,7 @@ function clock()
       var x,h,m,s,n,xingqi,y,mon,d;
       var x = new Date(new Date().getTime()+diff);
       y = x.getYear()+1900;
+      if (y>3000) y-=1900;
       mon = x.getMonth()+1;
       d = x.getDate();
       xingqi = x.getDay();

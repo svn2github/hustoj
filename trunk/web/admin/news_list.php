24d156e24cc3db02931e65ef4a13c6f112da86e3
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-05-09 15:21:08 +0000

    optimize promopt to avoid confusion
    git-svn-id: http://hustoj.googlecode.com/svn@1007 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/news_list.php
+++ trunk/web/admin/news_list.php
@@ -11,14 +11,14 @@ $sql="select `news_id`,`user_id`,`title`,`time`,`defunct` FROM `news` order by `
 $result=mysql_query($sql) or die(mysql_error());
 echo "<center><table width=90% border=1>";
 
-echo "<tr><td>PID<td>Title<td>Date<td>Defunct<td>Edit</tr>";
+echo "<tr><td>PID<td>Title<td>Date<td>Status<td>Edit</tr>";
 for (;$row=mysql_fetch_object($result);){
 	echo "<tr>";
 	echo "<td>".$row->news_id;
 	//echo "<input type=checkbox name='pid[]' value='$row->problem_id'>";
 	echo "<td><a href='news_edit.php?id=$row->news_id'>".$row->title."</a>";
 	echo "<td>".$row->time;
-	echo "<td><a href=news_df_change.php?id=$row->news_id&getkey=".$_SESSION['getkey'].">".($row->defunct=="N"?"Delete":"Resume")."</a>";
+	echo "<td><a href=news_df_change.php?id=$row->news_id&getkey=".$_SESSION['getkey'].">".($row->defunct=="N"?"<span class=green>Available</span>":"<span class=red>Reserved</span>")."</a>";
 		echo "<td><a href=news_edit.php?id=$row->news_id>Edit</a>";
 	
 	echo "</tr>";

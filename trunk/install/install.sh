798de90e0383ffef78c9894c7539bff04f23ed16
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-05-18 23:28:08 +0000

    miss /
    git-svn-id: http://hustoj.googlecode.com/svn@1017 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/install/install.sh
+++ trunk/install/install.sh
@@ -46,7 +46,7 @@ sudo chgrp -R root /home/judge/etc /home/judge/run?
 sudo chmod 771 /home/judge/data /home/judge/etc /home/judge/run?
 
 #boot up judged
-sudo cp judged etc/init.d/judged
+sudo cp judged /etc/init.d/judged
 sudo chmod +x  /etc/init.d/judged
 sudo ln -s /etc/init.d/judged /etc/rc3.d/S93judged
 sudo ln -s /etc/init.d/judged /etc/rc2.d/S93judged

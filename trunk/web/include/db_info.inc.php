b9b3c345ba043e2d10ed117a30bf01ec210a1a24
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-05-22 14:04:58 +0000

    modify note for sae.sina.com.cn
    git-svn-id: http://hustoj.googlecode.com/svn@1035 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -19,8 +19,14 @@ static  $OJ_EDITE_AREA=true;//true: syntax highlighting is active
 static  $OJ_AUTO_SHARE=false;//true: One can view all AC submit if he/she has ACed it onece.
 static  $OJ_CSS="hoj.css";
 if (isset($_SESSION['OJ_LANG'])) $OJ_LANG=$_SESSION['OJ_LANG'];
+//for normal install
 	if(mysql_pconnect($DB_HOST,$DB_USER,$DB_PASS));
 	else die('Could not connect: ' . mysql_error());
+	
+// for sae.sina.com.cn
+// mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+// $DB_NAME=SAE_MYSQL_DB;
+
 	// use db
 	mysql_query("set names utf8");
 	mysql_set_charset("utf8");

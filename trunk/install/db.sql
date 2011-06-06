0f341e6486d50474dd55a8999e67589bc1d88ab9
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-06-06 12:29:03 +0000

    latest database
    git-svn-id: http://hustoj.googlecode.com/svn@1060 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/install/db.sql
+++ trunk/install/db.sql
@@ -3,283 +3,170 @@ create database jol;
 use jol;
 
 
- 
-
-CREATE TABLE `compileinfo` (
-
-  `solution_id` int(11) NOT NULL default '0',
-
+CREATE TABLE  `jol`.`compileinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
   `error` text,
-
-  PRIMARY KEY  (`solution_id`)
-
+  PRIMARY KEY (`solution_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
- 
-
-CREATE TABLE `contest` (
-
-  `contest_id` int(11) NOT NULL auto_increment,
-
-  `title` varchar(255) default NULL,
-
-  `start_time` datetime default NULL,
-
-  `end_time` datetime default NULL,
-
-  `defunct` char(1) NOT NULL default 'N',
-
+CREATE TABLE  `jol`.`contest` (
+  `contest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
   `description` text,
+  `private` tinyint(4) NOT NULL DEFAULT '0',
+  `langmask` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'bits for LANG to mask',
+  PRIMARY KEY (`contest_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
-  `private` tinyint(4) NOT NULL default '0',
-
-  PRIMARY KEY  (`contest_id`)
-
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
-
- 
-
- 
-
-CREATE TABLE `contest_problem` (
-
-  `problem_id` int(11) NOT NULL default '0',
-
-  `contest_id` int(11) default NULL,
-
-  `title` char(200) NOT NULL default '',
-
-  `num` int(11) NOT NULL default '0'
-
+CREATE TABLE  `jol`.`contest_problem` (
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `contest_id` int(11) DEFAULT NULL,
+  `title` char(200) NOT NULL DEFAULT '',
+  `num` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
- 
 
-CREATE TABLE `loginlog` (
-
-  `user_id` varchar(20) NOT NULL default '',
-
-  `password` varchar(40) default NULL,
-
-  `ip` varchar(100) default NULL,
-
-  `time` datetime default NULL
-
+CREATE TABLE  `jol`.`loginlog` (
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(40) DEFAULT NULL,
+  `ip` varchar(100) DEFAULT NULL,
+  `time` datetime DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
- 
+CREATE TABLE  `jol`.`mail` (
+  `mail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_user` varchar(20) NOT NULL DEFAULT '',
+  `from_user` varchar(20) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` text,
+  `new_mail` tinyint(1) NOT NULL DEFAULT '1',
+  `reply` tinyint(4) DEFAULT '0',
+  `in_date` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`mail_id`),
+  KEY `uid` (`to_user`)
+) ENGINE=MyISAM AUTO_INCREMENT=1013 DEFAULT CHARSET=utf8;
 
- 
-
-CREATE TABLE `news` (
-
-  `news_id` int(11) NOT NULL auto_increment,
-
-  `user_id` varchar(20) NOT NULL default '',
-
-  `title` varchar(200) NOT NULL default '',
-
+CREATE TABLE  `jol`.`news` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
   `content` text NOT NULL,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `importance` tinyint(4) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`news_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8;
 
-  `time` datetime NOT NULL default '0000-00-00 00:00:00',
 
-  `importance` tinyint(4) NOT NULL default '0',
-
-  `defunct` char(1) NOT NULL default 'N',
-
-  PRIMARY KEY  (`news_id`)
-
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1004 ;
-
- 
-
- 
-
-CREATE TABLE `privilege` (
-
-  `user_id` char(20) NOT NULL default '',
-
-  `rightstr` char(30) NOT NULL default '',
-
-  `defunct` char(1) NOT NULL default 'N'
-
+CREATE TABLE  `jol`.`privilege` (
+  `user_id` char(20) NOT NULL DEFAULT '',
+  `rightstr` char(30) NOT NULL DEFAULT '',
+  `defunct` char(1) NOT NULL DEFAULT 'N'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
- 
-
-CREATE TABLE `problem` (
-
-  `problem_id` int(11) NOT NULL auto_increment,
-
-  `title` varchar(200) NOT NULL default '',
-
+CREATE TABLE  `jol`.`problem` (
+  `problem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL DEFAULT '',
   `description` text,
-
   `input` text,
-
   `output` text,
-
   `sample_input` text,
-
   `sample_output` text,
-
-  `spj` char(1) NOT NULL default '0',
-
+  `spj` char(1) NOT NULL DEFAULT '0',
   `hint` text,
+  `source` varchar(100) DEFAULT NULL,
+  `in_date` datetime DEFAULT NULL,
+  `time_limit` int(11) NOT NULL DEFAULT '0',
+  `memory_limit` int(11) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `accepted` int(11) DEFAULT '0',
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  PRIMARY KEY (`problem_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
 
-  `source` varchar(100) default NULL,
+CREATE TABLE  `jol`.`reply` (
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` varchar(20) NOT NULL,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `content` text NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `ip` varchar(30) NOT NULL,
+  PRIMARY KEY (`rid`),
+  KEY `author_id` (`author_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-  `sample_Program` varchar(255) default NULL,
 
-  `in_date` datetime default NULL,
+CREATE TABLE  `jol`.`sim` (
+  `s_id` int(11) NOT NULL,
+  `sim_s_id` int(11) DEFAULT NULL,
+  `sim` int(11) DEFAULT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-  `time_limit` int(11) NOT NULL default '0',
 
-  `memory_limit` int(11) NOT NULL default '0',
-
-  `defunct` char(1) NOT NULL default 'N',
-
-  `accepted` int(11) default '0',
-
-  `submit` int(11) default '0',
-
-  `ratio` tinyint(4) NOT NULL default '0',
-
-  `error` int(11) default '0',
-
-  `difficulty` tinyint(4) NOT NULL default '0',
-
-  `submit_user` int(11) default '0',
-
-  `solved` int(11) default '0',
-
-  `case_time_limit` int(11) NOT NULL default '0',
-
-  PRIMARY KEY  (`problem_id`)
-
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
-
- 
- 
-
-CREATE TABLE `solution` (
-
-  `solution_id` int(11) NOT NULL auto_increment,
-
-  `problem_id` int(11) NOT NULL default '0',
-
-  `user_id` char(20) NOT NULL default '',
-
-  `time` int(11) NOT NULL default '0',
-
-  `memory` int(11) NOT NULL default '0',
-
-  `in_date` datetime NOT NULL default '0000-00-00 00:00:00',
- 
-  `result` smallint(6) NOT NULL default '0',
-
-  `language` tinyint(4) NOT NULL default '0',
-
-  `ip` char(15) NOT NULL default '',
-
-  `contest_id` int(11) default NULL,
-
-  `valid` tinyint(4) NOT NULL default '1',
-
-  `num` tinyint(4) NOT NULL default '-1',
-
-  `code_length` int(11) NOT NULL default '0',
-
-  `judgetime` datetime default NULL,
-
-  PRIMARY KEY  (`solution_id`),
-
+CREATE TABLE  `jol`.`solution` (
+  `solution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` char(20) NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
+  `memory` int(11) NOT NULL DEFAULT '0',
+  `in_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `result` smallint(6) NOT NULL DEFAULT '0',
+  `language` tinyint(4) NOT NULL DEFAULT '0',
+  `ip` char(15) NOT NULL,
+  `contest_id` int(11) DEFAULT NULL,
+  `valid` tinyint(4) NOT NULL DEFAULT '1',
+  `num` tinyint(4) NOT NULL DEFAULT '-1',
+  `code_length` int(11) NOT NULL DEFAULT '0',
+  `judgetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`solution_id`),
   KEY `uid` (`user_id`),
-
   KEY `pid` (`problem_id`),
-
   KEY `res` (`result`),
-
   KEY `cid` (`contest_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
 
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
-
- 
- 
-
-CREATE TABLE `source_code` (
-
+CREATE TABLE  `jol`.`source_code` (
   `solution_id` int(11) NOT NULL,
-
   `source` text NOT NULL,
-
-  PRIMARY KEY  (`solution_id`)
-
+  PRIMARY KEY (`solution_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
-
-CREATE TABLE `users` (
-
-  `user_id` varchar(20) NOT NULL default '',
-
-  `email` varchar(100) default NULL,
-
-  `submit` int(11) default '0',
-
-  `solved` int(11) default '0',
-
-  `defunct` char(1) NOT NULL default 'N',
-
-  `ip` varchar(20) NOT NULL default '',
-
-  `accesstime` datetime default NULL,
-
-  `volume` int(11) NOT NULL default '1',
-
-  `language` int(11) NOT NULL default '1',
-
-  `password` varchar(32) default NULL,
-
-  `reg_time` datetime default NULL,
-
-  `nick` varchar(100) NOT NULL default '',
-
-  `school` varchar(100) NOT NULL default '',
-
-  PRIMARY KEY  (`user_id`)
-
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-ALTER TABLE `contest` ADD COLUMN `langmask` TINYINT  NOT NULL DEFAULT 0 COMMENT 'bits for LANG to mask' AFTER `private`;
-
-
-
-CREATE TABLE `topic` (
-  `tid` int(11) NOT NULL auto_increment,
+CREATE TABLE  `jol`.`topic` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varbinary(60) NOT NULL,
-  `status` int(2) NOT NULL default '0',
-  `top_level` int(2) NOT NULL default '0',
-  `cid` int(11) default NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `top_level` int(2) NOT NULL DEFAULT '0',
+  `cid` int(11) DEFAULT NULL,
   `pid` int(11) NOT NULL,
   `author_id` varchar(20) NOT NULL,
-  PRIMARY KEY  (`tid`),
+  PRIMARY KEY (`tid`),
   KEY `cid` (`cid`,`pid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `reply` (
- `rid` int(11) NOT NULL auto_increment,
- `author_id` varchar(20) NOT NULL,
- `time` datetime NOT NULL default '0000-00-00 00:00:00',
- `content` text NOT NULL,
- `topic_id` int(11) NOT NULL,
- `status` int(2) NOT NULL default '0',
- `ip` varchar(30) NOT NULL,
- PRIMARY KEY  (`rid`),
- KEY `author_id` (`author_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE  `jol`.`users` (
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(100) DEFAULT NULL,
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `accesstime` datetime DEFAULT NULL,
+  `volume` int(11) NOT NULL DEFAULT '1',
+  `language` int(11) NOT NULL DEFAULT '1',
+  `password` varchar(32) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `nick` varchar(100) NOT NULL DEFAULT '',
+  `school` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `online` (
   `hash` varchar(32) collate utf8_unicode_ci NOT NULL,
@@ -291,35 +178,5 @@ CREATE TABLE `online` (
   `uri` varchar(255) collate utf8_unicode_ci default NULL,
   PRIMARY KEY  (`hash`),
   UNIQUE KEY `hash` (`hash`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-ALTER TABLE `jol`.`online` ENGINE = MEMORY;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
-ALTER TABLE `jol`.`problem` DROP COLUMN `sample_Program`,
- DROP COLUMN `ratio`,
- DROP COLUMN `error`,
- DROP COLUMN `difficulty`,
- DROP COLUMN `submit_user`,
- DROP COLUMN `case_time_limit`;
-
-CREATE TABLE `sim` (
-  `s_id` int(11) NOT NULL,
-  `sim_s_id` int(11) NULL,
-  `sim` int(11) NULL,
-  PRIMARY KEY  (`s_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `mail` (
-
-  `mail_id` int(11) NOT NULL auto_increment,
-  `to_user` varchar(20) NOT NULL default '',
-  `from_user` varchar(20) NOT NULL default '',
-  `title` varchar(200) NOT NULL default '',
-  `content` text,
-  `new_mail` tinyint(1) NOT NULL default '1',
-  `reply` tinyint(4) default '0',
-  `in_date` datetime default NULL,
-  `defunct` char(1) NOT NULL default 'N',
-  PRIMARY KEY  (`mail_id`),
-  KEY `uid` (`to_user`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;

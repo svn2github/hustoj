56dfccc6015f711b6e05b187995bfff05542b85a
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-06-26 14:37:09 +0000

    more language
    git-svn-id: http://hustoj.googlecode.com/svn@1080 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -93,7 +93,7 @@ if (isset($_GET['user_id'])){
 if (isset($_GET['language'])) $language=intval($_GET['language']);
 else $language=-1;
 
-if ($language>6 || $language<0) $language=-1;
+if ($language>9 || $language<0) $language=-1;
 if ($language!=-1){
 	$sql=$sql."AND `language`='".strval($language)."' ";
 	$str2=$str2."&language=".$language;
@@ -107,10 +107,10 @@ if ($language!=-1){
 <?
 if (isset($_GET['language'])) $language=$_GET['language'];
 else $language=-1;
-if ($language<0||$language>5) $language=-1;
+if ($language<0||$language>9) $language=-1;
 if ($language==-1) echo "<option value='-1' selected>All</option>";
 else echo "<option value='-1'>All</option>";
-for ($i=0;$i<7;$i++){
+for ($i=0;$i<10;$i++){
 	if ($i==$language) echo "<option value=$i selected>$language_name[$i]</option>";
 	else echo "<option value=$i>$language_name[$i]</option>";
 }

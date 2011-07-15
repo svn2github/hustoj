7036674916abf1b2894f9d669d0b8cb4faffaf92
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-07-15 01:22:36 +0000

    issue51
    git-svn-id: http://hustoj.googlecode.com/svn@1127 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/registerpage.php
+++ trunk/web/registerpage.php
@@ -7,34 +7,34 @@ require_once("include/db_info.inc.php");
 <form action="register.php" method="post">
 	<br><br>
 	<center><table>
-		<tr><td colspan=2 height=40 width=500>&nbsp;&nbsp;&nbsp;<?php echo $MSG_REG_INFO?></tr>
-		<tr><td width=25%><?php echo $MSG_USER_ID?>:
-			<td width=75%><input name="user_id" size=20 type=text>*
+		<tr><td colspan=2 height=40 width=500>&nbsp;&nbsp;&nbsp;<?php echo $MSG_REG_INFO?></td></tr>
+		<tr><td width=25%><?php echo $MSG_USER_ID?>:</td>
+			<td width=75%><input name="user_id" size=20 type=text>*</td>
 		</tr>
-		<tr><td><?php echo $MSG_NICK?>:
-			<td><input name="nick" size=50 type=text>
+		<tr><td><?php echo $MSG_NICK?>:</td>
+			<td><input name="nick" size=50 type=text></td>
 		</tr>
-		<tr><td><?php echo $MSG_PASSWORD?>:
-			<td><input name="password" size=20 type=password>*
+		<tr><td><?php echo $MSG_PASSWORD?>:</td>
+			<td><input name="password" size=20 type=password>*</td>
 		</tr>
-		<tr><td><?php echo $MSG_REPEAT_PASSWORD?>:
-			<td><input name="rptpassword" size=20 type=password>*
+		<tr><td><?php echo $MSG_REPEAT_PASSWORD?>:</td>
+			<td><input name="rptpassword" size=20 type=password>*</td>
 		</tr>
-		<tr><td><?php echo $MSG_SCHOOL?>:
-			<td><input name="school" size=30 type=text>
+		<tr><td><?php echo $MSG_SCHOOL?>:</td>
+			<td><input name="school" size=30 type=text></td>
 		</tr>
-		<tr><td><?php echo $MSG_EMAIL?>:
-			<td><input name="email" size=30 type=text>
+		<tr><td><?php echo $MSG_EMAIL?>:</td>
+			<td><input name="email" size=30 type=text></td>
 		</tr>
 		<?php if($OJ_VCODE){?>
-		<tr><td>TYPE:<img src=vcode.php>
-			<td><input name="vcode" size=4 type=text>*
+		<tr><td>Verify Code::<img src=vcode.php></td>
+			<td><input name="vcode" size=4 type=text>*</td>
 		</tr>
 		<?php }?>
-		<tr><td>
+		<tr><td></td>
 			<td><input value="Submit" name="submit" type="submit">
 				&nbsp; &nbsp;
-				<input value="Reset" name="reset" type="reset">
+				<input value="Reset" name="reset" type="reset"></td>
 		</tr>
 	</table></center>
 	<br><br>

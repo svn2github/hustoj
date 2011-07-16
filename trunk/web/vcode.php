d334a2c8f8c8e6c7ffcfb374ed45ccabd03b9806
newsclan <newsclan@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-07-16 02:04:45 +0000

    issue 50
    git-svn-id: http://hustoj.googlecode.com/svn@1131 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/vcode.php
+++ trunk/web/vcode.php
@@ -1,52 +1,74 @@
 <?php
-   /*   网站验证码程序
-    *   运行环境： PHP5.0.18 下调试通过
-    *   需要 gd2 图形库支持（PHP.INI中 php_gd2.dll开启）
-    *   文件名: showimg.php
-    *   作者：  17php.com
-    *   Date:   2007.03
-    *   技术支持： www.17php.com
-    */
-
-   //随机生成一个4位数的数字验证码
-    $num="";
-    for($i=0;$i<4;$i++){
-    $num .= rand(0,9);
-    }
-   //4位验证码也可以用rand(1000,9999)直接生成
-   //将生成的验证码写入session，备验证页面使用
-    session_start();
-    $_SESSION["vcode"] = $num;
-   //创建图片，定义颜色值
-    Header("Content-type: image/PNG");
-    srand((double)microtime()*1000000);
-    $im = imagecreate(60,20);
-    $black = ImageColorAllocate($im, 0,0,0);
-    $gray = ImageColorAllocate($im, 200,200,200);
-    imagefill($im,0,0,$gray);
-
-    //随机绘制两条虚线，起干扰作用
-    $style = array($black, $black, $black, $black, $black, $gray, $gray, $gray, $gray, $gray);
-    imagesetstyle($im, $style);
-    $y1=rand(0,20);
-    $y2=rand(0,20);
-    $y3=rand(0,20);
-    $y4=rand(0,20);
-    imageline($im, 0, $y1, 60, $y3, IMG_COLOR_STYLED);
-    imageline($im, 0, $y2, 60, $y4, IMG_COLOR_STYLED);
-
-    //在画布上随机生成大量黑点，起干扰作用;
-    for($i=0;$i<80;$i++)
-    {
-    imagesetpixel($im, rand(0,60), rand(0,20), $black);
-    }
-    //将四个数字随机显示在画布上,字符的水平间距和位置都按一定波动范围随机生成
-    $strx=rand(3,8);
-    for($i=0;$i<4;$i++){
-    $strpos=rand(1,6);
-    imagestring($im,5,$strx,$strpos, substr($num,$i,1), $black);
-    $strx+=rand(8,12);
-    }
-    ImagePNG($im);
-    ImageDestroy($im);
-   ?>
+	/****************************************
+	*  验证码 v0.9
+	*  Powerd by awaysoft.com
+	*  本组件采用GPLv3发布
+	*  2011-07-15
+	****************************************/
+	/* $len 为随机字符串长度，$type为类型，a为字符数字，c为字符,n为数字， 已经去除可能误导的字符 */
+	function get_rand_string($len=4, $type="n"){
+		if ($len < 0) $len = 4;
+		if ($type == 'a') $chars = 'ABCDEFGHJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz023456789';
+		else if ($type == 'c') $chars = 'ABCDEFGHJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz';
+		else if ($type == 'n') $chars = '0123456789';
+		else $chars = '0123456789';
+		
+		$result = '';
+		for ($i = 0; $i < $len; $i ++){
+			$index = mt_rand(0, strlen($chars) - 1);
+			$result .= substr($chars, $index, 1);
+		}
+		return $result;
+	}
+	
+	session_start();
+	
+	/* 输出图片类型，字符长度，类型 */
+	$imgtype = 'gif';
+	$len = 4;
+	$vcodetype = 'n';
+	
+	$width = 15 * $len;
+	$height = 24;
+	/* 生成随机字符串并写入SESSION */
+	$vcode = get_rand_string($len, $vcodetype);
+	$_SESSION['vcode'] = $vcode;
+	header("Content-type: image/".$imgtype);
+	
+	if($imgtype != 'gif' && function_exists('imagecreatetruecolor')){
+		$im = imagecreatetruecolor($width, $height);
+	}else{
+		$im = imagecreate($width, $height);
+	}
+	
+	$r = mt_rand(0, 255);
+	$g = mt_rand(0, 255);
+	$b = mt_rand(0, 255);
+	/* 生成背景颜色 */
+	$backColor = ImageColorAllocate($im, $r, $g, $b);
+	/* 生成边框颜色 */
+	$borderColor = ImageColorAllocate($im, 0, 0, 0);
+	/* 生成干扰点颜色 */
+	$pointColor = ImageColorAllocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
+	
+	/* 背景位置 */
+	imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $backColor);
+	/* 边框位置 */
+	imagerectangle($im, 0, 0, $width - 1, $height - 1, $borderColor);
+	
+	/* 字符串颜色(背景反色) */
+	$stringColor = ImageColorAllocate($im, 255 - $r, 255 - $g, 255 - $b);
+	
+	/* 产生干扰点 */
+	$pointNumber = mt_rand($len * 25, $len * 50);
+	for($i=0; $i<=$pointNumber; $i++){
+		$pointX = mt_rand(2,$width-2);
+		$pointY = mt_rand(2,$height-2);
+		imagesetpixel($im, $pointX, $pointY, $pointColor);
+	}
+	
+	imagettftext($im, 15, 0, 4, 20, $stringColor, "include/Vera.ttf", $vcode);
+	$image_out = 'Image' . $imgtype;
+	$image_out($im);
+	@ImageDestroy($im);
+?>

5c5386c7790dde6e1ab5573376e16550185a3c3c
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-07-29 09:32:07 +0000

    try seperate cache
    git-svn-id: http://hustoj.googlecode.com/svn@1149 19ab8b60-bd6c-11dd-baed-41e496641fac

---
+++ trunk/web/include/cache_start.php
@@ -0,0 +1,50 @@
+<?php
+        require_once("./include/db_info.inc.php");
+        //cache head start
+        $cache_time=2;
+        $file="cache/index.html";
+        $sid="";
+        if (isset($_SESSION['user_id'])){
+                $sid=session_id().$_SERVER['REMOTE_ADDR'];
+        }
+        if (isset($_SERVER['QUERY_STRING'])){
+                $sid.=$_SERVER['QUERY_STRING'];
+        }
+        $sid=md5($sid);
+        $file = "cache/status_$sid.html";
+        
+        if($OJ_MEMCACHE ){
+                $mem = new Memcache;
+                if($OJ_SAE)
+                        $mem=memcache_init();
+                else{
+                        $mem->connect($OJ_MEMSERVER,  $OJ_MEMPORT);
+                }
+                $content=$mem->get($file);
+                if($content){
+                         echo $content;
+                         exit();
+                }else{
+                        $use_cache=false;
+                        $write_cache=true;
+                }
+        }else{
+                
+                if (file_exists ( $file ))
+                        $last = filemtime ( $file );
+                else
+                        $last =0;
+                $write_cache=$_SERVER['QUERY_STRING']==""||
+                                         (isset($_GET['cid'])&&$_SERVER['QUERY_STRING']==("cid=".$_GET['cid']));
+                $use_cache=(time () - $last < $cache_time)&&$write_cache;
+                $write_cache=(!$use_cache)&&$write_cache;
+        }
+        if ($use_cache) {
+                //header ( "Location: $file" );
+                include ($file);
+                exit ();
+        } else if($write_cache){
+                ob_start ();
+        }
+//cache head stop
+?>

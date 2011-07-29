47a2fb76ad349ddfafed780179aeeb2bbf570644
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-07-29 09:08:50 +0000

    memcached status.php first
    git-svn-id: http://hustoj.googlecode.com/svn@1146 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -2,28 +2,44 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 	@session_start();
-	
+	require_once("./include/db_info.inc.php");
 	//cache head start
 	$cache_time=2;
 	$file="cache/index.html";
 	if (isset($_SESSION['user_id'])){
 		$sid=session_id().$_SERVER['REMOTE_ADDR'];
-		if (isset($_GET['cid'])){
-			$sid.=intval($_GET['cid']);
+		if (isset($_SERVER['QUERY_STRING'])){
+			$sid.=$_SERVER['QUERY_STRING'];
 		}
 		$sid=md5($sid);
 		$file = "cache/status_$sid.html";
 	}
-	
-	if (file_exists ( $file ))
-		$last = filemtime ( $file );
-	else
-		$last =0;
-	$write_cache=$_SERVER['QUERY_STRING']==""||
-				 (isset($_GET['cid'])&&$_SERVER['QUERY_STRING']==("cid=".$_GET['cid']));
-	$use_cache=(time () - $last < $cache_time)&&$write_cache;
-	$write_cache=(!$use_cache)&&$write_cache;
-	
+	if($OJ_MEMCACHE ){
+		$mem = new Memcache;
+        	if($OJ_SAE)
+        		$mem=memcache_init();
+        	else{
+        		$mem->connect($OJ_MEMSERVER,  $OJ_MEMPORT);
+        	}
+        	$content=$mem->get($file);
+        	if($content){
+        		 echo $content;
+        		 exit();
+        	}else{
+        		$use_cache=false;
+        		$write_cache=true;
+        	}
+        }else{
+		
+		if (file_exists ( $file ))
+			$last = filemtime ( $file );
+		else
+			$last =0;
+		$write_cache=$_SERVER['QUERY_STRING']==""||
+					 (isset($_GET['cid'])&&$_SERVER['QUERY_STRING']==("cid=".$_GET['cid']));
+		$use_cache=(time () - $last < $cache_time)&&$write_cache;
+		$write_cache=(!$use_cache)&&$write_cache;
+	}
 	if ($use_cache) {
 		//header ( "Location: $file" );
 		include ($file);

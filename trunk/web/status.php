b00655adfed80fea28aef213efbff8fe81ff3720
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-08-02 02:47:33 +0000

    cache 2
    git-svn-id: http://hustoj.googlecode.com/svn@1165 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -3,6 +3,7 @@ header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
         @session_start();
         require_once("./include/db_info.inc.php");
+  $cache_time=2;
         require_once("./include/cache_start.php");
         
 require_once("./include/my_func.inc.php");

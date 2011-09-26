eb8481cd4c2c5f23b5f9f68e48a9145e76ad0308
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-09-26 05:40:37 +0000

    time call for pascal
    git-svn-id: http://hustoj.googlecode.com/svn@1265 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/okcalls.h
+++ trunk/core/judge_client/okcalls.h
@@ -26,8 +26,8 @@ int LANG_JC[256]={-1,         -1,              -1,            -1,               
 int LANG_CV[256]={SYS_read, SYS_uname, SYS_write, SYS_open, SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect, SYS_mmap2, SYS_fstat64, SYS_set_thread_area, 252,0};
 int LANG_CC[256]={-1,       -1,        -1,        -1,       -1,        1,          -1,         -1,      -1,         -1,           -1,        -1,          -1,                  2,0};
 
-int LANG_PV[256]={SYS_open, SYS_set_thread_area, SYS_brk, SYS_read, SYS_uname, SYS_write, SYS_execve, SYS_ioctl, SYS_readlink, SYS_mmap, SYS_rt_sigaction, SYS_getrlimit, 252,191,0};
-int LANG_PC[256]={-1,       -1,                  -1,      -1,       -1,        -1,        1,          -1,        -1,           -1,       -1,               -1,            2,-1,0};
+int LANG_PV[256]={13,SYS_open, SYS_set_thread_area, SYS_brk, SYS_read, SYS_uname, SYS_write, SYS_execve, SYS_ioctl, SYS_readlink, SYS_mmap, SYS_rt_sigaction, SYS_getrlimit, 252,191,0};
+int LANG_PC[256]={-1,-1,       -1,                  -1,      -1,       -1,        -1,        1,          -1,        -1,           -1,       -1,               -1,            2,-1,0};
 
 int LANG_RV[256]={126,SYS_access,SYS_brk,SYS_close,SYS_execve,SYS_exit_group,SYS_fstat64,SYS_futex,SYS_getegid32,SYS_geteuid32,SYS_getgid32,SYS_getrlimit,SYS_gettimeofday,SYS_getuid32,SYS_mmap2,SYS_mprotect,SYS_munmap,SYS_open,SYS_read,SYS_rt_sigaction,SYS_rt_sigprocmask,SYS_set_robust_list,SYS_set_thread_area,SYS_set_tid_address,SYS_uname,SYS_write,191,195,120,7,0};
 

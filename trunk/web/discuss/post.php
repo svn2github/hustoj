d979348c66f6a151efedd2cadfba3b9a0faa74e4
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-10-21 02:32:55 +0000

    real_escape
    git-svn-id: http://hustoj.googlecode.com/svn@1292 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/discuss/post.php
+++ trunk/web/discuss/post.php
@@ -30,15 +30,15 @@
                         else
                                 $pid=0;
                         if(array_key_exists('cid',$_REQUEST)&&$_REQUEST['cid']!='')
-                                $cid="'".mysql_escape_string($_REQUEST['cid'])."'";
+                                $cid="'".mysql_real_escape_string($_REQUEST['cid'])."'";
                         else
                                 $cid='NULL';
-                        $sql="INSERT INTO `topic` (`title`, `author_id`, `cid`, `pid`) SELECT '".mysql_escape_string($_POST['title'])."', '".mysql_escape_string($_SESSION['user_id'])."', $cid, '".mysql_escape_string($pid)."'";
+                        $sql="INSERT INTO `topic` (`title`, `author_id`, `cid`, `pid`) SELECT '".mysql_real_escape_string($_POST['title'])."', '".mysql_real_escape_string($_SESSION['user_id'])."', $cid, '".mysql_real_escape_string($pid)."'";
                         if($pid!=0)
                                 if($cid!='NULL')
-                                        $sql.=" FROM `contest_problem` WHERE `contest_id` = $cid AND `problem_id` = '".mysql_escape_string($pid)."'";
+                                        $sql.=" FROM `contest_problem` WHERE `contest_id` = $cid AND `problem_id` = '".mysql_real_escape_string($pid)."'";
                                 else
-                                        $sql.=" FROM `problem` WHERE `problem_id` = '".mysql_escape_string($pid)."'";
+                                        $sql.=" FROM `problem` WHERE `problem_id` = '".mysql_real_escape_string($pid)."'";
                         else if($cid!='NULL')
                                 $sql.=" FROM `contest` WHERE `contest_id` = $cid";
                         $sql.=" LIMIT 1";
@@ -54,7 +54,7 @@
         if ($_REQUEST['action']=='reply' || !is_null($tid)){
                 if(is_null($tid)) $tid=$_POST['tid'];
                 if (!is_null($tid) && array_key_exists('content', $_POST) && $_POST['content']!=''){
-                        $sql="INSERT INTO `reply` (`author_id`, `time`, `content`, `topic_id`,`ip`) SELECT '".mysql_escape_string($_SESSION['user_id'])."', NOW(), '".mysql_escape_string($_POST['content'])."', '".mysql_escape_string($tid)."','".$_SERVER['REMOTE_ADDR']."' FROM `topic` WHERE `tid` = '".mysql_escape_string($tid)."' AND `status` = 0 ";
+                        $sql="INSERT INTO `reply` (`author_id`, `time`, `content`, `topic_id`,`ip`) SELECT '".mysql_real_escape_string($_SESSION['user_id'])."', NOW(), '".mysql_real_escape_string($_POST['content'])."', '".mysql_real_escape_string($tid)."','".$_SERVER['REMOTE_ADDR']."' FROM `topic` WHERE `tid` = '".mysql_real_escape_string($tid)."' AND `status` = 0 ";
                         
                         mysql_query($sql) or die (mysql_error());
                         if(mysql_affected_rows()>0)

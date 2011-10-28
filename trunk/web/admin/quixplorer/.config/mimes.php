35cf5c1de6b247dca4a02a9c2cf56811f6056301
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2011-10-28 02:29:48 +0000

    no php but in out
    git-svn-id: http://hustoj.googlecode.com/svn@1298 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/quixplorer/.config/mimes.php
+++ trunk/web/admin/quixplorer/.config/mimes.php
@@ -2,7 +2,7 @@
 //------------------------------------------------------------------------------
 // editable files:
 $GLOBALS["editable_ext"]=array(
-	"\.txt$|\.php$|\.php3$|\.phtml$|\.inc$|\.sql$|\.pl$",
+	"\.txt$|\.in$|\.out$|\.phtml$|\.inc$|\.sql$|\.pl$",
 	"\.htm$|\.html$|\.shtml$|\.dhtml$|\.xml$",
 	"\.js$|\.css$|\.cgi$|\.cpp$\.c$|\.cc$|\.cxx$|\.hpp$|\.h$",
 	"\.pas$|\.p$|\.java$|\.py$|\.sh$\.tcl$|\.tk$"

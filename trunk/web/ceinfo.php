bf3727aca34ae4e23fb3aabfeda08c574a5a1936
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-02-06 06:02:24 +0000

    auto explain usual compiler message to new programmers
    git-svn-id: http://hustoj.googlecode.com/svn@1380 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/ceinfo.php
+++ trunk/web/ceinfo.php
@@ -4,7 +4,6 @@ require_once("oj-header.php")?>
 <title>Compile Error Info</title>
 <?php require_once("./include/db_info.inc.php");
 require_once("./include/const.inc.php");
-
 if (!isset($_GET['sid'])){
 	echo "No such code!\n";
 	require_once("oj-footer.php");
@@ -20,20 +19,52 @@ if (isset($_SESSION['source_browser'])) $ok=true;
 if ($ok==true){
 	if($row->user_id!=$_SESSION['user_id'])
 		echo "<a href='mail.php?to_user=$row->user_id&title=$MSG_SUBMIT $id'>Mail the auther</a>";
-	echo "<pre>";
+	echo "<pre id='errtxt'>";
 	mysql_free_result($result);
 	$sql="SELECT `error` FROM `compileinfo` WHERE `solution_id`='".$id."'";
 	$result=mysql_query($sql);
 	$row=mysql_fetch_object($result);
-	$ce=str_replace("\n\r","\n",$row->error);
-	$ce=htmlspecialchars($ce);
-	$ce=str_replace("Main.","<a href=showsource.php?id=$id>Main.</a>",$ce);
-	echo $ce."</pre>";
+	echo htmlspecialchars(str_replace("\n\r","\n",$row->error))."</pre>";
+  echo "<div id='errexp'>Explain:</div>";
 	mysql_free_result($result);
 }else{
 	mysql_free_result($result);
 	echo "I am sorry, You could not view this message!";
 }
 ?>
-<script></script>
+
+<script>
+   var pats=new Array();
+   var exps=new Array();
+   pats[0]=/warning.*declaration of 'main' with no type/;
+   exps[0]="C++标准中，main函数必须有返回值";
+   pats[1]=/'.*' was not declared in this scope/;
+   exps[1]="变量没有声明过，检查下是否拼写错误！";
+   pats[2]=/main’ must return ‘int’/;
+   exps[2]="在标准C语言中，main函数返回值类型必须是int，教材和VC中使用void是非标准的用法";
+   pats[3]=/ .* was not declared in this scope/;
+   exps[3]="函数或变量没有声明过就进行调用，检查下是否导入了正确的头文件";
+   pats[4]=/printf.*was not declared in this scope/;
+   exps[4]="printf函数没有声明过就进行调用，检查下是否导入了stdio.h或cstdio头文件";
+   
+   function explain(){
+     //alert("asdf");
+       var errmsg=document.getElementById("errtxt").innerHTML;
+	   var expmsg="";
+	   for(var i=0;i<pats.length;i++){
+		   var pat=pats[i];
+		   var exp=exps[i];
+		   var ret=pat.exec(errmsg);
+		   if(ret){
+		      expmsg+=ret+":"+exp+"<br>";
+		   }
+	   }
+	   document.getElementById("errexp").innerHTML=expmsg;
+     //alert(expmsg);
+   }
+   explain();
+ 
+ </script>
+
+
 <?php require_once("oj-footer.php")?>

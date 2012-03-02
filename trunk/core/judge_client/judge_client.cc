dd03a3aff0b69d052a51601b50e82fc0a23c309c
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-03-02 05:42:19 +0000

    do not change oj_home for multijudge
    git-svn-id: http://hustoj.googlecode.com/svn@1391 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1528,8 +1528,8 @@ void mk_shm_workdir(char * work_dir){
 	execute_cmd("mkdir -p %s",shm_path);
 	execute_cmd("rm -rf %s",work_dir);
 	execute_cmd("ln -s %s %s/",shm_path,oj_home);
-  execute_cmd("chown judge %s %s",shm_path,oj_home);
-  execute_cmd("chmod 775 %s %s",shm_path,oj_home);
+  execute_cmd("chown judge %s ",shm_path);
+  execute_cmd("chmod 775 %s ",shm_path);
   //sim need a soft link in shm_dir to work correctly
   sprintf(shm_path,"/dev/shm/hustoj/%s/",oj_home);
   execute_cmd("ln -s %s/data %s",oj_home,shm_path);

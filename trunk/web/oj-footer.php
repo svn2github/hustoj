951376c941d00009c98f1f4ce7c00fe9d3fddf67
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-03-17 09:59:57 +0000

    2012
    git-svn-id: http://hustoj.googlecode.com/svn@1403 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/oj-footer.php
+++ trunk/web/oj-footer.php
@@ -17,7 +17,7 @@
 	<?php }?>
 		<br>
 		All Copyright Reserved 2010-2011 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
-		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2011 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2012 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?php echo $OJ_ADMIN?>">admin</a><br>
 		
 	</p>

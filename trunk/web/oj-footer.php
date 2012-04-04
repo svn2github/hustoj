28ac5334968be4110cdce172ce5c90df36c14089
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-04 06:30:57 +0000

    powered by sinaapp
    git-svn-id: http://hustoj.googlecode.com/svn@1443 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/oj-footer.php
+++ trunk/web/oj-footer.php
@@ -19,6 +19,11 @@
 		All Copyright Reserved 2010-2011 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
 		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2012 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?php echo $OJ_ADMIN?>">admin</a><br>
+     <?php if ($OJ_SAE) {
+                   echo "<a href=http://sae.sina.com.cn><img bolder=0 src=http://static.sae.sina.com.cn/image/poweredby/poweredby.png></a>";
+            
+           }
+     ?>
 		
 	</p>
 </center>

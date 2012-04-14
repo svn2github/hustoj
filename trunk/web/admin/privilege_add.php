5bb9d3f4afd9b66fddd2acad4d906f69dabea17c
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-14 22:23:43 +0000

    problem editor
    git-svn-id: http://hustoj.googlecode.com/svn@1464 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/privilege_add.php
+++ trunk/web/admin/privilege_add.php
@@ -20,7 +20,7 @@ if(isset($_POST['do'])){
 	Privilege:
 	<select name="rightstr">
 <?php
-$rightarray=array("administrator","source_browser","contest_creator","http_judge" );
+$rightarray=array("administrator","problem_editor","source_browser","contest_creator","http_judge" );
 while(list($key, $val)=each($rightarray)) {
 	if (isset($rightstr) && ($rightstr == $val)) {
 		echo '<option value="'.$val.'" selected>'.$val.'</option>';

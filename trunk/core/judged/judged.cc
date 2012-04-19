87d624d090d5e2f3f5367833ee880c7b3a7c223f
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-19 02:50:41 +0000

    warn
    git-svn-id: http://hustoj.googlecode.com/svn@1480 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judged/judged.cc
+++ trunk/core/judged/judged.cc
@@ -91,7 +91,7 @@ void write_log(const char *fmt, ...)
 	va_list         ap;
 	char            buffer[4096];
 //	time_t          t = time(NULL);
-	int             l;
+//	int             l;
 	sprintf(buffer,"%s/log/client.log",oj_home);
 	FILE *fp = fopen(buffer, "a+");
 	if (fp==NULL){

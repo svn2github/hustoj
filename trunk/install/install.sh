94da8d106219ac7d3ba7af521af1808f6be0063b
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-23 15:49:45 +0000

    centos import fail
    git-svn-id: http://hustoj.googlecode.com/svn@1515 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/install/install.sh
+++ trunk/install/install.sh
@@ -47,7 +47,7 @@ sudo cp java0.policy  judge.conf /home/judge/etc
 sudo chown -R judge /home/judge
 sudo chgrp -R $APACHEUSER /home/judge/data
 sudo chgrp -R root /home/judge/etc /home/judge/run?
-sudo chmod 775 /home/judge/data /home/judge/etc /home/judge/run?
+sudo chmod 775 /home/judge /home/judge/data /home/judge/etc /home/judge/run?
 
 #boot up judged
 sudo cp judged /etc/init.d/judged

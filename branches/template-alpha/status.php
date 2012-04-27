fbc47e49c3ce8b314fbc984091b21768db7f865d
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-27 06:20:38 +0000

    complex code into control
    git-svn-id: http://hustoj.googlecode.com/svn@1534 19ab8b60-bd6c-11dd-baed-41e496641fac

--- branches/template-alpha/status.php
+++ branches/template-alpha/status.php
@@ -133,9 +133,114 @@ if($OJ_MEMCACHE)
 	$row=$result[$i];
 else
 	$row=mysql_fetch_array($result);
-	$view_status[$i]=$row;
+	//$view_status[$i]=$row;
+	
+		if ($top==-1) $top=$row['solution_id'];
+        $bottom=$row['solution_id'];
+        $flag=(!is_running(intval($row['contest_id']))) ||
+                        isset($_SESSION['source_browser']) ||
+                        isset($_SESSION['administrator']) || 
+
+                        (isset($_SESSION['user_id'])&&!strcmp($row['user_id'],$_SESSION['user_id']));
+
+        $cnt=1-$cnt;
+
+        $view_status[$i][0]= $row['solution_id'];
+        $view_status[$i][1]= "<a href='userinfo.php?user=".$row['user_id']."'>".$row['user_id']."</a>";
+
+
+       if ($row['contest_id']>0) {
+                $view_status[$i][2]= "<a href='problem.php?cid=".$row['contest_id']."&pid=".$row['num']."'>";
+                if(isset($cid)){
+                         $view_status[$i][2].= $PID[$row['num']]."</a>";
+                }else{
+                        $view_status[$i][2].= $row['problem_id']."</a>";
+                }
+
+        }else{
+                $view_status[$i][2]= "<a href='problem.php?id=".$row['problem_id']."'>".$row['problem_id']."</a>";
+        }
+
+       
+        if (intval($row['result'])==11 && ((isset($_SESSION['user_id'])&&$row['user_id']==$_SESSION['user_id']) || isset($_SESSION['source_browser']))){
+                $view_status[$i][3]= "<a href='ceinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$judge_result[$row['result']]."</a>";
+        }else if (intval($row['result'])==10 && ((isset($_SESSION['user_id'])&&$row['user_id']==$_SESSION['user_id']) || isset($_SESSION['source_browser']))){
+                $view_status[$i][3]= "<a href='reinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$judge_result[$row['result']]."</a>";
+
+        }else{
+
+                if($OJ_SIM&&$row['sim']>80&&$row['sim_s_id']!=$row['s_id']) {
+                        $view_status[$i][3]= "<span class=".$judge_color[$row['result']].">*".$judge_result[$row['result']]."</span>-<span class=red>";
+                       
+                        if( isset($_SESSION['source_browser'])){
+
+                                        $view_status[$i][3].= "<a href=showsource.php?id=".$row['sim_s_id']." target=original>".$row['sim_s_id']."(".$row['sim']."%)</a>";
+                        }else{
+
+                                        $view_status[$i][3].= $row['sim_s_id'];
+
+                        }
+                        if(isset($_GET['showsim'])&&isset($row[13])){
+                                        $view_status[$i][3].= "$row[13]";
+                                
+                        }
+                        $view_status[$i][3].="</span>";
+                }else{
+
+                        $view_status[$i][3]= "<span class=".$judge_color[$row['result']].">".$judge_result[$row['result']];
+                }
+                
+        }
+        if (isset($row['pass_rate'])&&$row['pass_rate']>0&&$row['pass_rate']<.98) 
+				$view_status[$i][3].= (100-$row['pass_rate']*100)."%";
+        if ($flag){
+
+
+                if ($row['result']>=4){
+                        $view_status[$i][4]= "<span class=red>".$row['memory'];
+                        $view_status[$i][5]= "<span class=red>".$row['time'];
+						
+                }else{
+                        $view_status[$i][4]= "---";
+                        $view_status[$i][5]= "---";
+						
+                }
+
+                if (!(isset($_SESSION['user_id'])&&strtolower($row['user_id'])==strtolower($_SESSION['user_id']) || isset($_SESSION['source_browser']))){
+                        $view_status[$i][6]=$language_name[$row['language']];
+                }else{
+
+                        $view_status[$i][6]= "<a target=_blank href=showsource.php?id=".$row['solution_id'].">".$language_name[$row['language']]."</a>/";
+
+                        if (isset($cid)) {
+                                $view_status[$i][6]= "<a target=_self href=\"submitpage.php?cid=".$cid."&pid=".$row['num']."&sid=".$row['solution_id']."\">Edit</a>";
+                        }else{
+                                $view_status[$i][6]= "<a target=_self href=\"submitpage.php?id=".$row['problem_id']."&sid=".$row['solution_id']."\">Edit</a>";
+                        }
+                }
+                $view_status[$i][7]= $row['code_length']." B";
+				
+        }else
+			
+		$view_status[$i][4]="----";
+        $view_status[$i][5]="----";
+        $view_status[$i][6]="----";
+        $view_status[$i][7]="----";
+        $view_status[$i][8]= $row['in_date'];
+        
+   
+   
+
 }
 mysql_free_result($result);
+
+
+
+
+
+
+
+
 ?>
 
 <?php

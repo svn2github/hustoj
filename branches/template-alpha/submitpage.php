08fbe5a3c7c31d46840dace279594d514fe5848d
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-27 13:19:06 +0000

    git-svn-id: http://hustoj.googlecode.com/svn@1542 19ab8b60-bd6c-11dd-baed-41e496641fac

--- branches/template-alpha/submitpage.php
+++ branches/template-alpha/submitpage.php
@@ -4,7 +4,7 @@
     require_once('./include/cache_start.php');
     require_once('./include/db_info.inc.php');
 	require_once('./include/setlang.php');
-
+	$view_title=$MSG_SUBMIT;
  if (!isset($_SESSION['user_id'])){
 
 	$view_errors= "<a href=loginpage.php>Please Login First</a>";

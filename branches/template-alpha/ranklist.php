544adbf81092112317f7a9cab2b50db68a48c166
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-04-28 06:36:12 +0000

    git-svn-id: http://hustoj.googlecode.com/svn@1560 19ab8b60-bd6c-11dd-baed-41e496641fac

--- branches/template-alpha/ranklist.php
+++ branches/template-alpha/ranklist.php
@@ -1,6 +1,6 @@
 <?php
 	$OJ_CACHE_SHARE=false;
-	$cache_time=3;
+	$cache_time=30;
 	require_once('./include/cache_start.php');
     require_once('./include/db_info.inc.php');
 	require_once('./include/setlang.php');

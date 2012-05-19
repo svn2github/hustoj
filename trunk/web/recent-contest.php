9afcb16cf3f329222bc6086e580e744ab6b1f7b6
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-05-19 00:36:53 +0000

    united cache for sae warning
    git-svn-id: http://hustoj.googlecode.com/svn@1608 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/recent-contest.php
+++ trunk/web/recent-contest.php
@@ -1,29 +1,16 @@
 <?php
 ////////////////////////////Common head
-	$cache_time=10;
-	$OJ_CACHE_SHARE=false;
+	$cache_time=60;
+	$OJ_CACHE_SHARE=true;
 	require_once('./include/cache_start.php');
-    require_once('./include/db_info.inc.php');
+  require_once('./include/db_info.inc.php');
 	require_once('./include/setlang.php');
 	$view_title= "Recent Contests from Naikai-contest-spider";
 
-  $local_file = 'cache/contests.json';//local file of contests.json
-$last_modified = filemtime($local_file);
-if (time() - $last_modified > 1*60*60) {
-    $json = @file_get_contents('http://contests.acmicpc.info/contests.json');
-    if ($json) {
-        file_put_contents($local_file, $json);
-    }
-    else {
-        $json = file_get_contents($local_file);
-        touch($local_file, time());
-    }
-}
-else {
-    $json = file_get_contents($local_file);
-}
 
-$rows = json_decode($json, true);
+  $json = @file_get_contents('http://contests.acmicpc.info/contests.json');
+   
+	$rows = json_decode($json, true);
 
 
 

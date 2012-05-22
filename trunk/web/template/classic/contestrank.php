7c84cfe2d211da1189daaef0e9511f48ddb17a43
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-05-22 05:22:53 +0000

    fix download link
    git-svn-id: http://hustoj.googlecode.com/svn@1621 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/classic/contestrank.php
+++ trunk/web/template/classic/contestrank.php
@@ -12,7 +12,7 @@
 <?php
 $rank=1;
 ?>
-<center><h3>Contest RankList -- <?php echo $title?></h3><a href=contestrank.xls.php?cid=$cid>Download</a></center>
+<center><h3>Contest RankList -- <?php echo $title?></h3><a href="contestrank.xls.php?cid=<?php echo $cid?>" >Download</a></center>
 <table><tr class=toprow align=center><td width=5%>Rank<td width=10%>User<td width=10%>Nick<td width=5%>Solved<td width=5%>Penalty
 <?php
 for ($i=0;$i<$pid_cnt;$i++)

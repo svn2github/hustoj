6ae8bb72799dda611b235002f438226da07b0055
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-05-26 23:55:45 +0000

    click
    git-svn-id: http://hustoj.googlecode.com/svn@1630 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/lang/cn.php
+++ trunk/web/lang/cn.php
@@ -26,8 +26,10 @@
 	$MSG_Time_Limit_Exceed="时间超限";
 	$MSG_Memory_Limit_Exceed="内存超限";
 	$MSG_Output_Limit_Exceed="输出超限";
-	$MSG_Runtime_Error="运行错误(点击看详细)";
-	$MSG_Compile_Error="编译错误（点击看详细）";
+	$MSG_Runtime_Error="运行错误";
+	$MSG_Compile_Error="编译错误";
+	$MSG_Runtime_Click="运行错误(点击看详细)";
+	$MSG_Compile_Click="编译错误（点击看详细）";
 	$MSG_Compile_OK="编译成功";
 	//fool's day
 if(date('m')==4&&date('d')==1&&rand(0,100)<10){

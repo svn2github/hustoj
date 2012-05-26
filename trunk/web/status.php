3dc5ecf353f80f2d2a739eecf45c19b5855178ce
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-05-26 23:57:26 +0000

    clicks
    git-svn-id: http://hustoj.googlecode.com/svn@1631 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -175,9 +175,9 @@ else
 
        
         if (intval($row['result'])==11 && ((isset($_SESSION['user_id'])&&$row['user_id']==$_SESSION['user_id']) || isset($_SESSION['source_browser']))){
-                $view_status[$i][3]= "<a href='ceinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$judge_result[$row['result']]."</a>";
+                $view_status[$i][3]= "<a href='ceinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$MSG_Compile_Click."</a>";
         }else if (intval($row['result'])==10 && ((isset($_SESSION['user_id'])&&$row['user_id']==$_SESSION['user_id']) || isset($_SESSION['source_browser']))){
-                $view_status[$i][3]= "<a href='reinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$judge_result[$row['result']]."</a>";
+                $view_status[$i][3]= "<a href='reinfo.php?sid=".$row['solution_id']."' class=".$judge_color[$row['result']].">".$MSG_Runtime_Click."</a>";
 
         }else{
 

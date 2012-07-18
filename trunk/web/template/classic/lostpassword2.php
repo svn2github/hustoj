76815c2162000481fc83e08b78a6d3ca8f2bc111
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-07-18 01:41:26 +0000

    remove test line
    git-svn-id: http://hustoj.googlecode.com/svn@1667 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/classic/lostpassword2.php
+++ trunk/web/template/classic/lostpassword2.php
@@ -12,7 +12,7 @@
         <center>
         <table width=400 algin=center>
         <tr><td width=200><?php echo $MSG_USER_ID?>:<td width=200><input name="user_id" type="text" size=20></tr>
-        <tr><td>Key in your <?php echo $MSG_EMAIL?>:<td><input name="lost_key" type="text" size=20><?php echo $_SESSION['lost_key']?></tr>
+        <tr><td>Key in your <?php echo $MSG_EMAIL?>:<td><input name="lost_key" type="text" size=20></tr>
         <?php if($OJ_VCODE){?>
                 <tr><td><?php echo $MSG_VCODE?>:</td>
                         <td><input name="vcode" size=4 type=text><img alt="click to change" src=vcode.php onclick="this.src='vcode.php#'+Math.random()">*</td>

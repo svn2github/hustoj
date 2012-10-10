1e992a442fc8fe4b940dccd43064154a1b714652
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-10-10 07:56:55 +0000

    mis place java method
    git-svn-id: http://hustoj.googlecode.com/svn@1752 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/classic/ceinfo.php
+++ trunk/web/template/classic/ceinfo.php
@@ -16,6 +16,9 @@
    var i=0;
    var pats=new Array();
    var exps=new Array();
+
+   pats[i]=/class, interface, or enum expected/;
+   exps[i++]="请不要将java函数（方法）放置在类声明外部，注意大括号的结束位置}";
    pats[i]=/asm.*java/;
    exps[i++]="请不要将java程序提交为C语言";
    pats[i]=/package .* does not exist/;

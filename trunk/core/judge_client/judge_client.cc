1611036c6688156398831b0c27b96da6df5d6fcb
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-11-01 23:40:44 +0000

    mis slash
    git-svn-id: http://hustoj.googlecode.com/svn@1772 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1015,7 +1015,7 @@ void prepare_files(char * filename, int namelen, char * infile, int & p_id,
         strncpy(fname, filename, namelen);
         fname[namelen] = 0;
         sprintf(infile, "%s/data/%d/%s.in", oj_home, p_id, fname);
-        execute_cmd("cp %s %sdata.in", infile, work_dir);
+        execute_cmd("cp %s %s/data.in", infile, work_dir);
         execute_cmd("cp %s/data/%d/*.dic %s/", oj_home, p_id,work_dir);
 
         sprintf(outfile, "%s/data/%d/%s.out", oj_home, p_id, fname);

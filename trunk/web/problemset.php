da44c8a4c7217d4963a20be814de690ca1162646
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-11-07 23:46:38 +0000

    from 1000
    git-svn-id: http://hustoj.googlecode.com/svn@1780 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/problemset.php
+++ trunk/web/problemset.php
@@ -6,7 +6,7 @@
 	require_once('./include/setlang.php');
     $view_title= "Problem Set";
 $first=1000;
-if($OJ_SAE) $first=1;
+  //if($OJ_SAE) $first=1;
 $sql="SELECT max(`problem_id`) as upid FROM `problem`";
 $page_cnt=100;
 $result=mysql_query($sql);

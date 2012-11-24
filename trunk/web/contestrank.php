e14ec92b76527977a13f7e883145d9a4aa729a28
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-11-24 14:16:51 +0000

    default 0
    git-svn-id: http://hustoj.googlecode.com/svn@1820 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/contestrank.php
+++ trunk/web/contestrank.php
@@ -94,6 +94,7 @@ if ($start_time>time()){
         require("template/".$OJ_TEMPLATE."/error.php");
         exit(0);
 }
+if(!isset($OJ_RANK_LOCK_PERCENT)) $OJ_RANK_LOCK_PERCENT=0;
 $lock=$end_time-($end_time-$start_time)*$OJ_RANK_LOCK_PERCENT;
 
 //echo $lock.'-'.date("Y-m-d H:i:s",$lock);

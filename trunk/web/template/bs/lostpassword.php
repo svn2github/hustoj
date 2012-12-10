a00ce4239441e918ec188786baaadd763ef8ab42
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-12-10 12:12:26 +0000

    force vcode
    git-svn-id: http://hustoj.googlecode.com/svn@1904 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/bs/lostpassword.php
+++ trunk/web/template/bs/lostpassword.php
@@ -13,11 +13,11 @@
         <table width=400 algin=center>
         <tr><td width=200><?php echo $MSG_USER_ID?>:<td width=200><input name="user_id" type="text" size=20></tr>
         <tr><td><?php echo $MSG_EMAIL?>:<td><input name="email" type="text" size=20></tr>
-        <?php if($OJ_VCODE){?>
+        
                 <tr><td><?php echo $MSG_VCODE?>:</td>
                         <td><input name="vcode" size=4 type=text><img alt="click to change" src=vcode.php onclick="this.src='vcode.php#'+Math.random()">*</td>
                 </tr>
-                <?php }?>
+                
         <tr><td><td><input name="submit" type="submit" size=10 value="Submit">
 </tr>
         </table>

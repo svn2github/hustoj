070c2bf5c3aec33fc16bfade9168e0d19c76f4c5
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-12-22 04:20:44 +0000

    new team gen with pre given nicks
    git-svn-id: http://hustoj.googlecode.com/svn@1976 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/team_generate.php
+++ trunk/web/admin/team_generate.php
@@ -12,6 +12,8 @@ if (!(isset($_SESSION['administrator']))){
 		exit(0);
 	}
 	$teamnumber=intval($_POST['teamnumber']);
+        $pieces = explode("\n", trim($_POST['ulist']));
+	
 	if ($teamnumber>0){
 		echo "<table border=1>";
 		echo "<tr><td colspan=2>Copy these accounts to distribute</td></tr>";
@@ -20,11 +22,15 @@ if (!(isset($_SESSION['administrator']))){
 			
 			$user_id=$prefix.$i;
 			$password=strtoupper(substr(MD5($user_id.rand(0,9999999)),0,10));
-			echo "<tr><td>$user_id</td><td>$password</td></tr>";
+                        if(isset($pieces[$i-1]))
+                        	$nick=$pieces[$i-1];
+                        else
+				$nick="your_own_nick";
+			echo "<tr><td>$nick<td>$user_id</td><td>$password</td></tr>";
 			
 			$password=pwGen($password);
 			$email="your_own_email@internet";
-			$nick="your_own_nick";
+                       
 			$school="your_own_school";
 			$sql="INSERT INTO `users`("."`user_id`,`email`,`ip`,`accesstime`,`password`,`reg_time`,`nick`,`school`)"."VALUES('".$user_id."','".$email."','".$_SERVER['REMOTE_ADDR']."',NOW(),'".$password."',NOW(),'".$nick."','".$school."')on DUPLICATE KEY UPDATE `email`='".$email."',`ip`='".$_SERVER['REMOTE_ADDR']."',`accesstime`=NOW(),`password`='".$password."',`reg_time`=now(),nick='".$nick."',`school`='".$school."'";
 			mysql_query($sql) or die(mysql_error());
@@ -41,7 +47,8 @@ if (!(isset($_SESSION['administrator']))){
 	<form action='team_generate.php' method=post>
 	    Prefix:<input type='test' name='prefix' value='team'>
 		Generate<input type=input name='teamnumber' value=50>Teams.
-		<input type=submit value=Generate>
+		<input type=submit value=Generate><br>
+                Users:<textarea name="ulist" rows="20" cols="20"><?php if (isset($ulist)) { echo $ulist; } ?></textarea>
 		<?php require_once("../include/set_post_key.php");?>
 	</form>
 

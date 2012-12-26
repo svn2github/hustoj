8b0e8830f530d90be06e0373790684945cbe5ffd
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-12-26 08:10:00 +0000

    fix
    git-svn-id: http://hustoj.googlecode.com/svn@1992 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1405,7 +1405,7 @@ void judge_solution(int & ACflg, int & usedtime, int time_lmt, int isspj,
         //usedtime-=1000;
         int comp_res;
         if(!oi_mode) num_of_test=1.0;
-        if (ACflg == OJ_AC && usedtime > time_lmt * 1000*(num_of_tes))
+        if (ACflg == OJ_AC && usedtime > time_lmt * 1000*(num_of_test))
                 ACflg = OJ_TL;
         if(topmemory>mem_lmt * STD_MB) ACflg=OJ_ML; //issues79
         // compare

98eadf97da245005ea639cc4d9c386b7ba8ef6f1
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-12-26 08:45:45 +0000

    flot
    git-svn-id: http://hustoj.googlecode.com/svn@1994 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/bs/conteststatistics.php
+++ trunk/web/template/bs/conteststatistics.php
@@ -13,6 +13,47 @@ $(document).ready(function()
     } 
 ); 
 </script>
+
+<script language="javascript" type="text/javascript" src="include/jquery-latest.js"></script>
+    <script language="javascript" type="text/javascript" src="include/jquery.flot.js"></script>
+    <script type="text/javascript">
+$(function () {
+    var d1 = [];
+    var d2 = [];
+    <?php
+       foreach($chart_data_all as $k=>$d){
+    ?>
+        d1.push([<?php echo $k?>, <?php echo $d?>]);
+        <?php }?>
+    <?php
+       foreach($chart_data_ac as $k=>$d){
+    ?>
+        d2.push([<?php echo $k?>, <?php echo $d?>]);
+        <?php }?>
+          //var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+    // a null signifies separate line segments
+    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+   
+  $.plot($("#submission"), [
+    {label:"<?php echo $MSG_SUBMIT?>",data:d1,lines: { show: true }},
+    {label:"<?php echo $MSG_AC?>",data:d2,bars:{show:true}} ],{
+   
+       
+            xaxis: {
+              mode: "time"
+              //,    max:(new Date()).getTime()
+              //,min:(new Date()).getTime()-100*24*3600*1000
+            },
+        grid: {
+            backgroundColor: { colors: ["#fff", "#333"] }
+        }
+        });
+});
+      //alert((new Date()).getTime());
+</script>
+
+
 </head>
 <body>
 <div id="wrapper">
@@ -47,7 +88,10 @@ for ($j=0;$j<15;$j++) {
 echo "</tr>";
 ?>
   </tbody>
-<table></center>;
+<table>
+<div id=submission style="width:600px;height:300px" ></div>
+
+</center>;
 
 <div id=foot>
 	<?php require_once("oj-footer.php");?>

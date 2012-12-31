ddbaad3d9eaef39be031a22c453cd696150f7822
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2012-12-31 11:33:27 +0000

    prefatch
    git-svn-id: http://hustoj.googlecode.com/svn@2016 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/bs/problem.php
+++ trunk/web/template/bs/problem.php
@@ -3,6 +3,15 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php echo $view_title?></title>
 	<link rel=stylesheet href='./template/<?php echo $OJ_TEMPLATE?>/<?php echo isset($OJ_CSS)?$OJ_CSS:"hoj.css" ?>' type='text/css'>
+     <link rel="next" href="submitpage.php?<?php
+        
+        if ($pr_flag){
+		echo "id=$id";
+	}else{
+		echo "cid=$cid&pid=$pid&langmask=$langmask";
+	}
+        
+        ?>">
 </head>
 <body>
 <div id="wrapper">

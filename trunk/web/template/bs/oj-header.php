c7dc1688d664cebc7edb96c84f9de8238ad2410f
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-01-03 02:47:35 +0000

    icons
    git-svn-id: http://hustoj.googlecode.com/svn@2022 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/template/bs/oj-header.php
+++ trunk/web/template/bs/oj-header.php
@@ -1,45 +1,40 @@
 <div id=head>
-<h2><img id=logo src="<?php echo "template/".$OJ_TEMPLATE?>/image/logo.png"><span class="red">Welcome To <?php echo $OJ_NAME?> Online Judge</span></h2>
+<h2><img id=logo src="<?php echo "template/".$OJ_TEMPLATE?>/image/logo.png"><span class="red"><?php echo $OJ_NAME?></span></h2>
 </div><!--end head-->
 <div id=subhead>
-	  <div id=menu>
-	    <div class='btn' >
-		<a  href="<?php echo $OJ_HOME?>"><?php if ($url=="JudgeOnline") echo "<span class='_selected'>";?>
-								<?php echo $MSG_HOME?>
-								<?php if ($url=="JudgeOnline") echo "</span>";?>
+	  <div id=menu class=navbar>
+	  <?php $ACTIVE="btn-warning";?>
+		<a  class='btn'  href="<?php echo $OJ_HOME?>"><i class="icon-home"></i>
+		<?php echo $MSG_HOME?>						
 		</a>
-		</div>
-		<div class='btn' >
-		<a  href="bbs.php"><?php if ($url==$OJ_BBS.".php") echo "<span class='_selected'>";?>
-		<?php echo $MSG_BBS?><?php if ($url==$OJ_BBS.".php") echo "</span>";?></a>
-		</div>
-		<div class='btn' >
-		<a  href="problemset.php"><?php if ($url=="problemset.php") echo "<span class='_selected'>";?>
-		<?php echo $MSG_PROBLEMS?><?php if ($url=="problemset.php") echo "</span>";?></a>
-		</div>
-		<div class='btn' >
-		<a  href="status.php"><?php if ($url=="status.php") echo "<span class='_selected'>";?>
-		<?php echo $MSG_STATUS?><?php if ($url=="status.php") echo "</span>";?></a>
-		</div>
-		<div class='btn' >
-		<a  href="ranklist.php"><?php if ($url=="ranklist.php") echo "<span class='_selected'>";?>
-		<?php echo $MSG_RANKLIST?><?php if ($url=="ranklist.php") echo "</span>";?></a>
-		</div>
-		<div class='btn' >
-		<a  href="contest.php"><?php if ($url=="contest.php") echo "<span class='_selected'>";?>
-		<?php echo checkcontest($MSG_CONTEST)?><?php if ($url=="contest.php") echo "</span>";?></a>
-		</div>
-        <div class='btn' >
-		<a  href="recent-contest.php"><?php if ($url=="recent-contest.php") echo "<span class='_selected'>";?>
-		<?php echo $MSG_RECENT_CONTEST?><?php if ($url=="recent-contest.php") echo "</span>";?></a>
-		</div>
-		<div class='btn' ><?php if ($url==isset($OJ_FAQ_LINK)?$OJ_FAQ_LINK:"faqs.php") echo "<span class='_selected'>";?>
-		<a  href="<?php echo isset($OJ_FAQ_LINK)?$OJ_FAQ_LINK:"faqs.php"?>"><?php echo $MSG_FAQ?><?php if ($url==isset($OJ_FAQ_LINK)?$OJ_FAQ_LINK:"faqs.php") echo "</span>";?></a>
-		</div>
+		
+		<a  class='btn <?php if ($url==$OJ_BBS.".php") echo " $ACTIVE";?>'  href="bbs.php">
+		<i class="icon-comment"></i><?php echo $MSG_BBS?></a>
+		<a  class='btn <?php if ($url=="problemset.php") echo " $ACTIVE";?>' href="problemset.php">
+		<i class="icon-question-sign"></i><?php echo $MSG_PROBLEMS?></a>
+		
+	  <!-- <a  class='btn <?php if ($url=="submitpage.php") echo " $ACTIVE";?>' href="submitpage.php">
+		<i class="icon-pencil"></i><?php echo "编辑器"?></a>
+		-->
+		<a  class='btn <?php if ($url=="status.php") echo "  $ACTIVE";?>' href="status.php">
+		<i class="icon-check"></i><?php echo $MSG_STATUS?></a>
+		
+		<a class='btn <?php if ($url=="ranklist.php") echo "  $ACTIVE";?>' href="ranklist.php">
+		<i class="icon-signal"></i><?php echo $MSG_RANKLIST?></a>
+		
+		<a class='btn <?php if ($url=="contest.php") echo "  $ACTIVE";?>'  href="contest.php">
+		<i class="icon-fire"></i><?php echo checkcontest($MSG_CONTEST)?></a>
+		
+		<a class='btn <?php if ($url=="recent-contest.php") echo " $ACTIVE";?>' href="recent-contest.php">
+		<i class="icon-share"></i><?php echo "$MSG_RECENT_CONTEST"?></a>
+		
+		<a class='btn <?php if ($url==(isset($OJ_FAQ_LINK)?$OJ_FAQ_LINK:"faqs.php")) echo " $ACTIVE";?>' href="<?php echo isset($OJ_FAQ_LINK)?$OJ_FAQ_LINK:"faqs.php"?>">
+                <i class="icon-info-sign"></i><?php echo "$MSG_FAQ"?></a>
+		
 		<?php if(isset($OJ_DICT)&&$OJ_DICT&&$OJ_LANG=="cn"){?>
-					  <div class='btn' >
-							  <span  style="color:1a5cc8" id="dict_status"></span>
-					  </div>
+					  
+		<span div class='btn '  style="color:1a5cc8" id="dict_status"></span>
+					 
 					  <script src="include/underlineTranslation.js" type="text/javascript"></script>
 					  <script type="text/javascript">dictInit();</script>
 		<?php }?>
@@ -48,8 +43,9 @@
 <script src="include/profile.php?<?php echo rand();?>" ></script>
 </div><!--end profile-->
 </div><!--end subhead-->
-<div id=broadcast>
+<div id=broadcast class="container span8 offset2">
 <marquee id=broadcast scrollamount=1 direction=up scrolldelay=250 onMouseOver='this.stop()' onMouseOut='this.start()';>
   <?php echo $view_marquee_msg?>
 </marquee>
 </div>
+<br>

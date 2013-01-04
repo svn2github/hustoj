ffec6c2a9662bfaaecafa050bd7abd684a153913
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-01-04 05:04:26 +0000

    bootstrap admin menu
    git-svn-id: http://hustoj.googlecode.com/svn@2025 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/menu.php
+++ trunk/web/admin/menu.php
@@ -15,90 +15,90 @@
 <hr>
 <ol>
 	<li>
-		<a href="watch.php" target="main"><b><?php echo $MSG_SEEOJ?></b></a>
+		<a class='btn btn-primary' href="watch.php" target="main"><b><?php echo $MSG_SEEOJ?></b></a>
 <?php if (isset($_SESSION['administrator'])){
 	?>
 	<li>
-		<a href="news_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_NEWS?></b></a>
+		<a class='btn btn-primary' href="news_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_NEWS?></b></a>
 	<li>
-		<a href="news_list.php" target="main"><b><?php echo $MSG_NEWS.$MSG_LIST?></b></a>
+		<a class='btn btn-primary' href="news_list.php" target="main"><b><?php echo $MSG_NEWS.$MSG_LIST?></b></a>
 		
 <?php }
 if (isset($_SESSION['administrator'])||isset($_SESSION['problem_editor'])){
 ?>
 	<li>
-		<a href="problem_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_PROBLEM?></b></a>
+		<a class='btn btn-primary' href="problem_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_PROBLEM?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])||isset($_SESSION['contest_creator'])||isset($_SESSION['problem_editor'])){
 ?>
 	<li>
-		<a href="problem_list.php" target="main"><b><?php echo $MSG_PROBLEM.$MSG_LIST?></b></a>
+		<a class='btn btn-primary' href="problem_list.php" target="main"><b><?php echo $MSG_PROBLEM.$MSG_LIST?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])||isset($_SESSION['contest_creator'])){
 ?>		
 <li>
-	<a href="contest_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_CONTEST?></b></a>
+	<a class='btn btn-primary' href="contest_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_CONTEST?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])||isset($_SESSION['contest_creator'])){
 ?>
 <li>
-	<a href="contest_list.php" target="main"><b><?php echo $MSG_CONTEST.$MSG_LIST?></b></a>
+	<a class='btn btn-primary' href="contest_list.php" target="main"><b><?php echo $MSG_CONTEST.$MSG_LIST?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?>
 <li>
-	<a href="team_generate.php" target="main"><b><?php echo $MSG_TEAMGENERATOR?></b></a>
+	<a class='btn btn-primary' href="team_generate.php" target="main"><b><?php echo $MSG_TEAMGENERATOR?></b></a>
 <li>
-	<a href="setmsg.php" target="main"><b><?php echo $MSG_SETMESSAGE?></b></a>
+	<a class='btn btn-primary' href="setmsg.php" target="main"><b><?php echo $MSG_SETMESSAGE?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])||isset( $_SESSION['password_setter'] )){
 ?><li>
-	<a href="changepass.php" target="main"><b><?php echo $MSG_SETPASSWORD?></b></a>
+	<a class='btn btn-primary' href="changepass.php" target="main"><b><?php echo $MSG_SETPASSWORD?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="rejudge.php" target="main"><b><?php echo $MSG_REJUDGE?></b></a>
+	<a class='btn btn-primary' href="rejudge.php" target="main"><b><?php echo $MSG_REJUDGE?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="privilege_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_PRIVILEGE?></b></a>
+	<a class='btn btn-primary' href="privilege_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_PRIVILEGE?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="privilege_list.php" target="main"><b><?php echo $MSG_PRIVILEGE.$MSG_LIST?></b></a>
+	<a class='btn btn-primary' href="privilege_list.php" target="main"><b><?php echo $MSG_PRIVILEGE.$MSG_LIST?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="source_give.php" target="main"><b><?php echo $MSG_GIVESOURCE?></b></a>
+	<a class='btn btn-primary' href="source_give.php" target="main"><b><?php echo $MSG_GIVESOURCE?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="problem_export.php" target="main"><b><?php echo $MSG_EXPORT.$MSG_PROBLEM?></b></a>
+	<a class='btn btn-primary' href="problem_export.php" target="main"><b><?php echo $MSG_EXPORT.$MSG_PROBLEM?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="problem_import.php" target="main"><b><?php echo $MSG_IMPORT.$MSG_PROBLEM?></b></a>
+	<a class='btn btn-primary' href="problem_import.php" target="main"><b><?php echo $MSG_IMPORT.$MSG_PROBLEM?></b></a>
 <?php }
 if (isset($_SESSION['administrator'])){
 ?><li>
-	<a href="update_db.php" target="main"><b><?php echo $MSG_UPDATE_DATABASE?></b></a>
+	<a class='btn btn-primary' href="update_db.php" target="main"><b><?php echo $MSG_UPDATE_DATABASE?></b></a>
 <?php }
 if (isset($OJ_ONLINE)&&$OJ_ONLINE){
 ?><li>
-	<a href="../online.php" target="main"><b><?php echo $MSG_ONLINE?></b></a>
+	<a class='btn btn-primary' href="../online.php" target="main"><b><?php echo $MSG_ONLINE?></b></a>
 <?php }
 ?>
 
 <li>
-	<a href="http://code.google.com/p/hustoj/" target="main"><b>HUSTOJ</b></a>
+	<a class='btn btn-primary' href="http://code.google.com/p/hustoj/" target="main"><b>HUSTOJ</b></a>
 <li>
-	<a href="http://code.google.com/p/freeproblemset/" target="main"><b>FreeProblemSet</b></a>
+	<a class='btn btn-primary' href="http://code.google.com/p/freeproblemset/" target="main"><b>FreeProblemSet</b></a>
 
 </ol>
 <?php if (isset($_SESSION['administrator'])&&!$OJ_SAE){
 ?>
-	<a href="problem_copy.php" target="main" title="Create your own data"><font color="eeeeee">CopyProblem</font></a> <br>
-	<a href="problem_changeid.php" target="main" title="Danger,Use it on your own risk"><font color="eeeeee">ReOrderProblem</font></a>
+	<a class='btn btn-primary' href="problem_copy.php" target="main" title="Create your own data"><font color="eeeeee">CopyProblem</font></a> <br>
+	<a class='btn btn-primary' href="problem_changeid.php" target="main" title="Danger,Use it on your own risk"><font color="eeeeee">ReOrderProblem</font></a>
 <?php }
 ?>
 </body>

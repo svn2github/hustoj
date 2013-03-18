bfb83f92fd1fbec33f782f85ab3fd2ea89f20e6b
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-03-18 02:33:25 +0000

    msg
    git-svn-id: http://hustoj.googlecode.com/svn@2048 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/lang/ko.php
+++ trunk/web/lang/ko.php
@@ -107,5 +107,11 @@
      //someone please translate these
   $MSG_PRIVATE_WARNING="This is a private contest which you don't have privilege。";
   $MSG_WATCH_RANK="Click HERE to watch contest rank.";
+  $MSG_Public="Public";
+  $MSG_Private="Private";
+  $MSG_Running="Running";
+  $MSG_Start="Start";
+  $MSG_TotalTime="Total";
+  $MSG_LeftTime="Left";
 
 ?>

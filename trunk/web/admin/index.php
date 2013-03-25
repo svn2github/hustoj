b0d3c61bd6805907001391b75e6db1bf4295f317
slixurd <fatearcadia@gmail.com>
2013-03-25 18:38:56 +0800

    初步修改主页,menu.剩下的页面等待修改.主页等待增加默认界面.

--- trunk/web/admin/index.php
+++ trunk/web/admin/index.php
@@ -1,12 +1,99 @@
+<?php require_once("admin-header.php");
+	if(isset($OJ_LANG)){
+		require_once("../lang/$OJ_LANG.php");
+	}
+?>
+
 <html>
 <head>
-<title>JudgeOnline Administration</title>
+<title>OJ Administration</title>
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Content-Language" content="zh-cn">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel=stylesheet href='admin.css' type='text/css'>
 </head>
-<frameset cols="20,90">
-<frame name="menu" src="menu.php">
-<frame name="main" src="about:blank">
-<noframes>
+<body>
 
-</noframes>
-</frameset>
+
+
+<div class="container-fluid">
+<div class="navbar">
+ 	<div class="navbar-inner navbar-fixed-top" style="visibility: visible; position: fixed;">
+ 		<a  class='brand'  href="<?php echo $OJ_HOME?>">
+			ADMIN PANEL					
+		</a>
+</div>
+</div>
+	<div class="row-fluid top-space">
+		<div class="span2">
+			<div class="menu-group">
+				<?php require_once("menu.php") ?>
+			</div>
+		</div>
+		<div class="span10">
+			<?php if (isset($_SESSION['administrator'])){
+				if(isset($_POST['news_add'])){
+					require("news_add_page.php");
+				}
+				if(isset($_POST['news_list'])){
+					require("news_list.php");
+				}
+				if(isset($_POST['problem_add_page'])){
+					require("problem_add_page.php");
+				}
+				if(isset($_POST['problem_list'])){
+					require("problem_list.php");
+				}	
+				if(isset($_POST['contest_add'])){
+					require("contest_add.php");
+				}	
+				if(isset($_POST['contest_list'])){
+					require("contest_list.php");
+				}	
+				if(isset($_POST['team_generate'])){
+					require("team_generate.php");
+				}	
+				if(isset($_POST['setmsg'])){
+					require("setmsg.php");
+				}	
+				if(isset($_POST['changepass'])){
+					require("changepass.php");
+				}	
+				if(isset($_POST['rejudge'])){
+					require("rejudge.php");
+				}	
+				if(isset($_POST['privilege_add'])){
+					require("privilege_add.php");
+				}	
+				if(isset($_POST['privilege_list'])){
+					require("privilege_list.php");
+				}	
+				if(isset($_POST['source_give'])){
+					require("source_give.php");
+				}	
+				if(isset($_POST['problem_export'])){
+					require("problem_export.php");
+				}	
+				if(isset($_POST['problem_import'])){
+					require("problem_import.php");
+				}	
+				if(isset($_POST['update_db'])){
+					require("update_db.php");
+				}	
+				if(isset($_POST['online'])){
+					require("../online.php");
+				}	
+				if(isset($_POST['problem_copy'])){
+					require("problem_copy.php");
+				}	
+				if(isset($_POST['problem_changeid'])){
+					require("problem_changeid.php");
+				}	
+
+			}
+			?>
+		</div>
+
+</body>
 </html>
--- trunk/web/admin/problem_add_page.php
+++ trunk/web/admin/problem_add_page.php
@@ -1,13 +1,3 @@
-<html>
-<head>
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache">
-<meta http-equiv="Content-Language" content="zh-cn">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>New Problem</title>
-</head>
-<body leftmargin="30" >
-
 <?php require_once("../include/db_info.inc.php");?>
 <?php require_once("admin-header.php");
 if (!(isset($_SESSION['administrator'])||isset($_SESSION['problem_editor']))){
@@ -102,6 +92,4 @@ if (mysql_num_rows($result)==0){
 <input type=submit value=Submit name=submit>
 </div></form>
 <p>
-<?php require_once("../oj-footer.php");?>
-</body></html>
 

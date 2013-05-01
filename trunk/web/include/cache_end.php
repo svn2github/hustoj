2ac3a88089cca171b109e80d5cc59c6de7798556
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-05-01 13:50:20 +0000

    cache files might cause file system out of inode
    git-svn-id: http://hustoj.googlecode.com/svn@2070 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/include/cache_end.php
+++ trunk/web/include/cache_end.php
@@ -4,8 +4,8 @@
                         if($OJ_MEMCACHE){
                                 $mem->set($file,ob_get_contents(),0,$cache_time);
                         }else{
-                                if(!file_exists("cache")) mkdir("cache");
-                                file_put_contents($file,ob_get_contents());
+                          // if(!file_exists("cache")) mkdir("cache");
+                          //      file_put_contents($file,ob_get_contents());
                         }
                 }
         //cache foot stop

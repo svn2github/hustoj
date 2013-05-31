cac35f30e67d67de6ca1f03645facfaba5fc2092
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-05-31 07:56:19 +0000

    2GB should works
    git-svn-id: http://hustoj.googlecode.com/svn@2085 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -822,8 +822,8 @@ int compile(int lang) {
                 LIM.rlim_cur = 900 * STD_MB;
                 setrlimit(RLIMIT_FSIZE, &LIM);
 
-                LIM.rlim_max =  STD_MB<<12;
-                LIM.rlim_cur =  STD_MB<<12;
+                LIM.rlim_max =  STD_MB<<11;
+                LIM.rlim_cur =  STD_MB<<11;
                 setrlimit(RLIMIT_AS, &LIM);
                 if (lang != 2) {
                         freopen("ce.txt", "w", stderr);

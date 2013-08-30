1eabefa9c494a6ebf1c9aaf5d3d2e2e5936146ed
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-08-30 04:59:23 +0000

    查找
    git-svn-id: http://hustoj.googlecode.com/svn@2136 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/admin/contest_list.php
+++ trunk/web/admin/contest_list.php
@@ -21,35 +21,42 @@ for ($i=1;$i<=$cnt;$i++){
         else echo "<a href='contest_list.php?page=".$i."'>".$i."</a>";
 }
 $sql="select `contest_id`,`title`,`start_time`,`end_time`,`private`,`defunct` FROM `contest` where contest_id>=$pstart and contest_id <=$pend order by `contest_id` desc";
-
+$keyword=$_GET['keyword'];
+$keyword=mysql_real_escape_string($keyword);
+if($keyword) $sql="select `contest_id`,`title`,`start_time`,`end_time`,`private`,`defunct` FROM `contest` where title like '%$keyword%' ";
 $result=mysql_query($sql) or die(mysql_error());
+?>
+<form action=contest_list.php>关键词<input name=keyword><input type=submit value=查找><form>
+
+
+<?php
 echo "<center><table width=90% border=1>";
-echo "<tr><td>ContestID<td>Title<td>StartTime<td>EndTime<td>Private<td>Status<td>Edit<td>Copy<td>Export<td>Logs"; 
+echo "<tr><td>ContestID<td>Title<td>StartTime<td>EndTime<td>Private<td>Status<td>Edit<td>Copy<td>Export<td>Logs";
 echo "</tr>";
 for (;$row=mysql_fetch_object($result);){
-	echo "<tr>";
-	echo "<td>".$row->contest_id;
-	echo "<td><a href='../contest.php?cid=$row->contest_id'>".$row->title."</a>";
-	echo "<td>".$row->start_time;
-	echo "<td>".$row->end_time;
-	$cid=$row->contest_id;
-	if(isset($_SESSION['administrator'])||isset($_SESSION["m$cid"])){
-		echo "<td><a href=contest_pr_change.php?cid=$row->contest_id&getkey=".$_SESSION['getkey'].">".($row->private=="0"?"Public->Private":"Private->Public")."</a>";
-		echo "<td><a href=contest_df_change.php?cid=$row->contest_id&getkey=".$_SESSION['getkey'].">".($row->defunct=="N"?"<span class=green>Available</span>":"<span class=red>Reserved</span>")."</a>";
-		echo "<td><a href=contest_edit.php?cid=$row->contest_id>Edit</a>";
-		echo "<td><a href=contest_add.php?cid=$row->contest_id>Copy</a>";
-		if(isset($_SESSION['administrator'])){
-			echo "<td><a href=\"problem_export_xml.php?cid=$row->contest_id&getkey=".$_SESSION['getkey']."\">Export</a>";
-		}else{
-		  echo "<td>";
-		}
+        echo "<tr>";
+        echo "<td>".$row->contest_id;
+        echo "<td><a href='../contest.php?cid=$row->contest_id'>".$row->title."</a>";
+        echo "<td>".$row->start_time;
+        echo "<td>".$row->end_time;
+        $cid=$row->contest_id;
+        if(isset($_SESSION['administrator'])||isset($_SESSION["m$cid"])){
+                echo "<td><a href=contest_pr_change.php?cid=$row->contest_id&getkey=".$_SESSION['getkey'].">".($row->private=="0"?"Public->Private":"Private->Public")."</a>";
+                echo "<td><a href=contest_df_change.php?cid=$row->contest_id&getkey=".$_SESSION['getkey'].">".($row->defunct=="N"?"<span class=green>Available</span>":"<span class=red>Reserved</span>")."</a>";
+                echo "<td><a href=contest_edit.php?cid=$row->contest_id>Edit</a>";
+                echo "<td><a href=contest_add.php?cid=$row->contest_id>Copy</a>";
+                if(isset($_SESSION['administrator'])){
+                        echo "<td><a href=\"problem_export_xml.php?cid=$row->contest_id&getkey=".$_SESSION['getkey']."\">Export</a>";
+                }else{
+                  echo "<td>";
+                }
      echo "<td> <a href=\"../export_contest_code.php?cid=$row->contest_id&getkey=".$_SESSION['getkey']."\">Logs</a>";
-	}else{
-		echo "<td colspan=5 align=right><a href=contest_add.php?cid=$row->contest_id>Copy</a><td>";
-		
-	}
-		
-	echo "</tr>";
+        }else{
+                echo "<td colspan=5 align=right><a href=contest_add.php?cid=$row->contest_id>Copy</a><td>";
+
+        }
+
+        echo "</tr>";
 }
 echo "</table></center>";
 require("../oj-footer.php");

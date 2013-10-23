7f6523e31dba17e413daa012f34b6eec1f77b350
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2013-10-23 07:42:34 +0000

    flex
    git-svn-id: http://hustoj.googlecode.com/svn@2204 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/install/install-interactive.sh
+++ trunk/install/install-interactive.sh
@@ -33,7 +33,7 @@ HTTP_START="sudo /etc/init.d/apache2 restart"
 
 else
 sudo yum -y update
-sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static
+sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static flex
 sudo /etc/init.d/mysqld start
 WEBBASE=/var/www/html
 APACHEUSER=apache

dc0de66edaec540893a141078c0c37f23482debe
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2014-03-31 02:04:26 +0000

    schema
    git-svn-id: http://hustoj.googlecode.com/svn@2236 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/okcalls.h
+++ trunk/core/judge_client/okcalls.h
@@ -100,6 +100,10 @@ HOJ_MAX_LIMIT,
 HOJ_MAX_LIMIT,
 HOJ_MAX_LIMIT,
 0};
+int LANG_SV[256]={100,295,SYS_fcntl64,SYS_getdents64 , SYS_ugetrlimit, SYS_rt_sigprocmask, SYS_futex, SYS_read, SYS_mmap2, SYS_stat64, SYS_open, SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_readlink, SYS_munmap, SYS_close, SYS_uname, SYS_clone, SYS_uname, SYS_mprotect, SYS_rt_sigaction, SYS_sigprocmask, SYS_getrlimit, SYS_fstat64, SYS_getuid32, SYS_getgid32, SYS_geteuid32, SYS_getegid32, SYS_set_thread_area, SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 0};
+int LANG_SC[256]={HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,              HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,                 HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,       HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,       HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,          HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,      HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,               HOJ_MAX_LIMIT,              HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,          HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,              0};
+
+
 #else
 
   
@@ -198,5 +202,7 @@ HOJ_MAX_LIMIT,
 HOJ_MAX_LIMIT,
 HOJ_MAX_LIMIT,
 0};
+int LANG_SV[256]={1,23,100,61,22,6,33,8,13,16,111,110,39,79,SYS_fcntl,SYS_getdents64 , SYS_getrlimit, SYS_rt_sigprocmask, SYS_futex, SYS_read, SYS_mmap, SYS_stat, SYS_open, SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_readlink, SYS_munmap, SYS_close, SYS_uname, SYS_clone, SYS_uname, SYS_mprotect, SYS_rt_sigaction, SYS_getrlimit, SYS_fstat, SYS_getuid, SYS_getgid, SYS_geteuid, SYS_getegid, SYS_set_thread_area, SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group,158, 0};
+int LANG_SC[256]={HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,              HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,                 HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,       HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,       HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,          HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,      HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,         HOJ_MAX_LIMIT,        HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,                             HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,          HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,           HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,            HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,                  HOJ_MAX_LIMIT,HOJ_MAX_LIMIT,              0};
 
 #endif

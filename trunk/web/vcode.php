fcb1ef673221552cfcdc25181ee3c00b5b07f38a
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2014-04-14 12:42:47 +0000

    stop cache
    git-svn-id: http://hustoj.googlecode.com/svn@2241 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/web/vcode.php
+++ trunk/web/vcode.php
@@ -22,6 +22,11 @@
 	}
 	
 	session_start();
+   header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pramga: no-cache");
+
 	
 	/* 输出图片类型，字符长度，类型 */
 	$imgtype = 'gif';

561f4b0b298342ac865e3fd94f1b27fa659e830c
newsclan@gmail.com <newsclan@gmail.com@19ab8b60-bd6c-11dd-baed-41e496641fac>
2014-05-10 09:49:43 +0000

    NO C
    git-svn-id: http://hustoj.googlecode.com/svn@2258 19ab8b60-bd6c-11dd-baed-41e496641fac

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -174,41 +174,41 @@ void init_syscalls_limits(int lang) {
                         call_counter[i] = 0;
                 }
         }else if (lang <= 1) { // C & C++
-                for (i = 0; LANG_CC[i]; i++) {
+                for (i = 0; LANG_CV[i]; i++) {
                         call_counter[LANG_CV[i]] = HOJ_MAX_LIMIT;
                 }
         } else if (lang == 2) { // Pascal
-                for (i = 0; LANG_PC[i]; i++)
+                for (i = 0; LANG_PV[i]; i++)
                         call_counter[LANG_PV[i]] = HOJ_MAX_LIMIT;
         } else if (lang == 3) { // Java
-                for (i = 0; LANG_JC[i]; i++)
+                for (i = 0; LANG_JV[i]; i++)
                         call_counter[LANG_JV[i]] = HOJ_MAX_LIMIT;
         } else if (lang == 4) { // Ruby
-                for (i = 0; LANG_RC[i]; i++)
+                for (i = 0; LANG_RV[i]; i++)
                         call_counter[LANG_RV[i]] = HOJ_MAX_LIMIT;
         } else if (lang == 5) { // Bash
-                for (i = 0; LANG_BC[i]; i++)
+                for (i = 0; LANG_BV[i]; i++)
                         call_counter[LANG_BV[i]] = HOJ_MAX_LIMIT;
         }else if (lang == 6) { // Python
-                for (i = 0; LANG_YC[i]; i++)
+                for (i = 0; LANG_YV[i]; i++)
                         call_counter[LANG_YV[i]] = HOJ_MAX_LIMIT;
         }else if (lang == 7) { // php
-            for (i = 0; LANG_PHC[i]; i++)
+            for (i = 0; LANG_PHV[i]; i++)
                     call_counter[LANG_PHV[i]] = HOJ_MAX_LIMIT;
     }else if (lang == 8) { // perl
-            for (i = 0; LANG_PLC[i]; i++)
+            for (i = 0; LANG_PLV[i]; i++)
                     call_counter[LANG_PLV[i]] = HOJ_MAX_LIMIT;
     }else if (lang == 9) { // mono c#
-            for (i = 0; LANG_CSC[i]; i++)
+            for (i = 0; LANG_CSV[i]; i++)
                     call_counter[LANG_CSV[i]] = HOJ_MAX_LIMIT;
     }else if (lang==10){//objective c
-	    for (i = 0; LANG_OC[i]; i++)
+	    for (i = 0; LANG_OV[i]; i++)
                     call_counter[LANG_OV[i]] = HOJ_MAX_LIMIT;
     }else if (lang==11){//free basic
-	    for (i = 0; LANG_BASICC[i]; i++)
+	    for (i = 0; LANG_BASICV[i]; i++)
                     call_counter[LANG_BASICV[i]] = HOJ_MAX_LIMIT;
     }else if (lang==12){//scheme guile
-           for (i = 0; LANG_SC[i]; i++)
+           for (i = 0; LANG_SV[i]; i++)
                     call_counter[LANG_SV[i]] = HOJ_MAX_LIMIT;
     }
 

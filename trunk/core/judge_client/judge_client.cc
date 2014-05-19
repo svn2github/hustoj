10d689372d865dd80a323b27c4e2772c1f9608c1
zhblue <newsclan@gmail.com>
2014-05-19 09:44:20 +0800

    fix ubuntu 14.04 LTS

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -175,41 +175,41 @@ void init_syscalls_limits(int lang) {
 			call_counter[i] = 0;
 		}
 	} else if (lang <= 1) { // C & C++
-		for (i = 0; LANG_CV[i]; i++) {
+		for (i = 0; i==0||LANG_CV[i]; i++) {
 			call_counter[LANG_CV[i]] = HOJ_MAX_LIMIT;
 		}
 	} else if (lang == 2) { // Pascal
-		for (i = 0; LANG_PV[i]; i++)
+		for (i = 0; i==0||LANG_PV[i]; i++)
 			call_counter[LANG_PV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 3) { // Java
-		for (i = 0; LANG_JV[i]; i++)
+		for (i = 0; i==0||LANG_JV[i]; i++)
 			call_counter[LANG_JV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 4) { // Ruby
-		for (i = 0; LANG_RV[i]; i++)
+		for (i = 0; i==0||LANG_RV[i]; i++)
 			call_counter[LANG_RV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 5) { // Bash
-		for (i = 0; LANG_BV[i]; i++)
+		for (i = 0; i==0||LANG_BV[i]; i++)
 			call_counter[LANG_BV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 6) { // Python
-		for (i = 0; LANG_YV[i]; i++)
+		for (i = 0; i==0||LANG_YV[i]; i++)
 			call_counter[LANG_YV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 7) { // php
-		for (i = 0; LANG_PHV[i]; i++)
+		for (i = 0; i==0||LANG_PHV[i]; i++)
 			call_counter[LANG_PHV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 8) { // perl
-		for (i = 0; LANG_PLV[i]; i++)
+		for (i = 0; i==0||LANG_PLV[i]; i++)
 			call_counter[LANG_PLV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 9) { // mono c#
-		for (i = 0; LANG_CSV[i]; i++)
+		for (i = 0; i==0||LANG_CSV[i]; i++)
 			call_counter[LANG_CSV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 10) { //objective c
-		for (i = 0; LANG_OV[i]; i++)
+		for (i = 0; i==0||LANG_OV[i]; i++)
 			call_counter[LANG_OV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 11) { //free basic
-		for (i = 0; LANG_BASICV[i]; i++)
+		for (i = 0; i==0||LANG_BASICV[i]; i++)
 			call_counter[LANG_BASICV[i]] = HOJ_MAX_LIMIT;
 	} else if (lang == 12) { //scheme guile
-		for (i = 0; LANG_SV[i]; i++)
+		for (i = 0; i==0||LANG_SV[i]; i++)
 			call_counter[LANG_SV[i]] = HOJ_MAX_LIMIT;
 	}
 
@@ -1788,7 +1788,7 @@ void watch_solution(pid_t pidApp, char * infile, int & ACflg, int isspj,
 			char error[BUFFER_SIZE];
 			sprintf(error,
 					"[ERROR] A Not allowed system call: runid:%d callid:%ld\n TO FIX THIS , ask admin to add the CALLID into corresponding LANG_XXV[] located at okcalls32/64.h ,and recompile judge_client",
-					solution_id, reg.REG_SYSCALL);
+					solution_id, (long)reg.REG_SYSCALL);
 			write_log(error);
 			print_runtimeerror(error);
 			ptrace(PTRACE_KILL, pidApp, NULL, NULL);

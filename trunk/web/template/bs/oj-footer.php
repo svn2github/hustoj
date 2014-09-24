c4e75be4003b93e94cada834e38160a6652c4c3f
zhblue <newsclan@gmail.com>
2014-09-24 20:41:21 +0800

    googlecode walled

--- trunk/web/template/bs/oj-footer.php
+++ trunk/web/template/bs/oj-footer.php
@@ -34,7 +34,7 @@
 	</script>
 		<br>
 		All Copyright Reserved 2010-2014 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
-		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2014 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2014 <a href='https://github.com/zhblue/hustoj'>HUSTOJ Project</a> TEAM<br>
 		
      <?php if ($OJ_SAE) {
                    echo "<a href=http://sae.sina.com.cn><img bolder=0 src=http://static.sae.sina.com.cn/image/poweredby/poweredby.png></a>";

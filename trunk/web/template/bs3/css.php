4d15d671eae4773b954eb0b353d6a02982cf62da
张浩斌 <newsclan@gmail.com>
2015-01-15 10:44:58 +0800

    local css js

--- trunk/web/template/bs3/css.php
+++ trunk/web/template/bs3/css.php
@@ -1,8 +1,8 @@
 
 <!-- 新 Bootstrap 核心 CSS 文件 -->
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo "template/$OJ_TEMPLATE/"?>bootstrap.min.css">
 
 <!-- 可选的Bootstrap主题文件（一般不用引入） -->
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="<?php echo "template/$OJ_TEMPLATE/"?>bootstrap-theme.min.css">
 
 
--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -1,7 +1,7 @@
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php echo "template/$OJ_TEMPLATE/"?>jquery.min.js"></script>
 
 <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="<?php echo "template/$OJ_TEMPLATE/"?>bootstrap.min.js"></script>
 
 

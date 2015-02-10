e06cac6ea0bed4d45d7b9d82efa1e263f8c8a66e
张浩斌 <newsclan@gmail.com>
2015-02-10 10:09:37 +0800

    ubuntu 14.04 64 freebasic

--- trunk/core/judge_client/okcalls64.h
+++ trunk/core/judge_client/okcalls64.h
@@ -103,7 +103,8 @@ int LANG_OV[256] = { 158, 4, 102, 191, SYS_access, SYS_brk, SYS_close,
 		SYS_set_robust_list, SYS_set_thread_area, SYS_set_tid_address,
 		SYS_uname, SYS_write, 0 };
 
-int LANG_BASICV[256] = { 101, 54, 122, 175, 174, 240, 311, 258, 243, 6, 197,
+int LANG_BASICV[256] = { 0,1,2,3,4,5,9,10,12,13,14,16,21,59,97,158,173,202,218,231,273,
+		101, 54, 122, 175, 174, 240, 311, 258, 243, 6, 197,
 		252, 146, 195, 192, 33, 45, 125, 191, SYS_access, SYS_brk, SYS_close,
 		SYS_execve, SYS_exit_group, SYS_fstat, SYS_futex, SYS_getrlimit,
 		SYS_ioctl, SYS_ioperm, SYS_mmap, SYS_open, SYS_read, SYS_rt_sigaction,

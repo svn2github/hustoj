14dff8800ab1279145e368c991489b57f700765b
张浩斌 <newsclan@gmail.com>
2015-03-13 07:50:13 +0800

    strange defunct switch double click failure

--- trunk/web/include/check_get_key.php
+++ trunk/web/include/check_get_key.php
@@ -1,4 +1,13 @@
 <?php
-if ($_SESSION['getkey']!=$_GET['getkey'])
+if ($_SESSION['getkey']!=$_GET['getkey']){
+?>
+<script language=javascript>
+        history.go(-1);
+</script>
+<?php 
 	exit(1);
+}
+else{
+   unset($_SESSION['getkey']);
+}
 ?>

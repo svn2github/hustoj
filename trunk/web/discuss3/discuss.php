49ae50e1e94c7bb2227304293ca444a738b965d3
张浩斌 <newsclan@gmail.com>
2015-03-14 08:34:48 +0800

    utf8

--- trunk/web/discuss3/discuss.php
+++ trunk/web/discuss3/discuss.php
@@ -91,8 +91,8 @@ for ($i=0;$i<$rows_cnt;$i++){
         if ($row->pid!=0) echo"<a href=\"discuss.php?pid={$row->pid}&cid={$row->cid}\">{$row->pid}</a>";
         echo "</td>";
         echo "<td><a href=\"../userinfo.php?user={$row->author_id}\">{$row->author_id}</a></td>";
-        if($row->cid)echo "<td><a href=\"thread.php?tid={$row->tid}&cid={$row->cid}\">".htmlentities($row->title,ENT_QUOTES)."</a></td>";
-        else echo "<td><a href=\"thread.php?tid={$row->tid}\">".htmlentities($row->title,ENT_QUOTES)."</a></td>";
+        if($row->cid)echo "<td><a href=\"thread.php?tid={$row->tid}&cid={$row->cid}\">".htmlentities($row->title,ENT_QUOTES,"UTF-8")."</a></td>";
+        else echo "<td><a href=\"thread.php?tid={$row->tid}\">".htmlentities($row->title,ENT_QUOTES,"UTF-8")."</a></td>";
         echo "<td>{$row->posttime}</td>";
         echo "<td>{$row->lastupdate}</td>";
         echo "<td>".($row->count-1)."</td>";

82773b094aa0a78ef8c46a4f80a1d47d8835ce22
张浩斌 <newsclan@gmail.com>
2015-03-25 17:24:32 +0800

    upgrade sim

--- trunk/core/make.sh
+++ trunk/core/make.sh
@@ -7,18 +7,18 @@ cd ../judge_client
 make
 chmod +x judge_client
 cp judge_client /usr/bin
-cd ../sim/sim_2_67
-make clean
-make sim_c
-make sim_java
-make sim_pasc
+cd ../sim/sim_2_77
+make fresh
+make exes
 chmod +x sim*
-cp sim_c /usr/bin
-cp sim_java /usr/bin/sim_java
-cp sim_pasc /usr/bin/sim_pas
+cp sim_c.exe /usr/bin/sim_c
+cp sim_java.exe /usr/bin/sim_java
+cp sim_pasc.exe /usr/bin/sim_pas
+cp sim_text.exe /usr/bin/sim_text
 cd ..
 cp sim.sh /usr/bin
 chmod +x /usr/bin/sim.sh
+rm /usr/bin/sim_cc /usr/bin/sim_rb /usr/bin/sim_sh
 ln -s /usr/bin/sim_c /usr/bin/sim_cc
-ln -s /usr/bin/sim_c /usr/bin/sim_rb
-ln -s /usr/bin/sim_c /usr/bin/sim_sh
+ln -s /usr/bin/sim_text /usr/bin/sim_rb
+ln -s /usr/bin/sim_text /usr/bin/sim_sh

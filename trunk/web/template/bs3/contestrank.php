945971892396c7548bd73025eb4845faa65d2dec
张浩斌 <newsclan@gmail.com>
2015-04-16 14:36:37 +0800

    Update contestrank.php

--- trunk/web/template/bs3/contestrank.php
+++ trunk/web/template/bs3/contestrank.php
@@ -58,7 +58,7 @@ $usolved=$U[$i]->solved;
 if(isset($_GET['user_id'])&&$uuid==$_GET['user_id']) echo "<td bgcolor=#ffff77>";
 else echo"<td>";
 echo "<a name=\"$uuid\" href=userinfo.php?user=$uuid>$uuid</a>";
-echo "<td><a href=userinfo.php?user=$uuid>".$U[$i]->nick."</a>";
+echo "<td><a href=userinfo.php?user=$uuid>".htmlentities($U[$i]->nick,ENT_QUOTES,"UTF-8")."</a>";
 echo "<td><a href=status.php?user_id=$uuid&cid=$cid>$usolved</a>";
 echo "<td>".sec2str($U[$i]->time);
 for ($j=0;$j<$pid_cnt;$j++){

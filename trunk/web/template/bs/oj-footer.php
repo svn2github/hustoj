ef16a75915f1d977e356ba40d0e90f8b5b4862c8
张浩斌 <newsclan@gmail.com>
2015-06-28 17:06:32 +0800

    Update oj-footer.php

--- trunk/web/template/bs/oj-footer.php
+++ trunk/web/template/bs/oj-footer.php
@@ -35,7 +35,7 @@
 		<br>
 		All Copyright Reserved 2010-2014 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
 		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2014 <a href='https://github.com/zhblue/hustoj'>HUSTOJ Project</a> TEAM<br>
-		
+		扫码关注微信<img src=http://hustoj.com/wx.jpg width=120 >，有惊喜。
      <?php if ($OJ_SAE) {
                    echo "<a href=http://sae.sina.com.cn><img bolder=0 src=http://static.sae.sina.com.cn/image/poweredby/poweredby.png></a>";
             

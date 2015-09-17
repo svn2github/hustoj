5bdd571c171f459681855720ff52a9a36b292e93
张浩斌 <newsclan@gmail.com>
2015-09-17 16:47:28 +0800

    Update oj-footer.php

--- trunk/web/template/classic/oj-footer.php
+++ trunk/web/template/classic/oj-footer.php
@@ -34,7 +34,7 @@
 		})()
 	</script>
 		<br>
-		All Copyright Reserved 2010-2011 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
+		All Copyright Reserved 2010-2015 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
 		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2012 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
 		
      <?php if ($OJ_SAE) {

8734ae95222c14740d36da89c1d11a26d382cebe
张浩斌 <newsclan@gmail.com>
2015-10-14 17:14:17 +0800

    Update js.php
    MathJax need by ZUCC

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -8,6 +8,9 @@
 $view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"./admin/msg.txt");
 
 ?>
+<script type="text/javascript"
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
 <script>
 $(document).ready(function(){
   var msg="<marquee  id=broadcast scrollamount=1 scrolldelay=50 onMouseOver='this.stop()' onMouseOut='this.start()' class=toprow><?php echo $view_marquee_msg?></marquee>";

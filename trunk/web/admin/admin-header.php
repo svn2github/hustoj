1f145a52a5b12a2218f7ef749b75357e0e7f9150
张浩斌 <newsclan@gmail.com>
2015-10-26 17:27:37 +0800

    admin csrf

--- trunk/web/admin/admin-header.php
+++ trunk/web/admin/admin-header.php
@@ -1,6 +1,14 @@
 <?php @session_start();?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel=stylesheet href='../include/hoj.css' type='text/css'>
+<script src="../template/bs3/jquery.min.js"></script>
+<script>
+$("document").ready(function (){
+  $("form").append("<div id='csrf' />");
+  $("#csrf").load("../csrf.php");
+});
+
+</script>
 <?php if (!(isset($_SESSION['administrator'])||
 			isset($_SESSION['contest_creator'])||
 			isset($_SESSION['problem_editor']))){

a265413788f5ff4fed2485cd5b548f0b7e47af80
张浩斌 <newsclan@gmail.com>
2016-06-13 10:59:46 +0800

    Update db.sql

--- trunk/install/db.sql
+++ trunk/install/db.sql
@@ -129,6 +129,7 @@ CREATE TABLE  `solution` (
   `judgetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `pass_rate` DECIMAL(2,2) UNSIGNED NOT NULL DEFAULT 0,
   `lint_error` int UNSIGNED NOT NULL DEFAULT 0,
+  `judger` CHAR(16) NOT NULL DEFAULT 'LOCAL',
   PRIMARY KEY (`solution_id`),
   KEY `uid` (`user_id`),
   KEY `pid` (`problem_id`),

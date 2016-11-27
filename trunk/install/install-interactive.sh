81283ed7449b13a1f6bbed3a9315677ae724837e
张浩斌 <newsclan@gmail.com>
2016-11-27 07:48:28 -0600

    CE

--- trunk/install/install-interactive.sh
+++ trunk/install/install-interactive.sh
@@ -70,9 +70,11 @@ sudo    mkdir /home/judge/run2
 sudo    mkdir /home/judge/run3
 sudo cp java0.policy  judge.conf /home/judge/etc
 
-sudo chown -R $APACHEUSER /home/judge
+sudo chown -R $APACHEUSER /home/judge/data
 sudo chown -R root /home/judge/log /home/judge/etc /home/judge/run?
-sudo chmod 711 /home/judge /home/judge/data /home/judge/run?
+sudo chmod 711 /home/judge /home/judge/data
+sudo chgrp judge /home/judge/run?
+sudo chmod 771 /home/judge/run?
 
 #update database account
 SED_CMD="s/OJ_USER_NAME=root/OJ_USER_NAME=$DBUSER/g"

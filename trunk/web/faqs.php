e9ea6b4205554b448655f8e9b90abc348d3cfb1e
张浩斌 <newsclan@gmail.com>
2016-12-14 17:32:11 +0800

    Update faqs.php

--- trunk/web/faqs.php
+++ trunk/web/faqs.php
@@ -1,15 +1,19 @@
 <?php
 ////////////////////////////Common head
-	$cache_time=10;
-	$OJ_CACHE_SHARE=false;
-	require_once('./include/cache_start.php');
+        $cache_time=10;
+        $OJ_CACHE_SHARE=false;
+        require_once('./include/cache_start.php');
     require_once('./include/db_info.inc.php');
-	require_once('./include/setlang.php');
-	$view_title= "Welcome To Online Judge";
+        require_once('./include/setlang.php');
+        $view_title= "Welcome To Online Judge";
 
 /////////////////////////Template
-require("template/".$OJ_TEMPLATE."/faqs.php");
+if(file_exists("template/".$OJ_TEMPLATE."/faqs.$OJ_LANG.php"))
+        require("template/".$OJ_TEMPLATE."/faqs.$OJ_LANG.php");
+else
+        require("template/".$OJ_TEMPLATE."/faqs.php");
 /////////////////////////Common foot
 if(file_exists('./include/cache_end.php'))
-	require_once('./include/cache_end.php');
+        require_once('./include/cache_end.php');
 ?>
+

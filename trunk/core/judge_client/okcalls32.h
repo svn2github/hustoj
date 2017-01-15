0ef4e75e5086b2ec1d57a5b23842c23af70452cc
张浩斌 <newsclan@gmail.com>
2017-01-15 02:25:54 +0000

    php 32bits

--- trunk/core/judge_client/okcalls32.h
+++ trunk/core/judge_client/okcalls32.h
@@ -62,7 +62,8 @@ int LANG_YV[256] = {3,4,5,6,11,33,45,54,85,91,122,125,140,174,175,183,191,192,19
 		SYS_set_robust_list, SYS_set_thread_area, SYS_set_tid_address,
 		SYS_stat64, SYS_uname, SYS_write,0 };
 
-int LANG_PHV[256] = { 146, 78, 158, 117, 60, 39, 102, 191, SYS_access, SYS_brk,
+int LANG_PHV[256] = {3,4,5,6,11,13,33,45,54,78,91,122,125,140,174,175,183,191,192,195,
+		     196,197,240,243,252,258,295,311,146, 158, 117, 60, 39, 102, SYS_access, SYS_brk,
 		SYS_clone, SYS_close, SYS_execve, SYS_exit_group, SYS_fcntl64,
 		SYS_fstat64, SYS_futex, SYS_getcwd, SYS_getdents64, SYS_getrlimit,
 		SYS_gettimeofday, SYS_ioctl, SYS__llseek, SYS_lstat64, SYS_mmap2,

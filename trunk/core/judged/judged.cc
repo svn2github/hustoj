7f5dafd435e93fab79e5643db373602962192660
张浩斌 <newsclan@gmail.com>
2017-01-15 02:40:57 +0000

    php loading big libs ?

--- trunk/core/judged/judged.cc
+++ trunk/core/judged/judged.cc
@@ -205,8 +205,8 @@ void run_client(int runid, int clientid) {
 	LIM.rlim_cur = 800;
 	setrlimit(RLIMIT_CPU, &LIM);
 
-	LIM.rlim_max = 80 * STD_MB;
-	LIM.rlim_cur = 80 * STD_MB;
+	LIM.rlim_max = 180 * STD_MB;
+	LIM.rlim_cur = 180 * STD_MB;
 	setrlimit(RLIMIT_FSIZE, &LIM);
 
 	LIM.rlim_max = STD_MB << 11;

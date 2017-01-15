47cf57a7acc393a53a66760dfb5d4373bfaa3d63
张浩斌 <newsclan@gmail.com>
2017-01-15 02:12:57 +0000

    Ruby 32bits

--- trunk/core/judge_client/okcalls32.h
+++ trunk/core/judge_client/okcalls32.h
@@ -34,7 +34,8 @@ int LANG_JV[256] = { 295, SYS_fcntl64, SYS_getdents64, SYS_ugetrlimit,
 		SYS_set_thread_area, SYS_set_tid_address, SYS_set_robust_list,
 		SYS_exit_group, 0 };
 
-int LANG_RV[256] = { 3,4,5,6,11,33,42,45,54,77,78,120,122,125,174,175,186,191,192,195,197,201,202,221,240,243,252,258,311,
+int LANG_RV[256] = { 3,4,5,6,11,33,42,45,54,77,78,91,120,122,125,174,175,183,186,191,192,
+		    195,196,197,199,200,201,202,221,240,243,252,258,311,
 		340, 126, SYS_access, SYS_brk, SYS_close, SYS_execve,
 		SYS_exit_group, SYS_fstat64, SYS_futex, SYS_getegid32, SYS_geteuid32,
 		SYS_getgid32, SYS_getrlimit, SYS_gettimeofday, SYS_getuid32, SYS_mmap2,

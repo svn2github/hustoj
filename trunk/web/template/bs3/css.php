c9836fc5e99d0639b761f0cfd8e0cdb8eac0dc41
张浩斌 <newsclan@gmail.com>
2017-01-17 21:24:58 +0800

    Update css.php

--- trunk/web/template/bs3/css.php
+++ trunk/web/template/bs3/css.php
@@ -8,9 +8,10 @@
 <!-- 新 Bootstrap 核心 CSS 文件 -->
 <link rel="stylesheet" href="<?php echo $path_fix."template/$OJ_TEMPLATE/"?>bootstrap.min.css">
 
+<?php if(!isset($OJ_FLAT)||!$OJ_FLAT){?>
 <!-- 可选的Bootstrap主题文件（一般不用引入） -->
 <link rel="stylesheet" href="<?php echo $path_fix."template/$OJ_TEMPLATE/"?>bootstrap-theme.min.css">
-
+<?php }?>
 <link rel="stylesheet" href="<?php echo $path_fix."template/$OJ_TEMPLATE/"?>local.css">
 
 

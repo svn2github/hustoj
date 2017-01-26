f06970c4ba08b29135d2eb3b81dc3c074631199b
张浩斌 <newsclan@gmail.com>
2017-01-26 08:51:57 +0800

    Update cn.php

--- trunk/web/lang/cn.php
+++ trunk/web/lang/cn.php
@@ -124,7 +124,7 @@ $MSG_Compile_OK="人品问题-愚人节快乐";
 	$MSG_UPDATE_DATABASE="更新数据库";
 	$MSG_ONLINE="在线";
   //contest
-  $MSG_PRIVATE_WARNING="私有比赛或考试作业，您无权查看题目。";
+  $MSG_PRIVATE_WARNING="比赛尚未开始或私有，不能查看题目。";
   $MSG_WATCH_RANK="点击这里查看做题排名。";
   $MSG_Public="公开";
   $MSG_Private="私有";

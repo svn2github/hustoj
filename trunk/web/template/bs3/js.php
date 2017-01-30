eaf51443613964a69e5ca82aa941d850d8dedfd4
张浩斌 <newsclan@gmail.com>
2017-01-30 15:38:05 +0800

    Update js.php

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -15,7 +15,8 @@ $view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"./admin/msg
 -->
 <script>
 $(document).ready(function(){
-  var msg="<marquee  id=broadcast scrollamount=1 scrolldelay=50 onMouseOver='this.stop()' onMouseOut='this.start()' class=toprow><?php echo str_replace("\r\n", '', $view_marquee_msg); ?></marquee>";
+  var msg="<marquee  id=broadcast scrollamount=1 scrolldelay=50 onMouseOver='this.stop()'"+
+      " onMouseOut='this.start()' class=toprow>"+<?php echo json_encode($view_marquee_msg); ?>+"</marquee>";
   $(".jumbotron").prepend(msg);
   $("form").append("<div id='csrf' />");
   $("#csrf").load("csrf.php");

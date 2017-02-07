c5ae03eed85f9134d713c6846bc59fcd2a0299ed
张浩斌 <newsclan@gmail.com>
2017-02-07 21:16:38 +0800

    Update okcalls64.h

--- trunk/core/judge_client/okcalls64.h
+++ trunk/core/judge_client/okcalls64.h
@@ -21,7 +21,7 @@ int LANG_CV[256] = {0,1,2,4,5,9,11,12,21,59,63,89,158,231,240, 8, SYS_time, SYS_
 		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
 		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 231, 0 };
 
-int LANG_PV[256] = {0,1,2,4,9,11,13,16,59,89,97,201,231,SYS_open, SYS_set_thread_area, SYS_brk, SYS_read,
+int LANG_PV[256] = {0,1,2,3,4,9,11,13,16,59,89,97,201,231,SYS_open, SYS_set_thread_area, SYS_brk, SYS_read,
 		SYS_uname, SYS_write, SYS_execve, SYS_ioctl, SYS_readlink, SYS_mmap,
 		SYS_rt_sigaction, SYS_getrlimit, 252, 191, 158, 231, SYS_close,
 		SYS_exit_group, SYS_munmap, SYS_time, 4, 0 };

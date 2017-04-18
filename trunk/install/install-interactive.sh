9bbc25fc0f41a46d3d45ea92139bedf6ce3faea1
张浩斌 <newsclan@gmail.com>
2017-04-18 14:17:11 +0800

    Update install-interactive.sh

--- trunk/install/install-interactive.sh
+++ trunk/install/install-interactive.sh
@@ -35,7 +35,7 @@ HTTP_START="sudo /etc/init.d/apache2 restart"
 
 else
 sudo yum -y update
-sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static flex
+sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static libstdc++-static flex
 sudo /etc/init.d/mysqld start
 WEBBASE=/var/www/html
 APACHEUSER=apache

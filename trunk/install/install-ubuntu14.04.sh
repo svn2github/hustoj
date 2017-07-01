457ca7560b4d1119283cf1dfad5f584e2f2e84e2
张浩斌 <newsclan@gmail.com>
2017-07-01 09:12:10 +0800

    Update install-ubuntu14.04.sh

--- trunk/install/install-ubuntu14.04.sh
+++ trunk/install/install-ubuntu14.04.sh
@@ -44,7 +44,7 @@ sed -i "s:#\tinclude fastcgi_params;:\tinclude fastcgi_params;\n\t}:g" /etc/ngin
 sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php5/fpm/php.ini
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php5/fpm/php.ini
 /etc/init.d/php5-fpm restart
-
+service php5-fpm restart
 cd src/core
 ./make.sh
 echo "/usr/bin/judged" >> /etc/rc.local

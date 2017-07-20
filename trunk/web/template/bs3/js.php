c76872a942e026db09594408aab4a3513dacaa83
张浩斌 <newsclan@gmail.com>
2017-07-20 15:40:32 +0800

    Update js.php

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -26,7 +26,7 @@ $(document).ready(function(){
   $("body").append("<div id=footer class=center >GPLv2 licensed by <a href='https://github.com/zhblue/hustoj' >HUSTOJ</a> "+(new Date()).getFullYear()+" </div>");
   $("body").append("<div class=center ><a href='https://www.duba.com/?un_454974_116387' title='每天点击一次开发者可获得￥0.06' target='_blank'>捐助系统开发者</a></div>");
 });
-  console.log("If you want to change the look, make a copy of bs3 under template directory.\nRename it to whatever you like, and change the $OJ_TEMPLATE value in db_info.inc.php\nAfter that modify files under your own directory .\n");
+  console.log("If you want to change the appearance of the web pages, make a copy of bs3 under template directory.\nRename it to whatever you like, and change the $OJ_TEMPLATE value in db_info.inc.php\nAfter that modify files under your own directory .\n");
 
 
 </script>

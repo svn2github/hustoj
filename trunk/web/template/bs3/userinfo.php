a13437e461e31a248f0e2a2ee0d7ab34a73c8f0c
张浩斌 <newsclan@gmail.com>
2017-10-11 10:21:45 +0000

    optimize sql

--- trunk/web/template/bs3/userinfo.php
+++ trunk/web/template/bs3/userinfo.php
@@ -42,8 +42,7 @@ function p(id,c){
 	document.write("<a href=problem.php?id="+id+">"+id+" </a>(<a href='status.php?user_id=<?php echo $user?>&problem_id="+id+"'>"+c+"</a>)");
 
 }
-<?php $sql="SELECT `problem_id`,count(1) FROM `solution` WHERE `user_id`=? 
-		AND problem_id in (select distinct problem_id from solution where `user_id`=? and result=4) group by `problem_id` ORDER BY `problem_id` ASC";
+<?php $sql="SELECT `problem_id`,count(1) from solution where `user_id`=? and result=4 group by `problem_id` ORDER BY `problem_id` ASC";
 if ($result=pdo_query($sql,$user,$user)){ 
     foreach($result as $row)
     echo "p($row[0],$row[1]);";
@@ -86,7 +85,7 @@ mypie.drawPie(y,x);
 //mypie.clearPie();
 </script>
 <tr ><td>School:<td align=center><?php echo $school?></tr>
-<tr ><td>Email:<td align=center><?php echo $email?></tr>
+<tr ><td>Email:<td align=center>***<?php// echo $email?></tr>
 </table>
 <?php
 if(isset($_SESSION['administrator'])){

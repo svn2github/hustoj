88dfe427088094fc54df5d46f53eeb7362b36ff9
张浩斌 <newsclan@gmail.com>
2017-11-12 02:47:18 +0000

    miss submit out of contest

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -292,8 +292,8 @@ for ($i=0;$i<$rows_cnt;$i++){
 
                         $view_status[$i][6]= "<a target=_blank href=showsource.php?id=".$row['solution_id'].">".$language_name[$row['language']]."</a>";
 			if($row["problem_id"]>0){
-                        	if (isset($cid)) {
-                                	$view_status[$i][6].= "/<a target=_self href=\"submitpage.php?cid=".$cid."&pid=".$row['num']."&sid=".$row['solution_id']."\">Edit</a>";
+                        	if ($row['contest_id']>0) {
+                                	$view_status[$i][6].= "/<a target=_self href=\"submitpage.php?cid=".$row['contest_id']."&pid=".$row['num']."&sid=".$row['solution_id']."\">Edit</a>";
                         	}else{
                                 	$view_status[$i][6].= "/<a target=_self href=\"submitpage.php?id=".$row['problem_id']."&sid=".$row['solution_id']."\">Edit</a>";
                         	}

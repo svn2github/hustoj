17074310ffb1ef1dcd252902c1118aee68c8d235
卜睿 <358194828@qq.com>
2017-11-28 06:26:22 +0800

    Update submit.php

--- trunk/web/submit.php
+++ trunk/web/submit.php
@@ -158,7 +158,7 @@ setcookie('lastlang',$language,time()+360000);
 
 $ip=$_SERVER['REMOTE_ADDR'];
 if( !empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ){
-                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 }
 if ($len<2){
 	$view_errors="Code too short!<br>";

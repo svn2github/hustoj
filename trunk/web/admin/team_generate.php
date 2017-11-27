80a71970dde9c5294813d967c1049d1a77786887
卜睿 <358194828@qq.com>
2017-11-28 06:35:51 +0800

    Update team_generate.php

--- trunk/web/admin/team_generate.php
+++ trunk/web/admin/team_generate.php
@@ -34,9 +34,18 @@ if (!(isset($_SESSION['administrator']))){
 			$email="your_own_email@internet";
                        
 			$school="your_own_school";
+			$ip = ($_SERVER['REMOTE_ADDR']);
+      
+      
+			if( !empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ){
+				    $REMOTE_ADDR = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+				    $tmp_ip=explode(',',$REMOTE_ADDR);
+				    $ip =(htmlentities($tmp_ip[0],ENT_QUOTES,"UTF-8"));
+			}
 			$sql="INSERT INTO `users`("."`user_id`,`email`,`ip`,`accesstime`,`password`,`reg_time`,`nick`,`school`)".
 			"VALUES(?,?,?,NOW(),?,NOW(),?,?)on DUPLICATE KEY UPDATE `email`=?,`ip`=?,`accesstime`=NOW(),`password`=?,`reg_time`=now(),nick=?,`school`=?";
-			pdo_query($sql,$user_id,$email,$_SERVER['REMOTE_ADDR'],$password,$nick,$school,$email,$_SERVER['REMOTE_ADDR'],$password,$nick,$school) ;
+			pdo_query($sql,$user_id,$email,$ip,$password,$nick,$school,$email,$_SERVER['REMOTE_ADDR'],$password,$nick,$school) ;
 		}
 		echo  "</table>";
 		

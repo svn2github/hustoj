f77a2232d56c952689bc0e3e68f4481cf6e2fe1f
张浩斌 <newsclan@gmail.com>
2017-12-31 12:13:20 +0800

    Update install-centos7.sh

--- trunk/install/install-centos7.sh
+++ trunk/install/install-centos7.sh
@@ -77,6 +77,11 @@ yum -y update
 yum -y install mono
 ln -s /usr/bin/mcs /usr/bin/gmcs
 
+#free pascal
+wget https://downloads.sourceforge.net/project/freepascal/Linux/3.0.4/fpc-3.0.4-1.x86_64.rpm
+rpm -ivh  fpc-3.0.4-1.x86_64.rpm
+
+
 reset
 echo "Remember your database account for HUST Online Judge:"
 echo "username:root"

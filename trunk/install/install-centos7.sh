0a617f9f728cd36dcd7037e6f1ba316eeeab3010
张浩斌 <newsclan@gmail.com>
2017-12-31 11:43:04 +0800

    Update install-centos7.sh

--- trunk/install/install-centos7.sh
+++ trunk/install/install-centos7.sh
@@ -65,7 +65,11 @@ else
 fi
 /usr/bin/judged
 
-
+# mono install for c# 
+rpm --import "http://keyserver.Ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+yum-config-manager --add-repo http://download.mono-project.com/repo/centos/ 
+yum install mono
+ln -s /usr/bin/mcs /usr/bin/gmcs
 
 reset
 echo "Remember your database account for HUST Online Judge:"

081d05fe42f3e981cab2450c79cef3c6b478b2b7
张浩斌 <newsclan@gmail.com>
2018-01-10 11:00:29 +0000

    fix upload fail

--- trunk/web/kindeditor/php/upload_json.php
+++ trunk/web/kindeditor/php/upload_json.php
@@ -6,7 +6,7 @@
  * 如果您确定直接使用本程序，使用之前请仔细确认相关安全设置。
  *
  */
-@session_start();
+require_once("../../include/db_info.inc.php");
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])
       ||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])
       ||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])

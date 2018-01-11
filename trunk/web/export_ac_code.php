5e58e1f6026e636eae2fc7ed4ddd9ba627850a11
张浩斌 <newsclan@gmail.com>
2018-01-11 11:51:35 +0800

    Update export_ac_code.php

--- trunk/web/export_ac_code.php
+++ trunk/web/export_ac_code.php
@@ -1,5 +1,5 @@
 <?php 
-@session_start();
+require_once("./include/db_info.inc.php");
          header ( "content-type:   application/file" );
          header ( "content-disposition:   attachment;   filename=\"ac-".$_SESSION[$OJ_NAME.'_'.'user_id'].".txt" );
 if (!isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
@@ -7,7 +7,6 @@ if (!isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
 	require("template/".$OJ_TEMPLATE."/error.php");
 	exit(0);
 }
-require_once('./include/db_info.inc.php');
 $sql="select distinct source,problem_id from source_code right join 
 		(select solution_id,problem_id from solution where user_id=? and result=4) S 
 		on source_code.solution_id=S.solution_id order by problem_id";

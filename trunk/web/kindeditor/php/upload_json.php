3758c3800119deb5afbbf9dc79461a3eb712e0db
张浩斌 <newsclan@gmail.com>
2018-01-14 10:06:21 +0000

    pdf

--- trunk/web/kindeditor/php/upload_json.php
+++ trunk/web/kindeditor/php/upload_json.php
@@ -30,7 +30,7 @@ $ext_arr = array(
 	'image' => array('gif', 'jpg', 'jpeg', 'png', 'bmp'),
 	'flash' => array('swf', 'flv'),
 	'media' => array('swf', 'flv', 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb'),
-	'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2'),
+	'file' => array('pdf','doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2'),
 );
 //最大文件大小
 $max_size = 1000000;

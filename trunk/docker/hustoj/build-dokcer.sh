d1c33b0112b87224d2ceb543b573c20dd4e59bcc
shiningrise <shiningrise@gmail.com>
2018-02-19 01:50:18 +0800

    docker debug

--- trunk/docker/hustoj/build-dokcer.sh
+++ trunk/docker/hustoj/build-dokcer.sh
@@ -1,16 +1,20 @@
 docker rm $(docker ps -a -q)
 #cd docker\hustoj-base
-#cd ../../
+cd ../../
 #cd core & ./makeForDocker.sh & cd ..
-docker build -t hustoj ./
+docker build -f docker/hustoj/Dockerfile -t hustoj  ./
 
-mkdir -p /home/data/
-chmod 777 /home/data/
+mkdir -p /home/data/ /home/data/config
+chmod 777 /home/data/ /home/data/config
 
 docker rm -f /hustoj
 docker run -d \
 -it \
 -v /home/data/mysql:/var/lib/mysql \
+-v /home/data/data:/home/judge/data \
+-v /home/data/config/db_info.inc.php:/home/judge/src/web/include/db_info.inc.php \
+-v /home/data/upload:/home/judge/src/web/upload \
+
 --name hustoj -p 80:80 \
 hustoj
 

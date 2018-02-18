6a8cc7ded757f409f2bf023df8ff59824f6dae9d
shiningrise <shiningrise@gmail.com>
2018-02-19 00:26:28 +0800

    docker v1

--- trunk/build-dokcer.sh
+++ trunk/build-dokcer.sh
@@ -1,11 +1,18 @@
 docker rm $(docker ps -a -q)
-cd docker\hustoj-base
-cd ../../
-docker rm -f /hustoj
+#cd docker\hustoj-base
+#cd ../../
 cd core
 ./makeForDocker.sh
 cd ..
 docker build -t hustoj ./
-docker run --name hustoj -p 80:80 -i -t -d hustoj
-#docker exec -i -t hustoj /bin/bash
+
+mkdir -p /home/data/
+chmod 777 /home/data/
+
+docker rm -f /hustoj
+docker run -d \
+-it \
+-v /home/data/mysql:/var/lib/mysql \
+--name hustoj -p 80:80 \
+hustoj
 
--- trunk/docker-entrypoint.sh
+++ trunk/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 service nginx start
 chown -R mysql:mysql /var/lib/mysql
 service mysql start
+mysql -h localhost -uroot < src/install/db.sql
+echo "insert into jol.privilege values('admin','administrator','N');"|mysql -h localhost -uroot 
 php5-fpm
 /usr/bin/judged
 /bin/bash  

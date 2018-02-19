0b70dccfe693e266a0fb444a2ec741b5dda9ccbe
张浩斌 <newsclan@gmail.com>
2018-02-19 12:18:35 +0800

    Update judge_client.cc

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1655,7 +1655,7 @@ void copy_lua_runtime(char * work_dir) {
 void copy_js_runtime(char * work_dir) {
 
 //	copy_shell_runtime(work_dir);
-	execute_cmd("/bin/mkdir -p %s/usr/lib /lib/i386-linux-gnu/", work_dir);
+	execute_cmd("/bin/mkdir -p %s/usr/lib %s/lib/i386-linux-gnu/", work_dir, work_dir);
         execute_cmd("/bin/cp /lib/i386-linux-gnu/libz.so.*  %s/lib/i386-linux-gnu/", work_dir);
         execute_cmd("/bin/cp /usr/lib/i386-linux-gnu/libcares.so.*  %s/lib/i386-linux-gnu/", work_dir);
         execute_cmd("/bin/cp /usr/lib/libv8.so.*  %s/lib/i386-linux-gnu/", work_dir);
@@ -1670,7 +1670,7 @@ void copy_js_runtime(char * work_dir) {
         execute_cmd("/bin/cp /lib/i386-linux-gnu/libgcc_s.so.1  %s/lib/i386-linux-gnu/", work_dir);
         execute_cmd("/bin/cp /lib/ld-linux.so.*  %s/lib/i386-linux-gnu/", work_dir);
 
-	execute_cmd("/bin/mkdir -p %s/usr/lib /lib/x86_64-linux-gnu/", work_dir);
+	execute_cmd("/bin/mkdir -p %s/usr/lib %s/lib/x86_64-linux-gnu/", work_dir, work_dir);
         execute_cmd("/bin/cp /lib/x86_64-linux-gnu/libz.so.*  %s/lib/x86_64-linux-gnu/", work_dir);
         execute_cmd("/bin/cp /usr/lib/x86_64-linux-gnu/libcares.so.*  %s/lib/x86_64-linux-gnu/", work_dir);
         execute_cmd("/bin/cp /usr/lib/libv8.so.*  %s/lib/x86_64-linux-gnu/", work_dir);

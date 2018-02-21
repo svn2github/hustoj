26931f77a2a6aec1e6ed74d637c9cda38429a4c8
吴晓阳 <shiningrise@gmail.com>
2018-02-21 08:45:59 +0800

    +

--- trunk/docker/hustoj-cpp/build-dokcer.sh
+++ trunk/docker/hustoj-cpp/build-dokcer.sh
@@ -1,6 +1,6 @@
 
 docker build -f Dockerfile -t hustoj-cpp  ./
 
-docker rm -f -v /hustoj-dev & docker run -d -it --privileged --name hustoj-dev -p 80:80 hustoj-dev
+docker rm -f -v /hustoj-dev & docker run -d -it --privileged --name hustoj-dev -p 8080:80 hustoj-dev
 #docker run -d -it --privileged --name hustoj-dev -p 80:80 -v /home/test/:/data hustoj-dev
 #docker exec -i -t hustoj-dev /bin/bash

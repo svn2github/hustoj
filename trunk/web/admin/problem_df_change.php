4dc6a9be55d8750b1b1ba2c76ec3a3555a4f622a
张浩斌 <newsclan@gmail.com>
2018-03-15 09:57:19 +0800

    fix one click

--- trunk/web/admin/problem_df_change.php
+++ trunk/web/admin/problem_df_change.php
@@ -1,5 +1,10 @@
 <?php require_once("admin-header.php");
-require_once("../include/check_post_key.php");
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+        require_once("../include/check_post_key.php");
+}else{
+        require_once("../include/check_get_key.php");
+
+}
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']))){
 	echo "<a href='../loginpage.php'>Please Login First!</a>";
 	exit(1);

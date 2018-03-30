435692cba26c427ad7c8d86034864caf460fe532
张浩斌 <newsclan@gmail.com>
2018-03-31 06:20:00 +0800

    fix on multi-core

--- trunk/install/autocpu.sh
+++ trunk/install/autocpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CPU=`grep "cpu cores" /proc/cpuinfo |head -1|awk '{print $4}'`
-COMPENSATION=`grep 'mips' /proc/cpuinfo|awk -F: '{printf("%.2f",$2/4000)}'`
+COMPENSATION=`grep 'mips' /proc/cpuinfo|head -1|awk -F: '{printf("%.2f",$2/5000)}'`
 cd /home/judge
 sed -i "s/OJ_RUNNING=1/OJ_RUNNING=$CPU/g" etc/judge.conf
 sed -i "s/OJ_CPU_COMPENSATION=1.0/OJ_CPU_COMPENSATION=$COMPENSATION/g" etc/judge.conf

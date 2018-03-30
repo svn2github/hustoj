29d27275dd79aa643db44b20265ff6d60e67b79c
张浩斌 <newsclan@gmail.com>
2018-03-30 18:22:07 +0800

    few adjusts

--- trunk/install/bak.sh
+++ trunk/install/bak.sh
@@ -5,8 +5,8 @@ OLD3=`date -d"3 day ago" +"%Y%m%d"`
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`
 
-mysqldump -R --all-databases -u$USER -p$PASSWORD | bzip2 >/var/backups/db_${DATE}.bz2
-if tar cjf /var/backups/hustoj_${DATE}.tar.bz2 /home/judge/data /home/judge/src /home/judge/etc /var/backups/db_${DATE}.bz2; then
+mysqldump -R --database jol -u$USER -p$PASSWORD | bzip2 >/var/backups/db_${DATE}.tar.bz2
+if tar cjf /var/backups/hustoj_${DATE}.tar.bz2 /home/judge/data /home/judge/src /home/judge/etc /var/backups/db_${DATE}.tar.bz2; then
 	rm /var/backups/hustoj_${OLD3}.tar.bz2
-	rm /var/backups/db_${OLD}.bz2
+	rm /var/backups/db_${OLD}.tar.bz2
 fi

a00ee457ad67e5d0814891b045564c0955ccb725
张浩斌 <newsclan@gmail.com>
2018-04-10 13:49:49 +0800

    restore.sh

---
+++ trunk/install/restore.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+DATE=`date +%Y%m%d`
+BAKDATE=`echo $1 |awk -F\. '{print $1}'|awk -F_ '{print $2}'`
+USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
+PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`
+echo $DATE
+echo $BAKDATE
+echo $USER
+echo $PASSWORD
+mkdir hustoj-restore
+cd hustoj-restore
+MAIN="../$1"
+tar xjf $MAIN
+/home/judge/src/install/bak.sh
+mv /home/judge/data /home/judge/data.del.$DATE
+mv home/judge/data /home/judge/
+mv /home/judge/src/web/upload /home/judge/src/web/upload.del.$DATE
+mv home/judge/src/web/upload /home/judge/src/web/
+

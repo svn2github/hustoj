6904563b574c479a6a7db771d9f2deab58ca05ab
张浩斌 <newsclan@gmail.com>
2018-04-25 16:56:29 +0800

    show contest menu

--- trunk/web/discuss3/oj-header.php
+++ trunk/web/discuss3/oj-header.php
@@ -2,5 +2,12 @@
 	require_once('../include/db_info.inc.php');
 	require_once('../include/setlang.php');
 	require_once('../include/const.inc.php');
+	if(isset($_GET['tid'])&&!isset($_GET['cid'])){
+		$tid=intval($_GET['tid']);
+		$cid=pdo_query("select cid from topic where tid=?",$tid)[0][0];
+		if($cid>0) $_GET['cid']=$cid;
+//		echo "cid:".$cid;
+
+	}
    ob_start();
 ?>

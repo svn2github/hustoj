b39203981308c493a0404c13c4b5b76cc8f37b76
张浩斌 <newsclan@gmail.com>
2018-05-19 03:45:04 +0000

    move Main.c to endy

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -933,8 +933,8 @@ void umount(char * work_dir){
 int compile(int lang,char * work_dir) {
 	int pid;
 
-	const char * CP_C[] = { "gcc", "Main.c", "-o", "Main","-O2", "-Wall",
-			"-lm", "--static", "-DONLINE_JUDGE", NULL };
+	const char * CP_C[] = { "gcc", "-o", "Main","-O2", "-Wall",
+			"-lm", "--static", "-DONLINE_JUDGE","Main.c", NULL };
 	const char * CP_X[] = { "g++", "-fno-asm","-O2", "-Wall",
 			"-lm", "--static", "-std=c++11", "-DONLINE_JUDGE", "-o", "Main", "Main.cc", NULL };
 	const char * CP_P[] =

a95718af45aa4799fe024f74c287d6c420b8b711
张浩斌 <newsclan@gmail.com>
2018-06-25 21:09:14 +0800

    contributed by melongist
    https://github.com/melongist

--- trunk/web/lang/ko.php
+++ trunk/web/lang/ko.php
@@ -1,123 +1,205 @@
 <?php
-	//oj-header.php
-	$MSG_FAQ="자주 묻는 질문";
-	$MSG_BBS="게시판";
-	$MSG_HOME="처음 화면";
-	$MSG_PROBLEMS="문제 모음";
-	$MSG_STATUS="채점 상황 열람";
-	$MSG_RANKLIST="순위";
-	$MSG_CONTEST="대회";
-	$MSG_RECENT_CONTEST="Recent";
-	
-	$MSG_LOGOUT="LOGOUT";
-	$MSG_LOGIN="LOGIN";
-	$MSG_LOST_PASSWORD="Lost Password";
-	$MSG_REGISTER="가입하기";
-	$MSG_ADMIN="관리자";
-	$MSG_STANDING="순위";
-	$MSG_STATISTICS="통계";
-	$MSG_USERINFO="정보 수정";
-	$MSG_MAIL="Mail";
-	
-	//status.php
-	$MSG_Pending="Pending";
-	$MSG_Pending_Rejudging="Pending Rejudging";
-	$MSG_Compiling="Compiling";
-	$MSG_Running_Judging="Running & Judging";
-	$MSG_Accepted="Accepted";
-	$MSG_Presentation_Error="Presentation Error";
-	$MSG_Wrong_Answer="Wrong Answer";
-	$MSG_Time_Limit_Exceed="Time Limit Exceed";
-	$MSG_Memory_Limit_Exceed="Memory Limit Exceed";
-	$MSG_Output_Limit_Exceed="Output Limit Exceed";
-	$MSG_Runtime_Error="Runtime Error";
-	$MSG_TEST_RUN="Test Running Done";
-	$MSG_Compile_Error="Compile Error";
-	$MSG_Compile_OK="Compile OK";
-	$MSG_RUNID="제출 번호";
-	$MSG_USER="사용자";
-	$MSG_PROBLEM="문제 번호";
-	$MSG_RESULT="결과";
-	$MSG_MEMORY="메모리 사용량";
-	$MSG_TIME="수행 시간";
-	$MSG_LANG="사용언어";
-	$MSG_CODE_LENGTH="코드 길이";
-	$MSG_SUBMIT_TIME="제출 시간";
-	//problemstatistics.php
-	$MSG_PD="PD";
-	$MSG_PR="PR";
-	$MSG_CI="CI";
-	$MSG_RJ="RJ";
-	$MSG_AC="AC";
-	$MSG_PE="PE";
-	$MSG_WA="WA";
-	$MSG_TLE="TLE";
-	$MSG_MLE="MLE";
-	$MSG_OLE="OLE";
-	$MSG_RE="RE";
-	$MSG_CE="CE";
-	$MSG_CO="CO";
-	$MSG_TR="TEST";
-	
-	//problemset.php
-	$MSG_SEARCH="Search";
-	$MSG_PROBLEM_ID="문제 ID";
-	$MSG_TITLE="문제명";
-	$MSG_SOURCE="출처";
-	$MSG_SUBMIT="제출";
-	
-	//ranklist.php
-	$MSG_Number="순위";
-	$MSG_NICK="닉네임";
-	$MSG_SOVLED="해결 문제 수";
-	$MSG_RATIO="정답률";
-	
-	//registerpage.php
-	$MSG_USER_ID="사용자 ID";
-	$MSG_PASSWORD="비밀번호";
-	$MSG_REPEAT_PASSWORD="비밀번호 반복 입력";
-	$MSG_SCHOOL="소속";
-	$MSG_EMAIL="이메일";
-	$MSG_REG_INFO="등록 정보";
-	$MSG_VCODE="Verify Code";
+//oj-header.php
+$MSG_FAQ="자주묻는질문";
+$MSG_BBS="묻고답하기";
+$MSG_HOME="처음화면";
+$MSG_PROBLEMS="문제";
+$MSG_STATUS="채점상황";
+$MSG_RANKLIST="순위";
+$MSG_CONTEST="대회";
+$MSG_RECENT_CONTEST="세계대회";
+$MSG_LOGOUT="로그아웃";
+$MSG_LOGIN="로그인";
+$MSG_LOST_PASSWORD="비밀번호 분실";
+$MSG_REGISTER="회원가입";
+$MSG_ADMIN="관리자 메뉴";
+$MSG_STANDING="순위";
+$MSG_STATISTICS="통계";
+$MSG_USERINFO="정보수정";
+$MSG_MAIL="E-mail";
 
-	//problem.php
-	$MSG_NO_SUCH_PROBLEM="해당 문제를 열람할 수 없습니다!";
-	$MSG_Description="문제 설명"  ;
-	$MSG_Input="입력"  ;
-	$MSG_Output= "출력" ;
-	$MSG_Sample_Input= "입력 예시" ;
-	$MSG_Sample_Output= "출력 예시" ;
-	$MSG_HINT= "도움말" ;
-	$MSG_Source= "출처" ;
-	$MSG_Time_Limit="시간 제한";
-	$MSG_Memory_Limit="메모리 제한";
-	
-	//admin menu
-	$MSG_SEEOJ="SeeOJ";
-	$MSG_ADD="Add";
-	$MSG_LIST="List";
-	$MSG_NEWS="News";
-	$MSG_TEAMGENERATOR="TeamGenerator";
-	$MSG_SETMESSAGE="SetMessage";
-	$MSG_SETPASSWORD="ChangePassWD";
-	$MSG_REJUDGE="Rejudge";
-	$MSG_PRIVILEGE="Privilege";
-	$MSG_GIVESOURCE="GiveSource";
-	$MSG_IMPORT="Import";
-	$MSG_EXPORT="Export";
-	$MSG_UPDATE_DATABASE="UpdateDatabase";
-	$MSG_ONLINE="Online";
+//status.php
+$MSG_Pending="Pending";
+$MSG_Pending_Rejudging="Pending Rejudging";
+$MSG_Compiling="Compiling";
+$MSG_Running_Judging="Running & Judging";
+$MSG_Accepted="Accepted";
+$MSG_Presentation_Error="Presentation Error";
+$MSG_Wrong_Answer="Wrong Answer";
+$MSG_Time_Limit_Exceed="Time Limit Exceed";
+$MSG_Memory_Limit_Exceed="Memory Limit Exceed";
+$MSG_Output_Limit_Exceed="Output Limit Exceed";
+$MSG_Runtime_Error="Runtime Error";
+$MSG_TEST_RUN="Test Running Done";
+$MSG_Compile_Error="Compile Error";
 
-	//someone please translate these
-	$MSG_PRIVATE_WARNING="This is a private contest which you don't have privilege。";
-	$MSG_WATCH_RANK="Click HERE to watch contest rank.";
-	$MSG_Public="Public";
-	$MSG_Private="Private";
-	$MSG_Running="Running";
-	$MSG_Start="Start";
-	$MSG_TotalTime="Total";
-	$MSG_LeftTime="Left";
-	$MSG_Ended="Finished";
-	
+$MSG_Runtime_Click="Runtime Error(Click)";
+$MSG_Compile_Click="Compile Error(Click)";
+$MSG_Click_Detail="Click To View Detail";
+$MSG_Compile_OK="Compile OK";
+
+$MSG_RUNID="채점번호";
+$MSG_USER="사용자ID";
+$MSG_PROBLEM="문제";
+$MSG_RESULT="채점결과";
+$MSG_MEMORY="메모리사용량";
+$MSG_TIME="실행시간";
+$MSG_LANG="제출언어";
+$MSG_CODE_LENGTH="코드용량";
+$MSG_SUBMIT_TIME="채점시간";
+$MSG_Manual="수동채점";
+$MSG_OK="OK";
+$MSG_Explain="이유나 설명을 입력하세요";
+
+//problemstatistics.php
+$MSG_PD="PD";
+$MSG_PR="PR";
+$MSG_CI="CI";
+$MSG_RJ="RJ";
+$MSG_AC="AC";
+$MSG_PE="PE";
+$MSG_WA="WA";
+$MSG_TLE="TLE";
+$MSG_MLE="MLE";
+$MSG_OLE="OLE";
+$MSG_RE="RE";
+$MSG_CE="CE";
+$MSG_CO="CO";
+$MSG_TR="Test";
+$MSG_RESET="초기화";
+
+//problemset.php
+$MSG_SEARCH="검색";
+$MSG_PROBLEM_ID="문제번호";
+$MSG_TITLE="제목";
+$MSG_SOURCE="출처/분류";
+$MSG_SUBMIT="제출";
+
+
+//ranklist.php
+$MSG_Number="순위";
+$MSG_NICK="별명";
+$MSG_SOVLED="해결한 문제";
+$MSG_RATIO="정답률";
+
+
+//registerpage.php
+$MSG_USER_ID="사용자ID";
+$MSG_PASSWORD="비밀번호";
+$MSG_REPEAT_PASSWORD="비밀번호 확인";
+$MSG_SCHOOL="소속/학교";
+$MSG_EMAIL="E-mail";
+$MSG_REG_INFO="등록 정보";
+$MSG_VCODE="확인 코드";
+
+//problem.php
+$MSG_NO_SUCH_PROBLEM="해당 문제가 없습니다!";
+$MSG_Description="문제 설명";
+$MSG_Input="입력 설명";
+$MSG_Output="출력 설명";
+$MSG_Sample_Input="입력 예시";
+$MSG_Sample_Output="출력 예시";
+$MSG_Test_Input="테스트 입력";
+$MSG_Test_Output="테스트 출력";
+$MSG_SPJ="Special Judge";
+$MSG_HINT="도움";
+$MSG_Source="출처";
+$MSG_Time_Limit="실행시간 제한";
+$MSG_Memory_Limit="메모리 제한";
+
+
+//admin menu
+$MSG_SEEOJ="OJ확인";
+$MSG_ADD="추가-";
+$MSG_LIST="리스트";
+$MSG_NEWS="공지사항";
+$MSG_TEAMGENERATOR="팀 만들기";
+$MSG_SETMESSAGE="메시지 설정";
+$MSG_SETPASSWORD="비밀번호 변경";
+$MSG_REJUDGE="재채점";
+$MSG_PRIVILEGE="권한설정";
+$MSG_GIVESOURCE="소스코드주기";
+$MSG_IMPORT="업로드-";
+$MSG_EXPORT="다운로드-";
+$MSG_UPDATE_DATABASE="데이터베이스 갱신";
+$MSG_ONLINE="Online";
+
+
+//contest.php
+$MSG_PRIVATE_WARNING="이 대회에 참가할 수 있는 권한이 없습니다.";
+$MSG_WATCH_RANK="대회 순위 보기";
+
+$MSG_Public="공개";
+$MSG_Private="비공개";
+$MSG_Running="대회중";
+$MSG_Start="시작 시간";
+$MSG_TotalTime="대회 시간";
+$MSG_LeftTime="남은 시간";
+
+$MSG_Ended="대회 종료됨";
+
+$MSG_Login="로그인하세요.";
+$MSG_JUDGER="채점서버";
+
+
+$MSG_SOURCE_NOT_ALLOWED_FOR_EXAM="대회중에는 제출했던 코드를 볼 수 없습니다.";
+$MSG_BBS_NOT_ALLOWED_FOR_EXAM="대회중에는 게시판을 사용할 수 없습니다.";
+$MSG_MODIFY_NOT_ALLOWED_FOR_EXAM="대회중에는 비밀번호를 변경할 수 없습니다.";
+$MSG_MAIL_NOT_ALLOWED_FOR_EXAM="대회중에는 메일을 사용할 수 없습니다.";
+$MSG_LOAD_TEMPLATE_CONFIRM="다시 작성하시겠습니까?\\n 작성했던 코드가 모두 삭제됩니다!";
+$MSG_BLOCKLY_OPEN="Blockly"; 
+$MSG_BLOCKLY_TEST="Blockly Test Run"; 
+
+$MSG_MY_SUBMISSIONS="채점기록"; 
+$MSG_MY_CONTESTS="대회기록"; 
+$MSG_Creator="만든사람";
+
+$MSG_IMPORTED="업로드 완료";
+
+
+
+$MSG_USER="사용자";
+
+$MSG_PRINTER="Printer";
+$MSG_PRINT_DONE="Printed Fine";
+$MSG_PRINT_PENDING="Pending for Print";
+$MSG_PRINT_WAITING="Please waiting for delivery, don't post duplicated print task";
+$MSG_COLOR="Color";
+$MSG_BALLOON="Balloon";
+$MSG_BALLOON_DONE="Balloon Sent";
+$MSG_BALLOON_PENDING="Balloon Pending";
+
+
+$MSG_HELP_SEEOJ=" OJ 확인";
+$MSG_HELP_ADD_NEWS=" 첫 페이지 공지사항 추가";
+$MSG_HELP_NEWS_LIST=" 첫 페이지 공지사항 활성화/비활성화/수정";
+$MSG_HELP_USER_LIST=" 사용자 활성화/비활성화";
+$MSG_HELP_ADD_PROBLEM=" 문제 등록. 문제 등록 후 문제리스트에서 각 문제별 채점데이터 추가 가능";
+$MSG_HELP_PROBLEM_LIST=" 문제 관리. zip 파일로 채점데이터 파일 업로드 가능";
+$MSG_HELP_ADD_CONTEST=" 대회 등록. 쉼표(,)로 구분해 문제 입력. 비공개대회(비밀번호/사용자등록) 가능";
+$MSG_HELP_CONTEST_LIST=" 대회 확인. 대회시간/공개/비공개 수정. 이미 시작된 대회는 수정 권장하지 않음";
+$MSG_HELP_TEAMGENERATOR=" 다른 소속/학교로 계정/비번 생성. 인원 적은 경우 사용 권장하지 않음. 계정오류/삭제어려움";
+$MSG_HELP_SETMESSAGE=" 메시지 설정";
+$MSG_HELP_SETPASSWORD=" 사용자 비밀번호 변경. 관리자 권한 필요";
+$MSG_HELP_REJUDGE=" 문제별/채점번호별/대회별 재채점";
+$MSG_HELP_ADD_PRIVILEGE=" 사용자별 특별 권한 부여 및 변경";
+$MSG_HELP_PRIVILEGE_LIST=" 사용자별 특별 권한 확인 및 삭제";
+$MSG_HELP_GIVESOURCE=" 특정 사용자 채점코드 밀어주기. 부진학생 도움용";
+$MSG_HELP_EXPORT_PROBLEM=" fps.xml 파일로 문제 다운로드";
+$MSG_HELP_IMPORT_PROBLEM=" fps.xml 파일로 문제 업로드";
+$MSG_HELP_UPDATE_DATABASE=" hustoj 업데이트(sudo update-hustoj) 후, 데이터베이스 구조 갱신. 한 번만 실행!";
+$MSG_HELP_ONLINE=" 현재 접속자";
+$MSG_HELP_PE=" 출력된 형식이 틀렸습니다. 출력형식/공백/줄바꿈 등을 다시 확인해주세요."; 
+$MSG_HELP_WA=" 출력된 답이 틀렸습니다. 입력데이터의 범위, 출력해야 할 결과 등을 다시 확인해주세요."; 
+$MSG_HELP_TLE=" 실행시간 초과: 실행 제한시간을 초과하였습니다."; 
+$MSG_HELP_MLE=" 메모리사용 초과: 메모리사용 용량을 초과하였습니다."; 
+$MSG_HELP_OLE=" 출력 초과: 너무 많이 출력하였습니다. 무한 루프 출력 등.";
+$MSG_HELP_RE=" 실행중 오류: 프로그램을 실행하는 도중에 오류가 발생하였습니다.";
+$MSG_HELP_CE=" 컴파일 오류: gcc/g++/gpc 컴파일러가 프로그램을 변환하지 못하였습니다. 링크를 눌러 오류메시지 확인."; 
+$MSG_HELP_AC=" 통과했습니다."; 
+
+$MSG_HELP_MORE_TESTDATA_LATER="문제 등록 후, 더 많은 채점데이터 추가 가능"; 
+$MSG_HELP_SPJ="<a href='https://cn.bing.com/search?q=hustoj+special+judge' target='_blank'>search hustoj special judge</a>for more details"; 
+
+$MSG_HELP_BALLOON_SCHOOL="School Field of the Printer/Balloon privileged Accout, will be used as filter in task list.";	
 ?>

ab85b335f0b099ea66aeebc1ee0fe30dc6a89de1
张浩斌 <newsclan@gmail.com>
2018-06-27 22:32:44 +0800

    accounts login from different ips during the contest/exam

--- trunk/web/admin/suspect_list.php
+++ trunk/web/admin/suspect_list.php
@@ -24,7 +24,30 @@ $result=pdo_query($sql,$contest_id,$contest_id);
 ?>
 <?php
 echo "<center><table class='table table-striped' width=90% border=1>";
-echo "<tr><td>IP<td>user<td>same ip user count<td>";
+echo "<tr><td>IP<td>user<td>same ip user count</td>";
+echo "</tr>";
+foreach($result as $row){
+        echo "<tr>";
+        echo "<td>".$row['ip'];
+        echo "<td><a href='../status.php?cid=$contest_id&user_id=".$row['user_id']."'>".$row['user_id']."</a>";
+        echo "<td>".$row['c'];
+
+        echo "</tr>";
+}
+echo "</table></center></div>";
+$start=pdo_query("select start_time from contest where contest_id=?",$contest_id)[0][0];
+$end=pdo_query("select end_time from contest where contest_id=?",$contest_id)[0][0];
+$sql="select * from (select count(distinct ip) c,user_id from loginlog where time>=? and time<=? group by user_id) suspect 
+inner join (select distinct user_id from solution where contest_id=? ) u on suspect.user_id=u.user_id and suspect.c>1
+inner join (select distinct ip,user_id from loginlog where time>=? and time<=? ) ips on ips.user_id=u.user_id
+order by c desc ,u.user_id ";
+echo "accounts login from different ips during the contest/exam";
+//echo "--$sql--";
+$result=pdo_query($sql,$start,$end,$contest_id,$start,$end);
+?>
+<?php
+echo "<center><table class='table table-striped' width=90% border=1>";
+echo "<tr><td>IP<td>user<td>same user ip count</td>";
 echo "</tr>";
 foreach($result as $row){
         echo "<tr>";

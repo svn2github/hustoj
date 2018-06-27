cbcfa08a33b48aa1f9664b23a3946571aa09d255
Jeon Hyunseok <what_is_computer@msn.com>
2018-06-27 11:00:17 +0900

    admin menu window frameset fix
    20%, *

--- trunk/web/admin/index.php
+++ trunk/web/admin/index.php
@@ -1,12 +1,11 @@
 <html>
 <head>
-<title>JudgeOnline Administration</title>
+  <title>JudgeOnline Administration</title>
 </head>
-<frameset cols="20,90">
-<frame name="menu" src="menu.php">
-<frame name="main" src="help.php">
-<noframes>
-
-</noframes>
+<frameset cols="20%,*">
+  <frame name="menu" src="menu.php">
+  <frame name="main" src="help.php">
+  <noframes>
+  </noframes>
 </frameset>
 </html>

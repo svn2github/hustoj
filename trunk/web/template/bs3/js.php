0581f77c760a843efc1ed160d766a677eaeea856
张浩斌 <newsclan@gmail.com>
2018-07-15 19:37:41 +0800

    alipay ads

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -31,7 +31,7 @@ $(document).ready(function(){
   $("form").append("<div id='csrf' />");
   $("#csrf").load("<?php echo $path_fix?>csrf.php");
   $("body").append("<div id=footer class=center >GPLv2 licensed by <a href='https://github.com/zhblue/hustoj' >HUSTOJ</a> "+(new Date()).getFullYear()+" </div>");
-  $("body").append("<div class=center > <img src='http://hustoj.com/wx.jpg' width='96px'><br> 欢迎关注微信公众号onlinejudge</div>");
+  $("body").append("<div class=center > <img src='http://hustoj.com/wx.jpg' width='96px'><img src='http://hustoj.com/alipay.png' width='96px'><br> 欢迎关注微信公众号onlinejudge</div>");
 });
   console.log("If you want to change the appearance of the web pages, make a copy of bs3 under template directory.\nRename it to whatever you like, and change the $OJ_TEMPLATE value in db_info.inc.php\nAfter that modify files under your own directory .\n");
   console.log("To enable mathjax in hustoj, check line 15 in /home/judge/src/web/template/bs3/js.php");

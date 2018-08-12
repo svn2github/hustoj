577f97ca3cdb5c9532549c2d002a579551656f9d
张浩斌 <newsclan@gmail.com>
2018-08-12 21:07:19 +0800

    install by tar.gz in release

---
+++ trunk/install/install-ubuntu16-bytgz.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+apt-get update
+apt-get install -y subversion
+/usr/sbin/useradd -m -u 1536 judge
+cd /home/judge/
+
+if [ -z "$1" ]  
+then  
+    echo "Using github latest..."  
+    cd /home/judge/
+    svn co https://github.com/zhblue/hustoj/trunk/trunk/ src
+else
+    tar xzf $1
+    SRC=`find -name 'trunk'`
+    mv $SRC /home/judge/src
+    cd /home/judge/
+fi 
+
+apt-get install -y make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm php-common php-xml-parser nginx mysql-server php-mysql php-gd php-zip fp-compiler openjdk-8-jdk mono-devel php-mbstring php-xml
+apt-get install -y php-memcache memcached
+USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
+PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`
+CPU=`grep "cpu cores" /proc/cpuinfo |head -1|awk '{print $4}'`
+
+mkdir etc data log
+
+cp src/install/java0.policy  /home/judge/etc
+cp src/install/judge.conf  /home/judge/etc
+chmod +x src/install/ans2out
+
+if grep "OJ_SHM_RUN=0" etc/judge.conf ; then
+	mkdir run0 run1 run2 run3
+	chown www-data run0 run1 run2 run3
+fi
+
+sed -i "s/OJ_USER_NAME=root/OJ_USER_NAME=$USER/g" etc/judge.conf
+sed -i "s/OJ_PASSWORD=root/OJ_PASSWORD=$PASSWORD/g" etc/judge.conf
+sed -i "s/OJ_COMPILE_CHROOT=1/OJ_COMPILE_CHROOT=0/g" etc/judge.conf
+sed -i "s/OJ_RUNNING=1/OJ_RUNNING=$CPU/g" etc/judge.conf
+
+chmod 700 etc/judge.conf
+
+sed -i "s/DB_USER=\"root\"/DB_USER=\"$USER\"/g" src/web/include/db_info.inc.php
+sed -i "s/DB_PASS=\"root\"/DB_PASS=\"$PASSWORD\"/g" src/web/include/db_info.inc.php
+chmod 700 src/web/include/db_info.inc.php
+chown www-data src/web/include/db_info.inc.php
+chown www-data src/web/upload data run0 run1 run2 run3
+if grep client_max_body_size /etc/nginx/nginx.conf ; then 
+	echo "client_max_body_size already added" ;
+else
+	sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    80m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
+fi
+
+mysql -h localhost -u$USER -p$PASSWORD < src/install/db.sql
+echo "insert into jol.privilege values('admin','administrator','N');"|mysql -h localhost -u$USER -p$PASSWORD 
+
+sed -i "s:root /var/www/html;:root /home/judge/src/web;:g" /etc/nginx/sites-enabled/default
+sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
+sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
+sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
+sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
+sed -i "s:}#added_by_hustoj::g" /etc/nginx/sites-enabled/default
+sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
+sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/7.0/fpm/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php/7.0/fpm/php.ini
+
+COMPENSATION=`grep 'mips' /proc/cpuinfo|head -1|awk -F: '{printf("%.2f",$2/5000)}'`
+sed -i "s/OJ_CPU_COMPENSATION=1.0/OJ_CPU_COMPENSATION=$COMPENSATION/g" etc/judge.conf
+
+/etc/init.d/php7.0-fpm restart
+service php7.0-fpm restart
+
+cd src/core
+chmod +x ./make.sh
+./make.sh
+if grep "/usr/bin/judged" /etc/rc.local ; then
+	echo "auto start judged added!"
+else
+	sed -i "s/exit 0//g" /etc/rc.local
+	echo "/usr/bin/judged" >> /etc/rc.local
+	echo "exit 0" >> /etc/rc.local
+fi
+if grep "bak.sh" /var/spool/cron/crontabs/root ; then
+	echo "auto backup added!"
+else
+	echo "1 0 * * * /home/judge/src/install/bak.sh" >> /var/spool/cron/crontabs/root
+fi
+ln -s /usr/bin/mcs /usr/bin/gmcs
+
+/usr/bin/judged
+cp /home/judge/src/install/hustoj /etc/init.d/hustoj
+update-rc.d hustoj defaults

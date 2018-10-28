6d890cb26a9ae0415326ac073b0b31048b21cec7
张浩斌 <newsclan@gmail.com>
2018-10-28 09:58:58 +0800

    Update team_generate.php

--- trunk/web/admin/team_generate.php
+++ trunk/web/admin/team_generate.php
@@ -18,6 +18,7 @@ if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']))){
 		echo "<table border=1>";
 		echo "<tr><td colspan=3>Copy these accounts to distribute</td></tr>";
 		echo "<tr><td>team_name<td>login_id</td><td>password</td></tr>";
+		$max_length=20;
 		for($i=1;$i<=$teamnumber;$i++){
 			
         $user_id=$prefix.($i<10?('0'.$i):$i);
@@ -40,10 +41,14 @@ if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']))){
 			    $tmp_ip=explode(',',$REMOTE_ADDR);
 			    $ip =(htmlentities($tmp_ip[0],ENT_QUOTES,"UTF-8"));
 			}
+			
 			if(mb_strlen($nick,'utf-8')>20){
 				$new_len=mb_strlen($nick,'utf-8');
-				$longer="ALTER TABLE `users` MODIFY COLUMN `nick` varchar($new_len) NULL DEFAULT '' ";
-				pdo_query($longer);
+				if($new_len>$max_length){
+					$max_length=$new_len;
+					$longer="ALTER TABLE `users` MODIFY COLUMN `nick` varchar($max_length) NULL DEFAULT '' ";
+					pdo_query($longer);
+				}
 			}
 			$sql="INSERT INTO `users`("."`user_id`,`email`,`ip`,`accesstime`,`password`,`reg_time`,`nick`,`school`)".
 			"VALUES(?,?,?,NOW(),?,NOW(),?,?)on DUPLICATE KEY UPDATE `email`=?,`ip`=?,`accesstime`=NOW(),`password`=?,`reg_time`=now(),nick=?,`school`=?";

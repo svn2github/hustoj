62de01aca538fe8516fa822673ffd09779c1d012
张浩斌 <newsclan@gmail.com>
2018-12-31 13:19:35 +0800

    tips

--- trunk/install/makeout.sh
+++ trunk/install/makeout.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-g++ -o main main.cc
+g++  -o main main.cc
 for i in `find -name "*.in"`
 do 
-	./main< $i > `basename -s ".in" $i`.out
+	if	./main< $i > `basename -s ".in" $i`.out 
+	then
+		echo "$i ok.";
+	else
+		echo "$i error!"
+	fi
 done
 

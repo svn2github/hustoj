c851084fc793d8f94110d27ccb3c6c64ec3d7c0a
张浩斌 <newsclan@gmail.com>
2018-12-31 15:50:44 +0800

    const

--- trunk/web/category.php
+++ trunk/web/category.php
@@ -4,6 +4,7 @@
 	$OJ_CACHE_SHARE=false;
 	require_once('./include/cache_start.php');
         require_once('./include/db_info.inc.php');
+        require_once('./include/const.inc.php');
         require_once('./include/memcache.php');
 	require_once('./include/setlang.php');
 	$view_title= "Welcome To Online Judge";
--- trunk/web/problemset.php
+++ trunk/web/problemset.php
@@ -2,6 +2,7 @@
 	$OJ_CACHE_SHARE=false;
 	$cache_time=60;
 	require_once('./include/db_info.inc.php');
+	require_once('./include/const.inc.php');
 	require_once('./include/cache_start.php');
 	require_once('./include/memcache.php');
 	require_once('./include/setlang.php');

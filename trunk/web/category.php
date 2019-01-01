01ac78bda33a3911565e4ff9ab8cdb2ce204244a
张浩斌 <newsclan@gmail.com>
2019-01-01 09:26:16 +0800

    Update category.php

--- trunk/web/category.php
+++ trunk/web/category.php
@@ -32,6 +32,7 @@
 			if(trim($cat)=="") continue;
 			$hash_num=hexdec(substr(md5($cat),0,15));
 			$label_theme=$color_theme[$hash_num%count($color_theme)];
+			if($label_theme=="") $label_theme="default";
 			$view_category.= "<a class='label label-$label_theme' style='display: inline-block;' href='problemset.php?search=".urlencode(htmlentities($cat,ENT_QUOTES,'UTF-8'))."'>".$cat."</a>&nbsp;";
 		}
 		

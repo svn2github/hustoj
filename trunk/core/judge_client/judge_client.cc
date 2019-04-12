b0b39c2456e1dcdfd35d757d83d41ac5fc85dcea
张浩斌 <newsclan@gmail.com>
2019-04-12 13:43:40 +0800

    less files copy for python

--- trunk/core/judge_client/judge_client.cc
+++ trunk/core/judge_client/judge_client.cc
@@ -1872,9 +1872,11 @@ void copy_python_runtime(char *work_dir)
 	if(!py2){	
 		execute_cmd("cp /usr/bin/python2* %s/", work_dir);
 		execute_cmd("cp -a /usr/lib/python2* %s/usr/lib/", work_dir);
+		execute_cmd("cp -a /usr/lib64/python2* %s/usr/lib64/", work_dir);
 	}else{
 		execute_cmd("cp /usr/bin/python3* %s/", work_dir);
 		execute_cmd("cp -a /usr/lib/python3* %s/usr/lib/", work_dir);
+		execute_cmd("cp -a /usr/lib64/python3* %s/usr/lib64/", work_dir);
 	}
 #ifdef __mips__
 	execute_cmd("/bin/cp -a /lib64/libpthread.so.0 %s/lib64/", work_dir);
@@ -1888,7 +1890,6 @@ void copy_python_runtime(char *work_dir)
 
 
 #endif
-	execute_cmd("cp -a /usr/lib64/python* %s/usr/lib64/", work_dir);
 	execute_cmd("cp -a /usr/lib64/libpython* %s/usr/lib64/", work_dir);
 	execute_cmd("cp -a /usr/local/lib/python* %s/usr/local/lib/", work_dir);
 	execute_cmd("cp -a /usr/include/python* %s/usr/include/", work_dir);

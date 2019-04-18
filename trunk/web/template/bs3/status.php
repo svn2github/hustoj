20d7701fdb5bbb6f922bb8ca1e3eeb9fe324eb61
张浩斌 <newsclan@gmail.com>
2019-04-19 07:09:32 +0800

    issues416

--- trunk/web/template/bs3/status.php
+++ trunk/web/template/bs3/status.php
@@ -164,12 +164,12 @@ echo "<input type=submit class='form-control' value='$MSG_SEARCH'></form>";
 				</table>
 			</div>
 			<div id=center>
-				<?php echo "[<a href=status.php?".$str2.">Top</a>]&nbsp;&nbsp;";
+				<?php echo "[<a href='status.php?".$str2."'>Top</a>]&nbsp;&nbsp;";
 if (isset($_GET['prevtop']))
-echo "[<a href=status.php?".$str2."&top=".intval($_GET['prevtop']).">Previous Page</a>]&nbsp;&nbsp;";
+echo "[<a href='status.php?".$str2."&top=".intval($_GET['prevtop'])."'>Previous Page</a>]&nbsp;&nbsp;";
 else
-echo "[<a href=status.php?".$str2."&top=".($top+20).">Previous Page</a>]&nbsp;&nbsp;";
-echo "[<a href=status.php?".$str2."&top=".$bottom."&prevtop=$top>Next Page</a>]";
+echo "[<a href='status.php?".$str2."&top=".($top+20)."'>Previous Page</a>]&nbsp;&nbsp;";
+echo "[<a href='status.php?".$str2."&top=".$bottom."&prevtop=$top'>Next Page</a>]";
 ?>
 			</div>
 

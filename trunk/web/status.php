9f5ebd737771a396aa31d882b108a09f0500f49c
张浩斌 <newsclan@gmail.com>
2019-04-19 07:27:26 +0800

    pulls416

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -85,6 +85,7 @@ if (isset($_GET['problem_id'])&&$_GET['problem_id']!=""){
 	if(isset($_GET['cid'])){
 		$problem_id=htmlentities($_GET['problem_id'],ENT_QUOTES,'UTF-8');
 		$num=strpos($PID,$problem_id);
+		$problem_id=$PID[$num];
 		$sql=$sql."AND `num`='".$num."' ";
         $str2=$str2."&problem_id=".trim($problem_id);
         

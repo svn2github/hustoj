67c47312420a36f8974789574898a1f8e9ea55b3
张浩斌 <newsclan@gmail.com>
2019-04-30 12:00:25 +0800

    Update install-ubuntu18.04.sh

--- trunk/install/install-ubuntu18.04.sh
+++ trunk/install/install-ubuntu18.04.sh
@@ -34,7 +34,7 @@ sed -i "s/DB_USER=\"root\"/DB_USER=\"$USER\"/g" src/web/include/db_info.inc.php
 sed -i "s/DB_PASS=\"root\"/DB_PASS=\"$PASSWORD\"/g" src/web/include/db_info.inc.php
 chmod 700 src/web/include/db_info.inc.php
 chown www-data src/web/include/db_info.inc.php
-chown www-data src/web/upload data run0 run1 run2 run3
+chown www-data src/web/upload data
 if grep client_max_body_size /etc/nginx/nginx.conf ; then 
 	echo "client_max_body_size already added" ;
 else

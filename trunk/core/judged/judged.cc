6a3946a7f59313f7d54493190bbfc1a59800d006
张浩斌 <newsclan@gmail.com>
2019-05-09 15:16:06 +0800

    java ask more nproc

--- trunk/core/judged/judged.cc
+++ trunk/core/judged/judged.cc
@@ -232,7 +232,7 @@ void run_client(int runid, int clientid) {
 #endif
 	setrlimit(RLIMIT_AS, &LIM);
 
-	LIM.rlim_cur = LIM.rlim_max = 800;
+	LIM.rlim_cur = LIM.rlim_max = 800* max_running;
 	setrlimit(RLIMIT_NPROC, &LIM);
 
 	//buf[0]=clientid+'0'; buf[1]=0;

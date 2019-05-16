e255f1caa062af43fb8fb373da720e1ea407f965
张浩斌 <newsclan@gmail.com>
2019-05-16 18:31:40 +0800

    Update status.php

--- trunk/web/template/bs3/status.php
+++ trunk/web/template/bs3/status.php
@@ -201,6 +201,6 @@ echo "[<a href='status.php?".$str2."&top=".$bottom."&prevtop=$top'>Next Page</a>
 			''
 		];
 	</script>
-	<script src="template/<?php echo $OJ_TEMPLATE?>/auto_refresh.js?v=0.35"></script>
+	<script src="template/<?php echo $OJ_TEMPLATE?>/auto_refresh.js?v=0.36"></script>
 </body>
 </html>

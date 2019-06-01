02d7c6c88a82f1553c1af0a4bc619548c5df614f
张浩斌 <newsclan@gmail.com>
2019-06-01 15:04:22 +0800

    Update okcalls32.h

--- trunk/core/judge_client/okcalls32.h
+++ trunk/core/judge_client/okcalls32.h
@@ -18,7 +18,7 @@
  */
 //c & c++
 int LANG_CV[256] = { 3,4,8,11,33,45,85,122,140,146,192,197,243,252,272,SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
-		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
+		SYS_close, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
 		SYS_mmap2, SYS_fstat64, SYS_set_thread_area, 463, 0 };
 //pascal
 int LANG_PV[256] = { 0,9, 59, 97, 13, 16, 89, 140, 91, 175, 195, 13, SYS_open, SYS_set_thread_area,

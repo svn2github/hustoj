0b7d0d91e481724d5c12aaee8b9d1de47d8e46ad
张浩斌 <newsclan@gmail.com>
2019-09-23 09:07:09 +0800

    fixing nick sychronization on contests

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -213,9 +213,9 @@ for ($i=0;$i<$rows_cnt;$i++){
                 	$view_status[$i][1]= "<a href='contestrank.php?cid=".$row['contest_id']."&user_id=".$row['user_id']."#".$row['user_id']."'>".$row['user_id']."</a>";
         }else{
                 if (isset($_SESSION[$OJ_NAME.'_'.'administrator']))
-			$view_status[$i][1]= "<a href='userinfo.php?user=".$row['user_id']."' title='".$row['ip']."'>".$row['user_id']."</a>";
+			$view_status[$i][1]= "<a href='userinfo.php?user=".$row['user_id']."' title='".$row['nick']."[".$row['ip']."]'>".$row['user_id']."</a>";
 		else
-                	$view_status[$i][1]= "<a href='userinfo.php?user=".$row['user_id']."'>".$row['user_id']."</a>";
+                	$view_status[$i][1]= "<a href='userinfo.php?user=".$row['user_id']."' title='".$row['nick']."'>".$row['user_id']."</a>";
         }
 
        if ($row['contest_id']>0) {

28e8d9b44581e749a3c844a719499e51ec5d08d9
jizhuozhi <mjiuming@gmail.com>
2019-10-06 19:55:58 +0800

    Exipiring csrf token if check succeed

--- trunk/web/include/csrf_check.php
+++ trunk/web/include/csrf_check.php
@@ -9,6 +9,9 @@
 		  	http_response_code(403);
 		  	echo "Invalid csrf token";
 		  	exit;
+		} else {
+			$index = array_search($_POST['csrf'],$_SESSION[$OJ_NAME.'_'.'csrf_keys']);
+			array_splice($_SESSION[$OJ_NAME.'_'.'csrf_keys'], $index);
 		}
   	}
 ?>

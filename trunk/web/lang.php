e631d039f5e942ed9efaab854fe47c48bb5aa46f
jizhuozhi <mjiuming@gmail.com>
2019-11-16 20:30:16 +0800

    Storing session when changing language

--- trunk/web/lang.php
+++ trunk/web/lang.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$_SESSION[$OJ_NAME . '_' . 'OJ_LANG'] = $_GET['lang'];
 setcookie("lang", $_GET['lang'], time() + 604800);
 header("Location: " . $_GET['back'], true, 302);
 ?>

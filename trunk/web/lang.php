30e39353d57c9ebeeda3ec80156f31bd7b50ef45
张浩斌 <newsclan@gmail.com>
2019-11-18 20:30:24 +0800

    Update lang.php

--- trunk/web/lang.php
+++ trunk/web/lang.php
@@ -4,7 +4,10 @@ if (isset($_GET['lang']) && in_array($_GET['lang'], array("cn", "en", 'fa', 'ko'
     $_SESSION[$OJ_NAME . '_' . 'OJ_LANG'] = $_GET['lang'];
     setcookie("lang", $_GET['lang'], time() + 604800);
     if (isset($_GET['back']) && !empty($_GET['back'])) {
-        header("Location: " . $_GET['back'], true, 302);
+        ?>
+        <script>window.location.go(-1);</script>
+        <?php
+            exit(0);
     } else {
         header("Location: " . "index.php", true, 302);
     }

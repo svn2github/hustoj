b6cba561142cb177caab513d6b7f16f64658985f
jizhuozhi <mjiuming@gmail.com>
2019-11-18 22:00:15 +0800

    Using HTML5 history API to go back in any case

--- trunk/web/lang.php
+++ trunk/web/lang.php
@@ -3,16 +3,8 @@ if (isset($_GET['lang']) && in_array($_GET['lang'], array("cn", "en", 'fa', 'ko'
     session_start();
     $_SESSION[$OJ_NAME . '_' . 'OJ_LANG'] = $_GET['lang'];
     setcookie("lang", $_GET['lang'], time() + 604800);
-    if (isset($_GET['back']) && !empty($_GET['back'])) {
-        ?>
-        <script>window.history.go(-1);</script>
-        <?php
-            exit(0);
-    } else {
-        header("Location: " . "index.php", true, 302);
-    }
-} else {
-    http_response_code(400);
-    echo "Bad Request";
 }
 ?>
+<script>
+    window.history.back();
+</script>

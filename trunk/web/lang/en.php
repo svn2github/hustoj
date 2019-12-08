2d27c866f0b4770a7f3c5148b5c01f9a8f7843ed
张浩斌 <newsclan@gmail.com>
2019-12-08 13:00:06 +0800

    Update en.php

--- trunk/web/lang/en.php
+++ trunk/web/lang/en.php
@@ -199,9 +199,9 @@ $MSG_HELP_ONLINE= "view online users";
   $MSG_HELP_BALLOON_SCHOOL="School Field of the Printer/Balloon privileged Accout, will be used as filter in task list.";
   $MSG_WARNING_LOGIN_FROM_DIFF_IP="Login from a diffrent ip ";
   $MSG_WARNING_DURING_EXAM_NOT_ALLOWED=" during exam is not allowed ";
+  $MSG_WARNING_ACCESS_DENIED="I am sorry, You could not view this message! Because it's not belong to you, or Administrator won't show you.";
 
   $MSG_LOSTPASSWORD_MAILBOX="Input the Code sended to your email (Trash Box )";
   $MSG_LOSTPASSWORD_WILLBENEW="if it's correct, it will be the new password";
-
-
+  
  ?>

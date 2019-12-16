80e2dbf0653edd57b21ad686cf57b057dfd0ea50
张浩斌 <newsclan@gmail.com>
2019-12-16 10:01:18 +0800

    Update conteststatistics.php

--- trunk/web/conteststatistics.php
+++ trunk/web/conteststatistics.php
@@ -22,7 +22,7 @@ $row=$result[0];
 $title=$row[0];
 $end_time=strtotime($row[1]);
 if(time()<$end_time && stripos($title,$OJ_NOIP_KEYWORD)!==false){
-      $view_errors =  "<h2>NOIP contest !</h2>";
+      $view_errors =  "<h2>$MSG_NOIP_WARNING</h2>";
       require("template/".$OJ_TEMPLATE."/error.php");
       exit(0);
 }

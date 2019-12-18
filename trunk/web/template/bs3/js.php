02d604b4fd29179720b04d4321fb3fb67aaa69c4
张浩斌 <newsclan@gmail.com>
2019-12-18 17:53:48 +0800

    Update js.php

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -37,6 +37,12 @@ $(document).ready(function(){
   $("#csrf").load("<?php echo $path_fix?>csrf.php");
   $("body").append("<div id=footer class=center >GPLv2 licensed by <a href='https://github.com/zhblue/hustoj' >HUSTOJ</a> "+(new Date()).getFullYear()+" </div>");
   $("body").append("<div class=center > <img src='http://hustoj.com/wx.jpg' width='120px'><img src='http://hustoj.com/alipay.png' width='120px'><br> 欢迎关注微信公众号onlinejudge</div>");
+  
+  <?php if(isset($OJ_BEIAN)&&$OJ_BEIAN){ ?>
+         $("body").append("<br><center><a href='http://beian.miit.gov.cn/' target='_blank'><?php echo $OJ_BEIAN?></a></center>");
+  <?php } ?>
+
+	
   <?php 
 	if(isset($_SESSION[$OJ_NAME."_administrator"])) echo "admin_mod();";
   ?>

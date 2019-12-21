bf54a3f1d354187cbda801e1848f2e9c56bec832
张浩斌 <newsclan@gmail.com>
2019-12-21 10:06:21 +0800

    Update install-ubuntu16+.sh

--- trunk/install/install-ubuntu16+.sh
+++ trunk/install/install-ubuntu16+.sh
@@ -59,6 +59,7 @@ fi
 sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/7.0/fpm/php.ini
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php/7.0/fpm/php.ini
 sed -i 's/;request_terminate_timeout = 0/request_terminate_timeout = 128/g' `find /etc/php -name www.conf`
+sed -i 's/pm.max_children = 5/pm.max_children = 200/g' `find /etc/php -name www.conf`
  
 COMPENSATION=`grep 'mips' /proc/cpuinfo|head -1|awk -F: '{printf("%.2f",$2/5000)}'`
 sed -i "s/OJ_CPU_COMPENSATION=1.0/OJ_CPU_COMPENSATION=$COMPENSATION/g" etc/judge.conf

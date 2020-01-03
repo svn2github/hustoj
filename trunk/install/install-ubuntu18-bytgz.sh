a854e12cbdbb8b0af42c909e83efeb39eeafa53c
张浩斌 <newsclan@gmail.com>
2020-01-03 20:00:07 +0800

    Update install-ubuntu18-bytgz.sh

--- trunk/install/install-ubuntu18-bytgz.sh
+++ trunk/install/install-ubuntu18-bytgz.sh
@@ -15,7 +15,7 @@ else
     cd /home/judge/
 fi 
 
-apt-get install -y make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm nginx mysql-server php-mysql  php-common php-gd php-zip fp-compiler openjdk-11-jdk mono-devel php-mbstring php-xml
+apt-get install -y net-tools make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm nginx mysql-server php-mysql  php-common php-gd php-zip fp-compiler openjdk-11-jdk mono-devel php-mbstring php-xml
 
 USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`

9136c6e80c5c0a6a111ae4fd03ceb3c95da41b1c
张浩斌 <newsclan@gmail.com>
2020-01-16 14:34:33 +0800

    Update problem_list.php

--- trunk/web/admin/problem_list.php
+++ trunk/web/admin/problem_list.php
@@ -25,7 +25,7 @@ $row = $result[0];
 
 $ids = intval($row['ids']);
 
-$idsperpage = 25;
+$idsperpage = 50;
 $pages = intval(ceil($ids/$idsperpage));
 
 if(isset($_GET['page'])){ $page = intval($_GET['page']);}

bb2050a7843d676fe3890754da1495c50107d0fb
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-07 09:22:09 +0800

    Update footer-files.php

--- trunk/web/template/bshark/footer-files.php
+++ trunk/web/template/bshark/footer-files.php
@@ -1,10 +1,16 @@
 <script src="https://cdn.bootcss.com/KaTeX/0.10.2/katex.min.js"></script>
 <script src="https://cdn.bootcss.com/KaTeX/0.10.2/contrib/auto-render.min.js"></script>
-<script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdn.bootcss.com/popper.js/1.15.0/esm/popper.min.js"></script>
 <script src="https://cdn.bootcss.com/wangEditor/10.0.13/wangEditor.min.js"></script>
 <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="./template/bshark/shards/dist/js/shards.min.js"></script>
+<script src="/template/bshark/shards/dist/js/shards.min.js"></script>
+<?php 
+$url=basename($_SERVER['REQUEST_URI']);
+$realurl=basename($_SERVER['REQUEST_URI']);
+$url=str_replace(strrchr($url, "?"),"",$url); 
+if ($url != 'loginpage.php' && $url != 'registerpage.php') { ?>
+<script src="/template/bshark/main.js"></script>
+<?php } ?>
 <script>
     renderMathInElement(document.body,
    {

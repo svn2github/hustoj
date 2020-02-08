2bb0dd573c81022ffd9fce5d717ea693d2f25997
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-08 11:04:08 +0800

    Update contest.php

--- trunk/web/template/bshark/contest.php
+++ trunk/web/template/bshark/contest.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title><?php echo $view_title;?> - 竞赛&作业 - MasterOJ</title>
+        <title><?php echo $view_title;?> - <?php echo $MSG_CONTEST;?> - <?php echo $OJ_NAME;?></title>
         <?php require("./template/bshark/header-files.php");?>
     </head>
     
@@ -10,7 +10,7 @@
         <?php require("./template/bshark/nav.php");?>
         <div class="card" style="margin: 3% 8% 5% 8%">
   <div class="card-body">
-    <h4>C<?php echo $view_cid;?>: <?php echo $view_title;?>(<?php echo $view_type==0?'ACM':'OI';?>) <span class="badge badge-outline-<?php
+    <h4>C<?php echo $view_cid;?>: <?php echo $view_title;?> <span class="badge badge-outline-<?php
 if ($now>$end_time)
 echo "success";
 else if ($now<$start_time)
@@ -19,11 +19,11 @@ else
 echo "danger";
 ?>"><?php
 if ($now>$end_time)
-echo "已结束";
+echo $MSG_Ended;
 else if ($now<$start_time)
 echo "未开始";
 else
-echo "运行中";
+echo $MSG_Running;
 ?></span>
 <span class="badge badge-outline-<?php
 if ($view_private=='0')
@@ -32,29 +32,30 @@ else
 echo "danger";
 ?>"><?php
 if ($view_private=='0')
-echo "公开";
+echo $MSG_Public;
 else
-echo "私有";
+echo $MSG_Private;
 ?></span></h4>
-    <h5>比赛时间</h5>
+    <h5><?php echo $MSG_CONTEST;?><?php echo $MSG_TIME;?></h5>
     <span class="badge badge-outline-dark"><?php echo $view_start_time;?></span>~<span class="badge badge-outline-dark"><?php echo $view_end_time;?></span> 现在:<span class="badge badge-outline-dark" id="nowdate"></span>
     <br>
-    <h5>比赛介绍</h5>
+    <h5><?php echo $MSG_CONTEST.$MSG_Description;?></h5>
     <blockquote>
         <?php echo $view_description?>
     </blockquote>
     <hr/>
     <ul class="pagination">
-    <li class="page-item"><a class="page-link" href='contest.php?cid=<?php echo $view_cid?>'>竞赛C<?php echo $cid;?></a></li>
-    <li class="page-item"><a class="page-link" href='status.php?cid=<?php echo $view_cid?>'>状态</a></li>
-    <li class="page-item"><a class="page-link" href='contestrank.php?cid=<?php echo $view_cid?>'>排名</a></li>
-    <li class="page-item"><a class="page-link" href='conteststatistics.php?cid=<?php echo $view_cid?>'>统计</a></li>
+    <li class="page-item"><a class="page-link" href='contest.php?cid=<?php echo $view_cid?>'><?php echo $MSG_CONTEST;?>C<?php echo $cid;?></a></li>
+    <li class="page-item"><a class="page-link" href='status.php?cid=<?php echo $view_cid?>'><?php echo $MSG_STATUS;?></a></li>
+    <li class="page-item"><a class="page-link" href='contestrank.php?cid=<?php echo $view_cid?>'><?php echo $MSG_STANDING;?></a></li>
+    <li class="page-item"><a class="page-link" href='contestrank-oi.php?cid=<?php echo $view_cid?>'>OI-<?php echo $MSG_STANDING;?></a></li>
+    <li class="page-item"><a class="page-link" href='conteststatistics.php?cid=<?php echo $view_cid?>'><?php echo $MSG_STATISTICS;?></a></li>
     </ul>
     <?php if (time()>$start_time) { ?>
     <table id='problemset' class='table table-hover'>
 <thead>
 <tr align=center class='toprow'>
-<td>状态
+<td><?php echo $MSG_STATUS;?>
 <td style="cursor:hand" onclick="sortTable('problemset', 1, 'int');" ><?php echo $MSG_PROBLEM_ID?>
 <td><?php echo $MSG_TITLE?></td>
 <td><?php echo $MSG_SOURCE?></td>

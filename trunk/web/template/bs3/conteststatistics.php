f2a4cf3a874c26add3f15359e659757778261024
Jeon Hyun-seok <what_is_computer@msn.com>
2020-02-10 11:49:42 +0900

    Active clock
    Active clock

--- trunk/web/template/bs3/conteststatistics.php
+++ trunk/web/template/bs3/conteststatistics.php
@@ -314,5 +314,32 @@ grid: {
 });
 //alert((new Date()).getTime());
 </script>
+
+<script>
+  var diff = new Date("<?php echo date("Y/m/d H:i:s")?>").getTime()-new Date().getTime();
+  //alert(diff);
+  function clock() {
+    var x,h,m,s,n,xingqi,y,mon,d;
+    var x = new Date(new Date().getTime()+diff);
+    y = x.getYear()+1900;
+
+    if (y>3000)
+      y -= 1900;
+
+    mon = x.getMonth()+1;
+    d = x.getDate();
+    xingqi = x.getDay();
+    h = x.getHours();
+    m = x.getMinutes();
+    s = x.getSeconds();
+    n = y+"-"+(mon>=10?mon:"0"+mon)+"-"+(d>=10?d:"0"+d)+" "+(h>=10?h:"0"+h)+":"+(m>=10?m:"0"+m)+":"+(s>=10?s:"0"+s);
+
+    //alert(n);
+    document.getElementById('nowdate').innerHTML = n;
+    setTimeout("clock()",1000);
+  }
+  clock();
+</script>
+
 </body>
 </html>

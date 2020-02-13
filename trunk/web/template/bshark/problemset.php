f7fa1d62848874c6cba6ef2d755ead7943404846
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-13 16:20:56 +0800

    Update problemset.php

--- trunk/web/template/bshark/problemset.php
+++ trunk/web/template/bshark/problemset.php
@@ -110,12 +110,40 @@
 					</div>
             </div>
         </div>
-        <!--br>
+		<?php if ($THEME_SHOW_CATEGORY == true) { ?>
+        <br>
         <div class="card">
             <div class="card-body">
-                <h4></h4>
+                <h4><?php echo $MSG_SOURCE;?></h4>
+				<?php $view_category="";
+	$sql=	"select distinct source "
+			."FROM `problem` where defunct='N'"
+			."LIMIT 500";
+	$result=pdo_query($sql);
+	$category=array();
+    foreach ($result as $row){
+		$cate=explode(" ",$row['source']);
+		foreach($cate as $cat){
+			array_push($category,trim($cat));	
+		}
+	}
+	$category=array_unique($category);
+	if (!$result){
+		$view_category= "No Category Now!";
+	}else{
+		foreach ($category as $cat){
+			if(trim($cat)=="") continue;
+			$hash_num=hexdec(substr(md5($cat),0,7));
+			$label_theme=$color_theme[$hash_num%count($color_theme)];
+			if($label_theme=="") $label_theme="default";
+			$view_category.= "<a class='badge badge-$label_theme' href='problemset.php?search=".urlencode(htmlentities($cat,ENT_QUOTES,'UTF-8'))."'>".$cat."</a>&nbsp;";
+		}
+	}
+	echo $view_category;
+	?>
             </div>
-        </div-->
+        </div>
+		<?php } ?>
     </div>
 </div>
 <?php require("./template/bshark/footer.php");?>

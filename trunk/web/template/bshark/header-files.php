bdd3df8dfccc0cba507c6d4f6d61f122ecfec1e7
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-16 08:49:33 +0800

    Update header-files.php

--- trunk/web/template/bshark/header-files.php
+++ trunk/web/template/bshark/header-files.php
@@ -3,17 +3,18 @@ require("./template/$OJ_TEMPLATE/theme.conf.php");?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=auto" />
 
-<!-- KaTeX核心css，不启用 -->
-<!--link href="https://cdn.bootcss.com/KaTeX/0.10.2/katex.min.css" rel="stylesheet"-->
+<!-- KaTeX核心css -->
+<link href="https://cdn.bootcss.com/KaTeX/0.10.2/katex.min.css" rel="stylesheet">
 
 <!-- Bootstrap4核心css -->
 <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- iconfont图标css -->
-<link href="//at.alicdn.com/t/font_1198024_t50akptjdvl.css" rel="stylesheet">
+<link rel="stylesheet" href="./template/bshark/iconfont.css">
+<script src="./template/bshark/iconfont.js"></script>
 
 <!-- Shards核心文件 -->
-<link id="shardsop" rel="stylesheet" href="/template/bshark/shards/dist/css/shards.min.css">
+<link id="shardsop" rel="stylesheet" href="./template/bshark/shards/dist/css/shards.min.css">
 
 <!-- 主题核心css文件（light and dark） -->
 <link href="./template/bshark/main-<?php echo $THEME_MOD;?>.css" rel="stylesheet">

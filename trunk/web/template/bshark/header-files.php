d5ea626b6599529b380d2c9d3072aeb7d44803be
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-16 08:26:56 +0800

    Update header-files.php

--- trunk/web/template/bshark/header-files.php
+++ trunk/web/template/bshark/header-files.php
@@ -5,15 +5,11 @@ require("./template/$OJ_TEMPLATE/theme.conf.php");?>
 <link href="https://cdn.bootcss.com/KaTeX/0.10.2/katex.min.css" rel="stylesheet">
 <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="//at.alicdn.com/t/font_1198024_t50akptjdvl.css" rel="stylesheet">
-<script src="/template/bshark/NProgress.js"></script>
-<link href="/template/bshark/NProgress.css" rel="stylesheet">
 <link id="shardsop" rel="stylesheet" href="/template/bshark/shards/dist/css/shards.min.css">
 <link href="/template/bshark/main-<?php echo $THEME_MOD;?>.css" rel="stylesheet">
-<link href="/template/bshark/animated.css" rel="stylesheet">
-<link href="https://cdn.bootcss.com/highlight.js/8.0/styles/tomorrow<?php if ($THEME_MOD=='dark') echo '-night';?>.min.css" rel="stylesheet">
+<link href="https://cdn.bootcss.com/highlight.js/8.0/styles/tomorrow.min.css" rel="stylesheet">
 <script src="https://cdn.bootcss.com/highlight.js/8.0/highlight.min.js"></script>
 <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
-<link href="https://cdn.bootcss.com/semantic-ui/2.4.1/components/icon.css" rel="stylesheet">
 <script>
 hljs.initHighlightingOnLoad();
 </script>

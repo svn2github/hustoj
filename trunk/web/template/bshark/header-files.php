2f05aecc91462e4a26050a3b983503c40586c569
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-17 09:57:58 +0800

    Update header-files.php

--- trunk/web/template/bshark/header-files.php
+++ trunk/web/template/bshark/header-files.php
@@ -7,27 +7,27 @@ require("./template/$OJ_TEMPLATE/theme.conf.php");?>
 <!--link href="https://cdn.bootcss.com/KaTeX/0.10.2/katex.min.css" rel="stylesheet"-->
 
 <!-- Bootstrap4核心css -->
-<link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="./template/bshark/bootstrap4/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- iconfont图标css -->
 <link rel="stylesheet" href="./template/bshark/iconfont.css">
 <script src="./template/bshark/iconfont.js"></script>
 
 <!-- Shards核心文件 -->
-<link id="shardsop" rel="stylesheet" href="./template/bshark/shards/dist/css/shards.min.css">
+<link id="shardsop" rel="stylesheet" href="./template/bshark/shards/css/shards.min.css">
 
 <!-- 主题核心css文件（light and dark） -->
 <link href="./template/bshark/main-<?php echo $THEME_MOD;?>.css" rel="stylesheet">
 
 <!-- Highlightjs 核心文件 -->
-<link href="https://cdn.bootcss.com/highlight.js/8.0/styles/tomorrow.min.css" rel="stylesheet">
-<script src="https://cdn.bootcss.com/highlight.js/8.0/highlight.min.js"></script>
+<link href="./template/bshark/highlight/styles/tomorrow.css" rel="stylesheet">
+<script src="./template/bshark/highlight/highlight.pack.js"></script>
 
 <!-- Jquery核心文件 -->
-<script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
+<script src="./template/bshark/jquery-3.4.1.min.js"></script>
 <script>
 hljs.initHighlightingOnLoad();
 </script>
 
 <!-- ChartJs 核心文件 -->
-<script src="https://cdn.bootcss.com/Chart.js/2.7.3/Chart.bundle.js"></script>
+<script src="./template/bshark/chartjs/Chart.bundle.js"></script>

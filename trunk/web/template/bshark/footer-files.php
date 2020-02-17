71a39232f79f618beff45ad79cd1ea0cfb95d6a5
Yemaster <30290923+yemaster@users.noreply.github.com>
2020-02-17 09:59:36 +0800

    Update footer-files.php

--- trunk/web/template/bshark/footer-files.php
+++ trunk/web/template/bshark/footer-files.php
@@ -3,11 +3,11 @@
 <!--script src="https://cdn.bootcss.com/KaTeX/0.10.2/contrib/auto-render.min.js"></script-->
 
 <!-- WangEditor编辑器文件-->
-<script src="https://cdn.bootcss.com/wangEditor/10.0.13/wangEditor.min.js"></script>
+<script src="./template/bshark/wangEditor/wangEditor.min.js"></script>
 
 <!-- Bootstrap4 和 shards -->
-<script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="/template/bshark/shards/dist/js/shards.min.js"></script>
+<script src="./template/bshark/bootstrap4/js/bootstrap.min.js"></script>
+<script src="./template/bshark/shards/js/shards.min.js"></script>
 
 <!-- 主题核心js -->
 <?php 
@@ -15,7 +15,7 @@ $url=basename($_SERVER['REQUEST_URI']);
 $realurl=basename($_SERVER['REQUEST_URI']);
 $url=str_replace(strrchr($url, "?"),"",$url); 
 if ($url != 'loginpage.php' && $url != 'registerpage.php') { ?>
-<script src="/template/bshark/main.js"></script>
+<script src="./template/bshark/main.js"></script>
 <?php } ?>
 
 <!-- KaTeX AutoRender 开启后用$就能引用数学公式 -->

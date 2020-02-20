5c29636b0518ee0d379acf121bdc64a199eb52f9
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-02-20 11:41:16 +0800

    Update problemstatus.php
    css changed.

--- trunk/web/template/mario/problemstatus.php
+++ trunk/web/template/mario/problemstatus.php
@@ -2,7 +2,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php echo $view_title?></title>
-	<link rel=stylesheet href='./template/<?php echo $OJ_TEMPLATE?>/<?php echo isset($OJ_CSS)?$OJ_CSS:"hoj.css" ?>' type='text/css'>
+	<link rel=stylesheet href='./template/<?php echo $OJ_TEMPLATE?>/mario.css' type='text/css'>
 </head>
 <body>
 <script type="text/javascript" src="include/wz_jsgraphics.js"></script>

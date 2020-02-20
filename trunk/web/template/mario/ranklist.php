ebe4d5b32364809351c951114e5a774876fd0fc2
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-02-20 16:06:16 +0800

    Update ranklist.php
    add some new mushrooms and Lang update

--- trunk/web/template/mario/ranklist.php
+++ trunk/web/template/mario/ranklist.php
@@ -8,6 +8,7 @@
 <div id="wrapper">
 	<?php require_once("oj-header.php");?>
 <div id=main>
+	<h1><?php echo $OJ_NAME.' '.$MSG_RANKLIST?></h1>
 	<table align=center width=90%>
 		<thead>
 		<tr><td colspan=3 align=left>
@@ -15,10 +16,10 @@
 				<?php echo $MSG_USER?><input name=user>
 				<input type=submit value=Go>
 			</form></td><td colspan=3 align=right>
-	                <a href=ranklist.php?scope=d>日排行</a>
-			<a href=ranklist.php?scope=w>周排行</a>
-			<a href=ranklist.php?scope=m>月排行</a>
-			<a href=ranklist.php?scope=y>年排行</a>
+	                <a href=ranklist.php?scope=d>$MSG_DAY</a>
+			<a href=ranklist.php?scope=w>$MSG_WEEK</a>
+			<a href=ranklist.php?scope=m>$MSG_MONTH</a>
+			<a href=ranklist.php?scope=y>$MSG_YEAR</a>
 			</td></tr>
 		<tr class='toprow'>
 				<td width=5% align=center><b><?php echo $MSG_Number?></b>
@@ -37,18 +38,19 @@
 					echo "<tr class='oddrow'>";
 				else
 					echo "<tr class='evenrow'>";
+				$cnt2=1;
 				foreach($row as $table_cell){
 					echo "<td>";
-					if($cnt==1&&$table_cell==1) echo"<img src='/template/mario/image/red.png' height=43px>";
-					else if($cnt==2&&$table_cell==2) echo"<img src='/template/mario/image/logo_l.jpg' height=43px>";
-					else if($cnt==3&&$table_cell==3) echo"<img src='/template/mario/image/logo_r.jpg' height=43px>";
+					if($cnt==1&&$cnt2==1) echo"<img src='/template/mario/image/red.png' height=43px>";
+					else if($cnt==2&&$cnt2==2) echo"<img src='/template/mario/image/logo.png' height=43px>";
+					else if($cnt==3&&$cnt2==3) echo"<img src='/template/mario/image/green.png' height=43px>";
 					else echo"<img src='/template/mario/image/green.png' height=43px>";
 					echo "\t".$table_cell;
-					echo "</td>";
+					echo "</td>"; $cnt2++;
 				}
 				
 				echo "</tr>";
-				$cnt1=$cnt1+1;
+				$cnt1++;
 				$cnt=1-$cnt;
 			}
 			?>

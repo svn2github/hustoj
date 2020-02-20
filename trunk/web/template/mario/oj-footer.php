9e20383e542d5481ad360679cf2bc3655fedd4a2
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-02-20 17:14:25 +0800

    Update oj-footer.php
    update

--- trunk/web/template/mario/oj-footer.php
+++ trunk/web/template/mario/oj-footer.php
@@ -30,9 +30,8 @@
 		})()
 	</script>
 		<br>
-		All Copyright Reserved 2010-2011 <a href='<?php echo $OJ_HOME?>'><?php echo $OJ_NAME?></a> TEAM<br>
-		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 2003-2012 <a href='http://code.google.com/p/hustoj/'>HUSTOJ Project</a> TEAM<br>
-		
+		<a href=gpl-2.0.txt><span class=green>GPL2.0</span></a> 20012-2022 licensed by<a href='https://www.github.com/zhblue/hustoj/'>HUSTOJ Project</a><br>
+		<a herf="https://www.github.com/MX-Qulin">Mario Template By MX-Qulin</a> 
      <?php if ($OJ_SAE) {
                    echo "<a href=http://sae.sina.com.cn><img bolder=0 src=http://static.sae.sina.com.cn/image/poweredby/poweredby.png></a>";
             

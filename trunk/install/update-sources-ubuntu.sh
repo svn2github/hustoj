2dfaa4136ab9232efcc25719d17d9478a429e360
任宝硕 <47095648+renbaoshuo@users.noreply.github.com>
2020-04-04 12:50:55 +0800

    Ubuntu 批量更换软件源脚本

--- trunk/install/update-sources-ubuntu.sh
+++ trunk/install/update-sources-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i s/http\:\/\/cn.archive.ubuntu.com/https\:\/\/mirrors.aliyun.com/g /etc/apt/sources.list
-sed -i s/http\:\/\/archive.ubuntu.com/https\:\/\/mirrors.aliyun.com/g    /etc/apt/sources.list
-sed -i s/http\:\/\/security.ubuntu.com/https\:\/\/mirrors.aliyun.com/g   /etc/apt/sources.list
+sed -i 's/http\:\/\/cn.archive.ubuntu.com/https\:\/\/mirrors.aliyun.com/g' /etc/apt/sources.list
+sed -i 's/http\:\/\/archive.ubuntu.com/https\:\/\/mirrors.aliyun.com/g'    /etc/apt/sources.list
+sed -i 's/http\:\/\/security.ubuntu.com/https\:\/\/mirrors.aliyun.com/g'   /etc/apt/sources.list
 
 apt update

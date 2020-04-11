aee480898daa06adee1d0ec762e90a71514aecad
Zhuozhi Ji <mjiuming@gmail.com>
2020-04-11 14:29:58 +0800

    Use env to config web in docker

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,9 +1,27 @@
 set -xe
 
-sed -i "s#DB_HOST=\"localhost\"#DB_HOST=\"$HOSTNAME\"#g"    /home/judge/src/web/include/db_info.inc.php
-sed -i "s#DB_NAME=\"jol\"#DB_NAME=\"$DATABASE\"#g"          /home/judge/src/web/include/db_info.inc.php
-sed -i "s#DB_USER=\"root\"#DB_USER=\"$USERNAME\"#g"         /home/judge/src/web/include/db_info.inc.php
-sed -i "s#DB_PASS=\"root\"#DB_PASS=\"$PASSWORD\"#g"         /home/judge/src/web/include/db_info.inc.php
+setpropertiy(){
+  if [ $# -ge 1 ];then
+    key=$1
+    value=`printenv $key || true`
+    if [ $# -ge 2 ];then
+      quate=$2
+    fi
+    if [ -n "$value" ];then
+      oldvalue=`cat /home/judge/src/web/include/db_info.inc.php | grep $key | awk -F '=' '{print $2}' | awk -F ';' '{print $1}' | awk -F '"' '{print $2}'`
+      if [ $quate == "quate" ] || [$quate == "true"];then
+        sed -i "s#$key=\"$oldvalue\"#$key=\"$value\"#g"   /home/judge/src/web/include/db_info.inc.php
+      else
+        sed -i "s#$key=$oldvalue#$key=$value#g"           /home/judge/src/web/include/db_info.inc.php
+      fi
+    fi
+  fi
+}
+
+setpropertiy DB_HOST quate
+setpropertiy DB_NAME quate
+setpropertiy DB_USER quate
+setpropertiy DB_PASS quate
 
 chown -R www-data:www-data /home/judge/data
 

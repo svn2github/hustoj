4c1504d558b7d5a6f61b3a8ab5636bf0cd77af17
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-07-13 16:48:04 +0800

    Update loginpage.php

--- trunk/web/loginpage.php
+++ trunk/web/loginpage.php
@@ -11,7 +11,7 @@ require_once('./include/cache_start.php');
 }
 
 /////////////////////////Template
-if($OJ_COOKIE_LOGIN=true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"])){
+if($OJ_LONG_LOGIN==true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"])){
 	require("./index.php");
 	echo"<script>let xhr=new XMLHttpRequest();xhr.open('GET','login.php',true);xhr.send();</script>";
 }

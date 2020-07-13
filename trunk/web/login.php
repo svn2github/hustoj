02a29445000aa50128d9a55a3231a0494c29a0d4
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-07-13 12:32:48 +0800

    Update login.php

--- trunk/web/login.php
+++ trunk/web/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once( "./include/db_info.inc.php" );
 require_once( './include/setlang.php' );
-$vcode = "";
+$C_success=false;
 if($OJ_COOKIE_LOGIN=true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"])){
 	$C_check=$_COOKIE[$OJ_NAME."_check"]; $C_user=$_COOKIE[$OJ_NAME."_user"];
 	if(array_pop($C_check)!=strlen($C_check)-1){
@@ -13,30 +13,36 @@ if($OJ_COOKIE_LOGIN=true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_
 		$tp=ord($C_info[0][i]);
 		$C_res+=chr(($tp*$tp+$C_info[1][$i % $C_len]*$tp)%127);
 	}
-	
-	$C_time=time()+86400*$OJ_KEEP_TIME;
-	setcookie($OJ_NAME."_user",$login,$C_time);
-	setcookie($OJ_NAME."_check",$C_res,$C_time);
+	if($C_check==$C_res){ 
+		$C_success=true; 
+		$login=$C_user; 
+	}
+	else{   echo "<script>\n alert('Cookie失效或错误!'); \n history.go(-1); \n </script>";
+		exit(0);
+	}
 }
-if ( isset( $_POST[ 'vcode' ] ) )$vcode = trim( $_POST[ 'vcode' ] );
-if ( $OJ_VCODE && ( $vcode != $_SESSION[ $OJ_NAME . '_' . "vcode" ] || $vcode == "" || $vcode == null ) ) {
+$vcode="";
+if((!isset($C_success)||$C_success!=true)&&!isset($_COOKIE[$OJ_NAME."_user"])&&!isset($_COOKIE[$OJ_NAME."_check"])){
+  if(isset($_POST[ 'vcode' ]))$vcode=trim($_POST['vcode']);
+  if($OJ_VCODE&&( $vcode != $_SESSION[ $OJ_NAME . '_' . "vcode" ] || $vcode == "" || $vcode == null ) ) {
 	echo "<script language='javascript'>\n";
 	echo "alert('Verify Code Wrong!');\n";
 	echo "history.go(-1);\n";
 	echo "</script>";
 	exit( 0 );
-}
-$view_errors = "";
-require_once( "./include/login-" . $OJ_LOGIN_MOD . ".php" );
-$user_id = $_POST[ 'user_id' ];
-$password = $_POST[ 'password' ];
-if ( get_magic_quotes_gpc() ) {
+  }
+  $view_errors = "";
+  require_once( "./include/login-" . $OJ_LOGIN_MOD . ".php" );
+  $user_id = $_POST[ 'user_id' ];
+  $password = $_POST[ 'password' ];
+  if ( get_magic_quotes_gpc() ) {
 	$user_id = stripslashes( $user_id );
 	$password = stripslashes( $password );
+  }
+  $sql = "SELECT `rightstr` FROM `privilege` WHERE `user_id`=?";
+  $login = check_login( $user_id, $password );
 }
-$sql = "SELECT `rightstr` FROM `privilege` WHERE `user_id`=?";
-$login = check_login( $user_id, $password );
-if ( $login ) {
+if($login){
 	$_SESSION[ $OJ_NAME . '_' . 'user_id' ] = $login;
 	$result = pdo_query( $sql, $login );
 

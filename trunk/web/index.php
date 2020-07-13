bad348d987cbc2535c94d1e665b6f1f31d36fd30
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-07-13 16:46:42 +0800

    Update index.php

--- trunk/web/index.php
+++ trunk/web/index.php
@@ -69,7 +69,7 @@ if ( isset( $_SESSION[ $OJ_NAME . '_' . 'administrator' ] ) ) {
 
 /////////////////////////Template
 require( "template/" . $OJ_TEMPLATE . "/index.php" );
-if(!isset($_SESSION[$OJ_NAME.'_user_id'])&&$OJ_LONG_LOGIN=true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"]))echo"<script>let xhr=new XMLHttpRequest();xhr.open('GET','login.php',true);xhr.send();</script>";
+if((!isset($_SESSION[$OJ_NAME.'_user_id']))&&$OJ_LONG_LOGIN==true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"]))echo"<script>let xhr=new XMLHttpRequest();xhr.open('GET','login.php',true);xhr.send();</script>";
 
 /////////////////////////Common foot
 if ( file_exists( './include/cache_end.php' ) )

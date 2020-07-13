fb9b3ba2400846d0298eaacf3009f45eba6ad0da
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-07-13 14:23:44 +0800

    Update logout.php

--- trunk/web/logout.php
+++ trunk/web/logout.php
@@ -1,5 +1,7 @@
 <?php session_start();
 unset($_SESSION[$OJ_NAME.'_'.'user_id']);
 session_destroy();
+setcookie($OJ_NAME."_user",'');
+setcookie($OJ_NAME."_check",'');
 header("Location:index.php");
 ?>

5b12132c29f0dea96cd39364ce029ad4cc2c22dd
张浩斌 <newsclan@gmail.com>
2020-09-22 17:32:36 +0800

    Update problem_export_xml.php

--- trunk/web/admin/problem_export_xml.php
+++ trunk/web/admin/problem_export_xml.php
@@ -213,7 +213,10 @@ if (isset($_POST ['do'])||isset($_GET['cid'])) {
 	}
   
 	?>
-   
+<!DOCTYPE fps PUBLIC 
+  "-//freeproblemset//An opensource XML standard for Algorithm Contest Problem Set//EN"
+  "http://hustoj.com/fps.current.dtd" >
+
 <fps version="1.2" url="https://github.com/zhblue/freeproblemset/">
 	<generator name="HUSTOJ" url="https://github.com/zhblue/hustoj/"/>
 	<?php

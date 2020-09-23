30e3e32b1dede83e3915463a45a0050ce27be449
张浩斌 <newsclan@gmail.com>
2020-09-23 12:25:17 +0800

    Update install-debian10.sh

--- trunk/install/install-debian10.sh
+++ trunk/install/install-debian10.sh
@@ -3,8 +3,11 @@ apt-get update
 apt-get install -y subversion
 /usr/sbin/useradd -m -u 1536 judge
 cd /home/judge/
-
-svn co https://github.com/zhblue/hustoj/trunk/trunk/  src
+#using tgz src files
+wget -O hustoj.tar.gz http://dl.hustoj.com/hustoj.tar.gz
+tar xzf hustoj.tar.gz
+svn up src
+#svn co https://github.com/zhblue/hustoj/trunk/trunk/  src
 for PKG in make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm nginx mysql-server php-mysql  php-common php-gd php-zip fp-compiler openjdk-11-jdk mono-devel php-mbstring php-xml mariadb-server libmariadb-dev libmariadbclient-dev libmariadb-dev default-libmysqlclient-dev
 do
    apt-get install -y $PKG 

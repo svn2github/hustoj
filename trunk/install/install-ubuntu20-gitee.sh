e7f11d0d93c2e81bdd5b66879eb3625b112b3174
张浩斌 <newsclan@gmail.com>
2020-10-11 08:33:05 +0800

    Create install-ubuntu20-gitee.sh

---
+++ trunk/install/install-ubuntu20-gitee.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+sed -i 's/tencentyun/aliyun/g' /etc/apt/sources.list
+
+apt-get update
+apt-get install -y subversion
+/usr/sbin/useradd -m -u 1536 judge
+cd /home/judge/ || exit
+
+#svn co https://github.com/zhblue/hustoj/trunk/trunk/  src
+
+git clone https://gitee.com/zhblue/hustoj.git git
+cp -a git/trunk src
+
+for pkg in net-tools make flex g++ clang libmysqlclient-dev libmysql++-dev php-fpm nginx mysql-server php-mysql  php-common php-gd php-zip fp-compiler openjdk-11-jdk mono-devel php-mbstring php-xml php-curl php-intl php-xmlrpc php-soap
+do
+	while ! apt-get install -y "$pkg" 
+	do
+		echo "Network fail, retry... you might want to change another apt source for install"
+	done
+done
+
+USER=$(grep user /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
+PASSWORD=$(grep password /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
+CPU=$(grep "cpu cores" /proc/cpuinfo |head -1|awk '{print $4}')
+
+mkdir etc data log backup
+
+cp src/install/java0.policy  /home/judge/etc
+cp src/install/judge.conf  /home/judge/etc
+chmod +x src/install/ans2out
+
+if grep "OJ_SHM_RUN=0" etc/judge.conf ; then
+	mkdir run0 run1 run2 run3
+	chown judge run0 run1 run2 run3
+fi
+
+sed -i "s/OJ_USER_NAME=root/OJ_USER_NAME=$USER/g" etc/judge.conf
+sed -i "s/OJ_PASSWORD=root/OJ_PASSWORD=$PASSWORD/g" etc/judge.conf
+sed -i "s/OJ_COMPILE_CHROOT=1/OJ_COMPILE_CHROOT=0/g" etc/judge.conf
+sed -i "s/OJ_RUNNING=1/OJ_RUNNING=$CPU/g" etc/judge.conf
+
+chmod 700 backup
+chmod 700 etc/judge.conf
+
+sed -i "s/DB_USER[[:space:]]*=[[:space:]]*\"root\"/DB_USER=\"$USER\"/g" src/web/include/db_info.inc.php
+sed -i "s/DB_PASS[[:space:]]*=[[:space:]]*\"root\"/DB_PASS=\"$PASSWORD\"/g" src/web/include/db_info.inc.php
+chmod 700 src/web/include/db_info.inc.php
+chown -R www-data src/web/
+chown www-data:judge src/web/upload
+chown www-data:judge data
+chmod 711 -R data
+if grep "client_max_body_size" /etc/nginx/nginx.conf ; then 
+	echo "client_max_body_size already added" ;
+else
+	sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    80m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
+fi
+
+mysql -h localhost -u"$USER" -p"$PASSWORD" < src/install/db.sql
+echo "insert into jol.privilege values('admin','administrator','N');"|mysql -h localhost -u"$USER" -p"$PASSWORD" 
+
+if grep "added by hustoj" /etc/nginx/sites-enabled/default ; then
+	echo "default site modified!"
+else
+	echo "modify the default site"
+	sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
+	sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
+	sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
+	sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
+	sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
+	sed -i "s:}#added by hustoj::g" /etc/nginx/sites-enabled/default
+	sed -i "s:php7.0:php7.4:g" /etc/nginx/sites-enabled/default
+	sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
+fi
+/etc/init.d/nginx restart
+sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/7.4/fpm/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php/7.4/fpm/php.ini
+WWW_CONF=$(find /etc/php -name www.conf)
+sed -i 's/;request_terminate_timeout = 0/request_terminate_timeout = 128/g' "$WWW_CONF"
+sed -i 's/pm.max_children = 5/pm.max_children = 200/g' "$WWW_CONF"
+ 
+COMPENSATION=$(grep 'mips' /proc/cpuinfo|head -1|awk -F: '{printf("%.2f",$2/5000)}')
+sed -i "s/OJ_CPU_COMPENSATION=1.0/OJ_CPU_COMPENSATION=$COMPENSATION/g" etc/judge.conf
+
+PHP_FPM=$(find /etc/init.d/ -name "php*-fpm")
+$PHP_FPM restart
+PHP_FPM=$(service --status-all|grep php|awk '{print $4}')
+if [ "$PHP_FPM" != ""  ]; then service "$PHP_FPM" restart ;else echo "NO PHP FPM";fi;
+
+cd src/core || exit 
+chmod +x ./make.sh
+./make.sh
+if grep "/usr/bin/judged" /etc/rc.local ; then
+	echo "auto start judged added!"
+else
+	sed -i "s/exit 0//g" /etc/rc.local
+	echo "/usr/bin/judged" >> /etc/rc.local
+	echo "exit 0" >> /etc/rc.local
+fi
+if grep "bak.sh" /var/spool/cron/crontabs/root ; then
+	echo "auto backup added!"
+else
+	crontab -l > conf && echo "1 0 * * * /home/judge/src/install/bak.sh" >> conf && crontab conf && rm -f conf
+fi
+ln -s /usr/bin/mcs /usr/bin/gmcs
+
+/usr/bin/judged
+cp /home/judge/src/install/hustoj /etc/init.d/hustoj
+update-rc.d hustoj defaults
+systemctl enable hustoj
+systemctl enable nginx
+systemctl enable mysql
+systemctl enable php7.4-fpm
+#systemctl enable judged
+
+mkdir /var/log/hustoj/
+chown www-data -R /var/log/hustoj/
+
+cls
+reset
+
+echo "Remember your database account for HUST Online Judge:"
+echo "username:$USER"
+echo "password:$PASSWORD"

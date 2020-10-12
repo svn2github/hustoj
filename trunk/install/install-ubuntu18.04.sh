d93dbcf63e90c9d1a022b639f0caf07c4f01786a
张浩斌 <newsclan@gmail.com>
2020-10-12 16:33:10 +0800

    Update install-ubuntu18.04.sh

--- trunk/install/install-ubuntu18.04.sh
+++ trunk/install/install-ubuntu18.04.sh
@@ -45,8 +45,13 @@ chmod 700 etc/judge.conf
 
 sed -i "s/DB_USER[[:space:]]*=[[:space:]]*\"root\"/DB_USER=\"$USER\"/g" src/web/include/db_info.inc.php
 sed -i "s/DB_PASS[[:space:]]*=[[:space:]]*\"root\"/DB_PASS=\"$PASSWORD\"/g" src/web/include/db_info.inc.php
+
 chmod 700 src/web/include/db_info.inc.php
 chown -R www-data src/web/
+
+chown -R root:root src/web/.svn
+chmod 750 -R src/web/.svn
+
 chown www-data src/web/upload data
 if grep "client_max_body_size" /etc/nginx/nginx.conf ; then 
 	echo "client_max_body_size already added" ;

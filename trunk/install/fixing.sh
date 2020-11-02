74d3af0af097ee77094f02bcacdbce72467be745
张浩斌 <newsclan@gmail.com>
2020-11-02 11:10:12 +0800

    Update fixing.sh

--- trunk/install/fixing.sh
+++ trunk/install/fixing.sh
@@ -4,18 +4,19 @@ USER=`cat /etc/mysql/debian.cnf |grep user|head -1|awk  '{print $3}'`
 PASSWORD=`cat /etc/mysql/debian.cnf |grep password|head -1|awk  '{print $3}'`
 if [ `whoami` = "root" ];then
 	cd /home/judge/
-	svn co https://github.com/zhblue/hustoj/trunk/trunk new
-	if test -e /home/judge/src/web/include/db_info.inc.php ;then
-	    echo 'db_info.inc.php exists !';
-	    cp src/web/include/db_info.inc.php new/web/include/
-	else
-	   echo 'db_info.inc.php not found';
-	   sed -i "s/DB_USER[[:space:]]*=[[:space:]]*\"root\"/DB_USER=\"$USER\"/g" new/web/include/db_info.inc.php
-	   sed -i "s/DB_PASS[[:space:]]*=[[:space:]]*\"root\"/DB_PASS=\"$PASSWORD\"/g" new/web/include/db_info.inc.php
+	if svn co https://github.com/zhblue/hustoj/trunk/trunk new ; then
+		if test -e /home/judge/src/web/include/db_info.inc.php ;then
+		    echo 'db_info.inc.php exists !';
+		    cp src/web/include/db_info.inc.php new/web/include/
+		else
+		   echo 'db_info.inc.php not found';
+		   sed -i "s/DB_USER[[:space:]]*=[[:space:]]*\"root\"/DB_USER=\"$USER\"/g" new/web/include/db_info.inc.php
+		   sed -i "s/DB_PASS[[:space:]]*=[[:space:]]*\"root\"/DB_PASS=\"$PASSWORD\"/g" new/web/include/db_info.inc.php
+		fi
+		cp -a src/web/upload/* new/web/upload/
+		mv src "old.$DATE"
+		mv new src
 	fi
-	cp -a src/web/upload/* new/web/upload/
-	mv src "old.$DATE"
-	mv new src
 	chown www-data:www-data -R src
 else
 	echo "usage: sudo $0"

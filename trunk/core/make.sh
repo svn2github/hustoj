1bca1da3082f68ef35fca409d64f2a4e34c9df8d
张浩斌 <newsclan@gmail.com>
2020-11-09 07:49:51 +0800

    Update make.sh

--- trunk/core/make.sh
+++ trunk/core/make.sh
@@ -20,5 +20,5 @@ cp sim_lisp.exe /usr/bin/sim_scm
 cd ..
 cp sim.sh /usr/bin
 chmod +x /usr/bin/sim.sh
-ln -fs /usr/bin/sim_c /usr/bin/sim_cc 2>&1 > /dev/null
+#ln -fs /usr/bin/sim_c /usr/bin/sim_cc 2>&1 > /dev/null
 echo "done!"

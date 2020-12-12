4c92f19137d224236a9663e3c071895c4c2b6823
melongist <what_is_computer@msn.com>
2020-12-13 07:39:57 +0900

    problemstatus page update
    problemstatus page update

--- trunk/web/template/bs3/problemstatus.php
+++ trunk/web/template/bs3/problemstatus.php
@@ -25,45 +25,64 @@
 
 	<div class="container">
 		<?php include("template/$OJ_TEMPLATE/nav.php");?>
+
 		<!-- Main component for a primary marketing message or call to action -->
 		<div class="jumbotron">
-			<h1>Problem <?php echo $id ?> Status</h1>
+
 			<center>
-				<table>
+			<h3><?php echo $MSG_PROBLEM." : ".$id." ".$MSG_STATISTICS?></h3>
+				<hr>
+				<table align=center width=70%>
 					<tr>
 						<td>
-							<table id='statics'>
+
+							<table id="statics" class="table-hover table-striped" align=center width=90%>
 								<?php
 								$cnt = 0;
-								foreach ( $view_problem as $row ) {
-									if ( $cnt )
+								foreach ($view_problem as $row) {
+									if ($cnt)
 										echo "<tr class='oddrow'>";
 									else
 										echo "<tr class='evenrow'>";
-									foreach ( $row as $table_cell ) {
-										echo "<td>";
-										echo "\t" . $table_cell;
+									$i=1;
+									foreach ($row as $table_cell) {
+										if ($i==1)
+											echo "<td class='text-center'>";
+										else
+											echo "<td class='text-right'>";
+
+										echo $table_cell;
 										echo "</td>";
+										$i++;
 									}
 									echo "</tr>";
-									$cnt = 1 - $cnt;
+									$cnt = 1-$cnt;
 								}
 								?>
+
 								<tr id=pie bgcolor=white>
 									<td colspan=2>
-										<div id='PieDiv' style='position:relative;height:150px;width:200px;'></div>
+										<center>
+											<div id='PieDiv' style='position:relative; height:150px; width:200px;'></div>
+										</center>
+									</td>
 								</tr>
-							</table><br>
-							<?php if(isset($view_recommand)){?>
-							<table id=recommand>
+							</table>
+
+							<br>
+
+							<?php if (isset($view_recommand)) {?>
+							<table id=recommand class="table-hover table-striped" align=center width=90%>
 								<tr>
-									<td>
-										Recommanded Next Problem<br>
+									<td class='text-center'>
+										Recommanded Next Problem
+										<br>
 										<?php
 										$cnt = 1;
-										foreach ( $view_recommand as $row ) {
+										foreach ($view_recommand as $row) {
 											echo "<a href=problem.php?id=$row[0]>$row[0]</a>&nbsp;";
-											if ( $cnt % 3 == 0 )echo "<br>";
+											if ($cnt%5 == 0)
+												echo "<br>";
 											$cnt++;
 										}
 										?>
@@ -71,105 +90,131 @@
 								</tr>
 							</table>
 							<?php }?>
-							</td>
-							<td>
-								<table id=problemstatus>
-									<thead>
-										<tr class=toprow>
-											<th style="cursor:hand" onclick="sortTable('problemstatus', 0, 'int');">
-												<?php echo $MSG_Number?>
-												<th>RunID
-													<th>
-														<?php echo $MSG_USER?>
-														<th>
-															<?php echo $MSG_MEMORY?>
-															<th>
-																<?php echo $MSG_TIME?>
-																<th>
-																	<?php echo $MSG_LANG?>
-																	<th>
-																		<?php echo $MSG_CODE_LENGTH?>
-																		<th>
-																			<?php echo $MSG_SUBMIT_TIME?>
-																		</th>
-																	</th>
-																</th>
-															</th>
-														</th>
-													</th>
-												</th>
-											</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php
-										$cnt = 0;
-										foreach ( $view_solution as $row ) {
-											if ( $cnt )
-												echo "<tr class='oddrow'>";
-											else
-												echo "<tr class='evenrow'>";
-											foreach ( $row as $table_cell ) {
-												echo "<td>";
-												echo "\t" . $table_cell;
-												echo "</td>";
-											}
-											echo "</tr>";
-											$cnt = 1 - $cnt;
-										}
-										?>
-								</table>
+
+						</td>
+
+						<td>
+							<table id=problemstatus class="table-hover table-striped" align=center width=100%>
+								<thead>
+									<tr class=toprow>
+										<th style="cursor:hand" onclick="sortTable('problemstatus', 0, 'int');" class="text-center" width=10%>
+											<?php echo $MSG_Number?>
+										</th>
+										<th class="text-center" width=10%>
+											<?php echo $MSG_RUNID?>
+										</th>
+										<th class="text-center" width=15%>
+											<?php echo $MSG_USER?>
+										</th>
+										<th class="text-center" width=10%>
+											<?php echo $MSG_MEMORY?>
+										</th>
+										<th class="text-center" width=10%>
+											<?php echo $MSG_TIME?>
+										</th>
+										<th class="text-center" width=10%>
+											<?php echo $MSG_LANG?>
+										</th>
+										<th class="text-center" width=10%>
+											<?php echo $MSG_CODE_LENGTH?>
+										</th>
+										<th class="text-center" width=20%>
+											<?php echo $MSG_SUBMIT_TIME?>
+										</th>
+									</tr>
+								</thead>
+								<tbody>
 								<?php
-								echo "<a href='problemstatus.php?id=$id'>[TOP]</a>";
-								echo "&nbsp;&nbsp;<a href='status.php?problem_id=$id'>[STATUS]</a>";
-								if ( $page > $pagemin ) {
-									$page--;
-									echo "&nbsp;&nbsp;<a href='problemstatus.php?id=$id&page=$page'>[PREV]</a>";
-									$page++;
-								}
-								if ( $page < $pagemax ) {
-									$page++;
-									echo "&nbsp;&nbsp;<a href='problemstatus.php?id=$id&page=$page'>[NEXT]</a>";
-									$page--;
-								}
-								?>
+									$cnt = 0;
+									foreach ($view_solution as $row) {
+										if ( $cnt )
+											echo "<tr class='oddrow'>";
+										else
+											echo "<tr class='evenrow'>";
+										
+										$i = 1;
+										foreach ($row as $table_cell) {
+											if ($i==1 || $i==8)
+												echo "<td class='text-center'>";
+											else if ($i==2 || $i==4 || $i==5 || $i==6  || $i==7)
+												echo "<td class='text-right'>";
+											else
+												echo "<td>";
+
+											echo $table_cell;
+											echo "&nbsp";
+											echo "</td>";
+											$i++;
+										}
+
+										echo "</tr>";
+										$cnt = 1-$cnt;
+									}
+									?>
+								</tbody>
+							</table>
+
+							<br>
+
+							<center>
+							<?php
+							echo "<a href='problemstatus.php?id=$id'>[TOP]</a>";
+							//echo "&nbsp;&nbsp;<a href='status.php?problem_id=$id'>[STATUS]</a>";
+							
+							if ($page>$pagemin) {
+								$page--;
+								echo "&nbsp;&nbsp;<a href='problemstatus.php?id=$id&page=$page'>[PREV]</a>";
+								$page++;
+							}
+
+							if ($page<$pagemax) {
+								$page++;
+								echo "&nbsp;&nbsp;<a href='problemstatus.php?id=$id&page=$page'>[NEXT]</a>";
+								$page--;
+							}
+							?>
+						  </center>
+
+						</td>
+					</tr>
 				</table>
+			</center>
+
 				<script type="text/javascript" src="include/wz_jsgraphics.js"></script>
 				<script type="text/javascript" src="include/pie.js"></script>
 				<script language="javascript">
 					var y = new Array();
 					var x = new Array();
-					var dt = document.getElementById( "statics" );
+					var dt = document.getElementById("statics");
 					var data = dt.rows;
 					var n;
-					for ( var i = 3; dt.rows[ i ].id != "pie"; i++ ) {
-						x.push( dt.rows[ i ].cells[ 0 ].innerHTML );
-						n = dt.rows[ i ].cells[ 1 ];
+					for (var i=3; dt.rows[i].id!="pie"; i++) {
+						x.push(dt.rows[i].cells[0].innerHTML);
+						n = dt.rows[i].cells[1];
 						n = n.innerText || n.textContent;
 						//alert(n);
-						n = parseInt( n );
-						y.push( n );
+						n = parseInt(n);
+						y.push(n);
 					}
-					var mypie = new Pie( "PieDiv" );
-					mypie.drawPie( y, x );
+					var mypie = new Pie("PieDiv");
+					mypie.drawPie(y, x);
 					//mypie.clearPie();
 				</script>
-
+			
+			</div>
 		</div>
 
-	</div>
-	<!-- /container -->
+		<!-- /container -->
 
-
-	<!-- Bootstrap core JavaScript
+		<!-- Bootstrap core JavaScript
     ================================================== -->
-	<!-- Placed at the end of the document so the pages load faster -->
-	<?php include("template/$OJ_TEMPLATE/js.php");?>
-	<script type="text/javascript" src="include/jquery.tablesorter.js"></script>
-	<script type="text/javascript">
-		$( document ).ready( function () {
-			$( "#problemstatus" ).tablesorter();
-		} );
-	</script>
-</body>
+		<!-- Placed at the end of the document so the pages load faster -->
+		<?php include("template/$OJ_TEMPLATE/js.php");?>
+		<script type="text/javascript" src="include/jquery.tablesorter.js"></script>
+		<script type="text/javascript">
+			$( document ).ready( function () {
+				$( "#problemstatus" ).tablesorter();
+			} );
+		</script>
+	</body>
 </html>

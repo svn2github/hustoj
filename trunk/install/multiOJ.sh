9e02753b4b8d1ae98f12947c63fb213d1c7b6726
张浩斌 <newsclan@gmail.com>
2021-01-31 08:16:33 +0800

    Update multiOJ.sh

--- trunk/install/multiOJ.sh
+++ trunk/install/multiOJ.sh
@@ -1,6 +1,6 @@
 function judge_round(){
 
-  for config in `sudo find -O2  "$1" -name judge.conf`
+  for config in `sudo find -O2  "$1" -maxdepth 4 -name judge.conf`
   do
      etc=`dirname "$config"`
      home=`dirname "$etc"`

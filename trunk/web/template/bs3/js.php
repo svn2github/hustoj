821994f7855879daa08bb48ae41b6b10192606e0
张浩斌 <newsclan@gmail.com>
2021-07-05 09:07:51 +0800

    Update js.php

--- trunk/web/template/bs3/js.php
+++ trunk/web/template/bs3/js.php
@@ -9,23 +9,6 @@ if(file_exists("./admin/msg.txt"))
 $view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"./admin/msg.txt");
 if(file_exists("../admin/msg.txt"))
 $view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"../admin/msg.txt");
-/*
-
-<!--  to enable mathjax in hustoj:
-svn export http://github.com/mathjax/MathJax/trunk/es5 /home/judge/src/web/mathjax
-<script type="text/javascript"
-  src="mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
-or
-<script type="text/javascript"
-  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
-
--->
-
-
-*/
-
 ?>
 
 <script>
@@ -63,7 +46,7 @@ $(".hint pre").each(function(){
 });
 
   console.log("If you want to change the appearance of the web pages, make a copy of bs3 under template directory.\nRename it to whatever you like, and change the $OJ_TEMPLATE value in db_info.inc.php\nAfter that modify files under your own directory .\n");
-  console.log("To enable mathjax in hustoj, check line 15 in /home/judge/src/web/template/bs3/js.php");
+  console.log("To enable mathjax in hustoj, set \n static  $OJ_MATHJAX=true;  // 激活mathjax \n in db_info.inc.php");
 function admin_mod(){
 	$("div[fd=source]").each(function(){
 		let pid=$(this).attr('pid');	

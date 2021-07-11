1e55f987f79f5b98fe835add535a731ed53393c6
张浩斌 <newsclan@gmail.com>
2021-07-11 13:57:00 +0800

    bbcode

--- trunk/web/template/syzoj/viewnews.php
+++ trunk/web/template/syzoj/viewnews.php
@@ -1,5 +1,6 @@
 <?php $show_title="公告 - $OJ_NAME"; ?>
 <?php include("template/$OJ_TEMPLATE/header.php");?>
+<?php include("include/bbcode.php");?>
 <div class="padding">
     <h1><?php echo $news_title ?></h1>
     <p style="margin-bottom: -5px; ">
@@ -8,7 +9,7 @@
         <b style="margin-right: 30px; "><i class="calendar icon"></i> <?php echo $news_date ?></b>
     </p>
     <div class="ui existing segment">
-        <div id="content" class="font-content"><?php echo $news_content?></div>
+        <div id="content" class="font-content"><?php echo bbcode_to_html($news_content)?></div>
     </div>
 </div>
 

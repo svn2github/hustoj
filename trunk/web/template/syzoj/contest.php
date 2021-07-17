7723f1138a8b56202db520e7c65045efcf394c42
张浩斌 <newsclan@gmail.com>
2021-07-17 20:06:41 +0800

    fixing

--- trunk/web/template/syzoj/contest.php
+++ trunk/web/template/syzoj/contest.php
@@ -36,7 +36,8 @@
         <div class="row">
             <div class="column">
                 <div class="ui buttons">
-                    <a class="ui small blue button" href="contestrank.php?cid=<?php echo $view_cid?>">排行榜</a>
+                    <a class="ui small blue button" href="contestrank.php?cid=<?php echo $view_cid?>">ACM排行榜</a>
+                    <a class="ui small yellow button" href="contestrank-oi.php?cid=<?php echo $view_cid?>">OI排行榜</a>
                     <a class="ui small positive button" href="status.php?cid=<?php echo $view_cid?>">提交记录</a>
                     <!-- <a class="ui small pink button" href="conteststatistics.php?cid=<?php echo $view_cid?>">比赛统计</a> -->
                 </div>

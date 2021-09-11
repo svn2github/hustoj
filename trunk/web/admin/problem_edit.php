a5cb53ce43d5b6b1cb1d39e26623cf9b51919ebc
张浩斌 <newsclan@gmail.com>
2021-09-11 10:28:09 +0800

    Update problem_edit.php

--- trunk/web/admin/problem_edit.php
+++ trunk/web/admin/problem_edit.php
@@ -43,9 +43,9 @@ include_once("kindeditor.php") ;
       </p>
         <p align=left>
           <?php echo $MSG_Time_Limit?><br>
-          <input class="input input-mini" type=number min="0.001" max="300" step="0.001" name=time_limit size=20 value=1> sec<br><br>
+          <input class="input input-mini" type=number min="0.001" max="300" step="0.001" name=time_limit size=20 value="<?php echo $row['time_limit']?>"> sec<br><br>
           <?php echo $MSG_Memory_Limit?><br>
-          <input class="input input-mini" type=number min="1" max="1024" step="1" name=memory_limit size=20 value=128> MB<br><br>
+          <input class="input input-mini" type=number min="1" max="1024" step="1" name=memory_limit size=20 value="<?php echo $row['memory_limit']?>"> MB<br><br>
         </p>
       <p align=left>
         <?php echo "<h4>".$MSG_Description."</h4>"?>

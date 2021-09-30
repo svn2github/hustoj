4cde412e9396908d675f32e20ead7dfc0f7d36dd
张浩斌 <newsclan@gmail.com>
2021-09-30 14:22:29 +0800

    Update db_info.inc.php

--- trunk/web/include/db_info.inc.php
+++ trunk/web/include/db_info.inc.php
@@ -69,7 +69,7 @@ static  $OJ_NOIP_KEYWORD="noip";  // 标题包含此关键词，激活noip模式
 static  $OJ_BEIAN=false;  // 如果有备案号，填写备案号
 static  $OJ_RANK_HIDDEN="'admin','zhblue'";  // 管理员不显示在排名中
 static  $OJ_FRIENDLY_LEVEL=0; //系统友好级别，暂定0-9级，级别越高越傻瓜，系统易用度高的同时将降低安全性，仅供非专业用途，造成泄题、抄袭概不负责。
-
+static  $OJ_JUDGE_HUB_PATH="judge";  // 提供给JudgeHub的子路径
 
 //static  $OJ_EXAM_CONTEST_ID=1000; // 启用考试状态，填写考试比赛ID
 //static  $OJ_ON_SITE_CONTEST_ID=1000; //启用现场赛状态，填写现场赛比赛ID

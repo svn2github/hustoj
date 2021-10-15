e2272b86f1784987c6c3e2239725eff91331c8f3
张浩斌 <newsclan@gmail.com>
2021-10-16 07:52:05 +0800

    Update contestset.php

--- trunk/web/template/syzoj/contestset.php
+++ trunk/web/template/syzoj/contestset.php
@@ -1,4 +1,4 @@
-<?php $show_title="$MSG_CONTESTS - $OJ_NAME"; ?>
+<?php $show_title="$MSG_CONTEST - $OJ_NAME"; ?>
 <?php include("template/$OJ_TEMPLATE/header.php");?>
 <div class="padding">
 <div class="ui grid" style="margin-bottom: 10px; ">

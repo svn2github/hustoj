eb4784bfa6677b5c491207fe934f4751e7203e70
张浩斌 <newsclan@gmail.com>
2021-10-17 09:56:18 +0800

    Update header.php

--- trunk/web/template/syzoj/header.php
+++ trunk/web/template/syzoj/header.php
@@ -68,10 +68,11 @@
                         <div class="menu">
                             <a class="item" href="<?php echo $path_fix?>modifypage.php"><i
                                     class="edit icon"></i><?php echo $MSG_REG_INFO;?></a>
-				<?php if ($OJ_SaaS_ENABLE){ ?>
-				<?php if($_SERVER['HTTP_HOST']==$DOMAIN)
-					echo  "<a class='item' href='http://".  $_SESSION[$OJ_NAME.'_'.'user_id'].".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";?>
-				<?php } ?>
+				
+				<?php if($OJ_SaaS_ENABLE && $domain==$DOMAIN)
+					echo  "<a class='item' href='http://".  $_SESSION[$OJ_NAME.'_'.'user_id'].".$DOMAIN'><i class='globe icon' ></i>MyOJ</a>";
+				 ?>
+				
                             <?php if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])){ ?>
                             <a class="item" href="<?php echo $path_fix ?>/admin"><i class="settings icon"></i><?php echo $MSG_ADMIN;?></a>
                             <?php } ?>

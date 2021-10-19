1e5d38b98c544f732f128e1099b9ac0c1d872222
张浩斌 <newsclan@gmail.com>
2021-10-19 11:25:04 +0800

    Update docker.sh

--- trunk/install/docker.sh
+++ trunk/install/docker.sh
@@ -1,3 +1,10 @@
-apt-get install -y docker.io
-docker build -t hustoj .
+#!/bin/bash
+while ! apt-get install -y docker.io
+do
+		echo "Network fail, retry... you might want to make sure docker.io is available in your apt source"
+done
 
+while ! docker build -t hustoj .
+do
+		echo "Network fail, retry... you might want to make sure https://hub.docker.com/ is available"
+done

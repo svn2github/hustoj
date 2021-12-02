50ffc1d13ceffc1642440242ddb9f597611363cd
张浩斌 <newsclan@gmail.com>
2021-12-02 09:14:21 +0800

    Update css.php

--- trunk/web/template/bs3/css.php
+++ trunk/web/template/bs3/css.php
@@ -6,7 +6,7 @@
 ?>
 
 <!-- 新 Bootstrap 核心 CSS 文件 -->
-<link rel="stylesheet" href="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE/"?>bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo $path_fix."template/$OJ_TEMPLATE/"?>bootstrap.min.css">
 
 <?php if(!isset($OJ_FLAT)||!$OJ_FLAT){?>
 <!-- 可选的Bootstrap主题文件（一般不用引入） -->

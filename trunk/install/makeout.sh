2a6078d9b731667cf4a3d1f2e306d12566c324e5
张浩斌 <newsclan@gmail.com>
2021-12-24 09:42:25 +0800

    Update makeout.sh

--- trunk/install/makeout.sh
+++ trunk/install/makeout.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-g++  -o main main.cc
-for i in `find -name "*.in"`
-do 
-	if	./main< $i > `basename -s ".in" $i`.out 
-	then
-		echo "$i ok.";
-	else
-		echo "$i error!"
-	fi
+if [ "$1" = "" ]; then
+   echo "Usage: $0 standard-execute-binary"
+   echo "Example: after compiled your standard program by 'gcc -o main main.cc' "
+   echo "         type '$0 main'  "
+   echo "         will generate .out files for each .in files in "`pwd`
+   exit 1
+fi
+EXEC=./$1
+for INFILE in `ls *.in`
+do
+        OUTFILE=`basename -s .in $INFILE`.out
+        if $EXEC < $INFILE > $OUTFILE ; then
+                echo "make out for $INFILE -> $OUTFILE"
+        else
+                echo "make out for $INFILE .....failed"
+        fi
 done
 

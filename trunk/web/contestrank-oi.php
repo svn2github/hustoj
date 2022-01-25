62e96571fb428e8d058366cff3da34ecff47d7e8
张浩斌 <newsclan@gmail.com>
2022-01-25 09:14:56 +0800

    Update contestrank-oi.php

--- trunk/web/contestrank-oi.php
+++ trunk/web/contestrank-oi.php
@@ -5,6 +5,7 @@
     require_once('./include/db_info.inc.php');
         require_once('./include/setlang.php');
         $view_title= $MSG_CONTEST.$MSG_RANKLIST;
+	$show_title= $view_title;
         $title="";
         require_once("./include/const.inc.php");
         require_once("./include/my_func.inc.php");

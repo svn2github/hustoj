4a8d0af435152b92f745968885b7d0bd1e60dfa6
张浩斌 <newsclan@gmail.com>
2022-02-06 16:18:12 +0800

    Update ug.php

--- trunk/web/lang/ug.php
+++ trunk/web/lang/ug.php
@@ -384,4 +384,16 @@ if(date('m')==4&&date('d')==1&&rand(0,100)<10){
 	$MSG_FAIL="Fail";
 	$MSG_TEXT_COMPARE="Text Compare";
         $MSG_JUDGE_STYLE="Judge Style";
+	// reinfo.php 
+	$MSG_ERROR_INFO="Runtion information";
+	$MSG_INFO_EXPLAINATION="Explaination";
+	// ceinfo.php
+	$MSG_COMPILE_INFO="Compile information";
+	$MSG_SOURCE_CODE="Source code";
+	//contest.php
+	$MSG_Contest_Pending="Pending";
+	$MSG_Server_Time="Server Current Time";
+	$MSG_Contest_Infomation="Contest Information";
+	// sourcecompare.php
+	$MSG_Source_Compare="Source Code Comparation";
  ?>

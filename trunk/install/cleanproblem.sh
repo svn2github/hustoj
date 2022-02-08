55fd7e2ffeabfa8582ffc8d41e8319a55b04a92b
undefined <i@undefined.moe>
2022-02-08 18:00:01 +0800

    Update cleanproblem.sh

--- trunk/install/cleanproblem.sh
+++ trunk/install/cleanproblem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/judge/src/install
-rm -rf ../..//data/*
+rm -rf ../../data/*
 echo "delete from problem ;" |./mysql.sh
 echo "ALTER TABLE problem AUTO_INCREMENT = 1000 ;"|./mysql.sh

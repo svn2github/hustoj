a4b7d82b5aa83e1dae54f62e660a0d6683f850c0
张浩斌 <newsclan@gmail.com>
2022-02-22 10:45:59 +0800

    Update saasinit.php

--- trunk/web/saasinit.php
+++ trunk/web/saasinit.php
@@ -3,7 +3,7 @@
 require_once("./include/db_info.inc.php");
 require_once("./include/my_func.inc.php");
 
-if($OJ_SaaS_ENABLE){
+if(isset($OJ_SaaS_ENABLE) && $OJ_SaaS_ENABLE){
         if($_SERVER['HTTP_HOST']==$DOMAIN && isset($_SESSION[$OJ_NAME.'_user_id'])){
                 $template="bs3";
                 $friendly=0;

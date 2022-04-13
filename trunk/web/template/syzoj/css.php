5c32f1ba4be218f747d19c11f93319635623f759
张浩斌 <newsclan@gmail.com>
2022-04-13 15:58:28 +0800

    Update css.php

--- trunk/web/template/syzoj/css.php
+++ trunk/web/template/syzoj/css.php
@@ -7,17 +7,7 @@
 <link rel="stylesheet" href="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/tomorrow.css">
 <link rel="stylesheet" href="<?php echo $path_fix."template/$OJ_TEMPLATE"?>/css/semantic.min.css">
 <link rel="stylesheet" href="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/katex.min.css">
-<script defer src="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/katex.js"></script>
-<script>
-var katex_config = {
-	delimiters: 
-	[
-		{left: "$$", right: "$$", display: true},
-  		{left: "$", right: "$", display: false}
-	]
-};
-</script>
-<script defer src="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/auto-render.min.js" onload="renderMathInElement(document.body, katex_config)"></script>
+
 <link href="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/morris.min.css" rel="stylesheet">
 <link href="<?php echo $OJ_CDN_URL.$path_fix."template/$OJ_TEMPLATE"?>/css/FiraMono.css" rel="stylesheet">
 <link href="<?php echo $path_fix."template/$OJ_TEMPLATE"?>/css/latin.css" rel="stylesheet">

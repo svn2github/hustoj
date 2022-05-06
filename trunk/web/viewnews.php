d13623009dcd90622afd770d4c01bd62463e1fec
张浩斌 <newsclan@gmail.com>
2022-05-07 07:56:36 +0800

    Update viewnews.php

--- trunk/web/viewnews.php
+++ trunk/web/viewnews.php
@@ -40,32 +40,6 @@ if ( !$result ) {
 		$news_date=$row['time'];
 	}
 }
-$view_apc_info = "";
-
-$sql = "SELECT UNIX_TIMESTAMP(date(in_date))*1000 md,count(1) c FROM (select * from solution order by solution_id desc limit 8000) solution  where result<13 group by md order by md desc limit 200";
-$result = mysql_query_cache( $sql ); //mysql_escape_string($sql));
-$chart_data_all = array();
-//echo $sql;
-
-foreach ( $result as $row ) {
-	array_push( $chart_data_all, array( $row[ 'md' ], $row[ 'c' ] ) );
-}
-
-$sql = "SELECT UNIX_TIMESTAMP(date(in_date))*1000 md,count(1) c FROM  (select * from solution order by solution_id desc limit 8000) solution where result=4 group by md order by md desc limit 200";
-$result = mysql_query_cache( $sql ); //mysql_escape_string($sql));
-$chart_data_ac = array();
-//echo $sql;
-
-foreach ( $result as $row ) {
-	array_push( $chart_data_ac, array( $row[ 'md' ], $row[ 'c' ] ) );
-}
-if ( isset( $_SESSION[ $OJ_NAME . '_' . 'administrator' ] ) ) {
-	$sql = "select avg(sp) sp from (select  avg(1) sp,judgetime from solution where result>3 and judgetime>date_sub(now(),interval 1 hour)  group by (judgetime DIV 60 * 60) order by sp) tt;";
-	$result = mysql_query_cache( $sql );
-	$speed = ( $result[ 0 ][ 0 ] ? $result[ 0 ][ 0 ] : 0 ) . '/min';
-} else {
-	$speed = ( $chart_data_all[ 0 ][ 1 ] ? $chart_data_all[ 0 ][ 1 ] : 0 ) . '/day';
-}
 
 /////////////////////////Template
 require( "template/" . $OJ_TEMPLATE . "/viewnews.php" );

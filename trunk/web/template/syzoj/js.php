2208b147bbda4eb2f21804d7ea76a7fc895a8a81
张浩斌 <newsclan@gmail.com>
2022-05-12 13:58:15 +0800

    Update js.php

--- trunk/web/template/syzoj/js.php
+++ trunk/web/template/syzoj/js.php
@@ -1,9 +1,10 @@
 
 <?php
-if(file_exists("./admin/msg.txt"))
-$view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"./admin/msg/$domain.txt");
-if(file_exists("../admin/msg.txt"))
-$view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"../admin/msg/$domain.txt");
+$msg_path=realpath(dirname(__FILE__)."/../../admin/msg/$domain.txt");
+if(file_exists($msg_path))
+	$view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":$msg_path);
+else
+	$view_marquee_msg="";
 
 
 ?>

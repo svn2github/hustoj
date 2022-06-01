7d9230ae9a1046ec776b77bdfb5393df8e166e27
张浩斌 <newsclan@gmail.com>
2022-06-01 12:13:00 +0800

    Update faqs.php

--- trunk/web/template/syzoj/faqs.php
+++ trunk/web/template/syzoj/faqs.php
@@ -57,6 +57,11 @@ int main(){
 <code class="lang-c++">#include &lt;iostream&gt;
 using namespace std;
 int main(){
+    // io speed up
+    const char endl = '\n';
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int a, b;
     while (cin &gt;&gt; a &gt;&gt; b){
         cout &lt;&lt; a+b &lt;&lt; endl;

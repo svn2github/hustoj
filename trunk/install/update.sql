ee6508744f273a23d75dff78ce0be0d0f5f7b389
张浩斌 <newsclan@gmail.com>
2022-06-20 09:24:51 +0800

    Update update.sql

--- trunk/install/update.sql
+++ trunk/install/update.sql
@@ -40,6 +40,7 @@ update solution s inner join users u on s.user_id=u.user_id set s.nick=u.nick;
 alter table privilege add index user_id_index(user_id);
 ALTER TABLE `problem` CHANGE `time_limit` `time_limit` DECIMAL(10,3) NOT NULL DEFAULT '0';
 alter table privilege add column valuestr char(11) not null default 'true' after rightstr; 
+alter table news modify column `time` datetime NOT NULL DEFAULT '2016-05-13 19:24:00';
 ALTER TABLE `news` ADD COLUMN `menu` int(11) NOT NULL DEFAULT 0 AFTER `importance`;
 #create fulltext index problem_title_source_index on problem(title,source);
 

5d1e85667ab829838be790fdefea224a770a4dec
张浩斌 <newsclan@gmail.com>
2022-06-21 12:17:12 +0800

    Update loginpage.php

--- trunk/web/loginpage.php
+++ trunk/web/loginpage.php
@@ -1,7 +1,11 @@
 <?php
 $cache_time=1;
-require_once('./include/cache_start.php');
-require_once('./include/memcache.php');
+require_once( './include/cache_start.php' );
+require_once( './include/db_info.inc.php' );
+require_once( './include/memcache.php' );
+require_once( './include/setlang.php' );
+require_once( './include/bbcode.php' );
+
     require_once("./include/db_info.inc.php");
 	require_once("./include/setlang.php");
 	$view_title= "LOGIN";

60453eb1ac7546d4fd216a49d3a950acb54aff3b
张浩斌 <newsclan@gmail.com>
2022-06-24 09:31:42 +0800

    Update registerpage.php

--- trunk/web/registerpage.php
+++ trunk/web/registerpage.php
@@ -2,8 +2,12 @@
 ////////////////////////////Common head
 	$cache_time=10;
 	$OJ_CACHE_SHARE=false;
-	require_once('./include/cache_start.php');
-    require_once('./include/db_info.inc.php');
+require_once( './include/cache_start.php' );
+require_once( './include/db_info.inc.php' );
+require_once( './include/memcache.php' );
+require_once( './include/setlang.php' );
+require_once( './include/bbcode.php' );
+
 if(isset($OJ_REGISTER)&&!$OJ_REGISTER) exit(0);
 	require_once('./include/setlang.php');
 	$view_title= "Registe a new account";

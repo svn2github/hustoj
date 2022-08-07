1bf04222c8c1780fa68d00abf4d5b4e6e0a7bb87
张浩斌 <newsclan@gmail.com>
2022-08-07 18:12:25 +0800

    Update install-ubuntu22.04.sh

--- trunk/install/install-ubuntu22.04.sh
+++ trunk/install/install-ubuntu22.04.sh
@@ -134,6 +134,7 @@ if grep "bak.sh" /var/spool/cron/crontabs/root ; then
 	echo "auto backup added!"
 else
 	crontab -l > conf && echo "1 0 * * * /home/judge/src/install/bak.sh" >> conf && crontab conf && rm -f conf
+	/etc/init.d/cron reload
 fi
 ln -s /usr/bin/mcs /usr/bin/gmcs
 

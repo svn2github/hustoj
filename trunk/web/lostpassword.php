0ec8a86ea69a47871348f76aa54561faaf553a1d
张浩斌 <newsclan@gmail.com>
2022-08-31 13:26:01 +0800

    Update lostpassword.php

--- trunk/web/lostpassword.php
+++ trunk/web/lostpassword.php
@@ -36,7 +36,7 @@
 	$smtpuser = "mailer@hustoj.com";//SMTP服务器的用户帐号
 	$smtppass = "hustojmailer";//SMTP服务器的用户密码
 	$mailtitle = "OJ系统密码重置激活";//邮件主题
-	$mailcontent = "$lost_user_id:\n您好！\n您在$OJ_NAME系统选择了找回密码服务,为了验证您的身份,请将下面字串输入口令重置页面以确认身份:".$_SESSION[$OJ_NAME.'_'.'lost_key']."\n\n\nHUSTOJ在线评测系统";//邮件内容
+	$mailcontent = "$lost_user_id:\n您好！\n您在$OJ_NAME系统选择了找回密码服务,为了验证您的身份,请将下面字串输入口令重置页面以确认身份:".$_SESSION[$OJ_NAME.'_'.'lost_key']."\n 注意：验证通过后，上面这个字符串将成为您的新密码!\n\n\nHUSTOJ在线评测系统";//邮件内容
 	$mailtype = "TXT";//邮件格式（HTML/TXT）,TXT为文本邮件
 	//************************ 配置信息 ****************************
 	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.

1e4e399f3fd78cc4e6790ae8536938507636c8f9
张浩斌 <newsclan@gmail.com>
2022-09-22 08:27:40 +0800

    Update problem_import_xml.php

--- trunk/web/admin/problem_import_xml.php
+++ trunk/web/admin/problem_import_xml.php
@@ -2,7 +2,7 @@
 require_once ("admin-header.php");
 require_once("../include/check_post_key.php");
 
-if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_problem_editor']))) {
+if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_problem_importer']))) {
   echo "<a href='../loginpage.php'>Please Login First!</a>";
   exit(1);
 }

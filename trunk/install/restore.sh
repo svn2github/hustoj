f7a751124a88dfae8242285cc91d1eebeb3db22d
张浩斌 <newsclan@gmail.com>
2022-09-22 15:09:45 +0800

    Update restore.sh

--- trunk/install/restore.sh
+++ trunk/install/restore.sh
@@ -23,3 +23,5 @@ mv /home/judge/src/web/upload /home/judge/src/web/upload.del.$DATE
 mv home/judge/src/web/upload /home/judge/src/web/
 bzip2 -d var/backups/db_${BAKDATE}.sql.bz2
 mysql -h $SERVER -u$USER -p$PASSWORD $DATABASE < var/backups/db_${BAKDATE}.sql
+mysql -h $SERVER -u$USER -p$PASSWORD $DATABASE < /home/judge/src/install/update.sql
+

6e641b31ff0dc3243cbed262a3e1bd4d2f104aa8
张浩斌 <newsclan@gmail.com>
2022-10-11 10:05:41 +0800

    Update conteststatus.php

--- trunk/web/template/sweet/conteststatus.php
+++ trunk/web/template/sweet/conteststatus.php
@@ -84,16 +84,7 @@ echo "SIM:
 <option value=90 ".($showsim==90?'selected':'').">90</option>
 <option value=100 ".($showsim==100?'selected':'').">100</option>
 </select>";
-/* if (isset($_GET['cid']))
-echo "<input type=hidden name=cid value='".$_GET['cid']."'>";
-if (isset($_GET['language']))
-echo "<input type=hidden name=language value='".$_GET['language']."'>";
-if (isset($_GET['user_id']))
-echo "<input type=hidden name=user_id value='".$_GET['user_id']."'>";
-if (isset($_GET['problem_id']))
-echo "<input type=hidden name=problem_id value='".$_GET['problem_id']."'>";
-//echo "<input type=submit>";
-*/
+
 }
 echo "<input type=submit class='form-control' value='$MSG_SEARCH'></form>";
 ?>

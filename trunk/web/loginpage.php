9246f0e8e86a95fa41985f846e0bd8d161db3f4a
张浩斌 <newsclan@gmail.com>
2022-10-17 17:52:27 +0800

    Update loginpage.php

--- trunk/web/loginpage.php
+++ trunk/web/loginpage.php
@@ -5,23 +5,31 @@ require_once( './include/db_info.inc.php' );
 require_once( './include/memcache.php' );
 require_once( './include/setlang.php' );
 require_once( './include/bbcode.php' );
-require_once("./include/db_info.inc.php");
-require_once("./include/setlang.php");
-$view_title= "LOGIN";
 
-if (isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
-		header("location:index.php");
-		echo "<a href=logout.php>Please logout First!</a>";
-		exit(1);
+    require_once("./include/db_info.inc.php");
+        require_once("./include/setlang.php");
+        $view_title= "LOGIN";
+
+        if (isset($_SESSION[$OJ_NAME.'_'.'user_id'])){
+                header("location:index.php");
+        echo "<a href=logout.php>Please logout First!</a>";
+        exit(1);
 }
 
 /////////////////////////Template
 if($OJ_LONG_LOGIN==true&&isset($_COOKIE[$OJ_NAME."_user"])&&isset($_COOKIE[$OJ_NAME."_check"])){
-	echo"<script>let xhr=new XMLHttpRequest();xhr.open('GET','login.php',true);xhr.send();setTimeout('history.go(-1)',500);</script>";
-}
-else
+?>
+        <script>
+        let xhr=new XMLHttpRequest();
+        xhr.open('GET','login.php',true);
+        xhr.send();
+        setTimeout("location.href='index.php'",1500);
+        </script>
+<?php
+}else{
         require("template/".$OJ_TEMPLATE."/loginpage.php");
+}
 /////////////////////////Common foot
 if(file_exists('./include/cache_end.php'))
-	require_once('./include/cache_end.php');
+        require_once('./include/cache_end.php');
 ?>

88037c79834488025bee50c34b282fa3fb9aee01
张浩斌 <newsclan@gmail.com>
2022-10-19 15:09:03 +0800

    Update problemset.php

--- trunk/web/problemset.php
+++ trunk/web/problemset.php
@@ -80,7 +80,7 @@ if (isset($_GET['search']) && trim($_GET['search'])!="") {
 	  $pid=intval($pid);
      	  $pids.=",$pid";
 	}
-	$filter_sql = " problem_id in ($pids)  order by accepted desc"; // 如果希望按给定顺序改成 order by FIELD(problem_id,$pids);
+	$filter_sql = " problem_id in ($pids) order by FIELD(problem_id,$pids)"; // 如果希望按难度顺序改成 order by accepted desc ;
 	$pstart = 0;
 	$pend = 100;
 	//echo $filter_sql ;

12905b0edd25ba1886c13d9141ceab380f8982e1
张浩斌 <newsclan@gmail.com>
2022-10-21 10:00:29 +0800

    Update lostpassword.php

--- trunk/web/lostpassword.php
+++ trunk/web/lostpassword.php
@@ -27,7 +27,8 @@
    $_SESSION[$OJ_NAME.'_'.'lost_key']=getToken(16);
 
   
-	require_once "include/email.class.php";
+	require_once "include/email.class.php";     // 新版本的邮件发送信息请填写到这个文件中
+/* 弃用留存，不做修改
 	//******************** 配置信息 ********************************
 	$smtpserver = "smtp.qiye.aliyun.com";//SMTP服务器
 	$smtpserverport =25;//SMTP服务器端口
@@ -35,13 +36,21 @@
 	$smtpemailto = $row['email'];//发送给谁
 	$smtpuser = "mailer@hustoj.com";//SMTP服务器的用户帐号
 	$smtppass = "hustojmailer";//SMTP服务器的用户密码
-	$mailtitle = "OJ系统密码重置激活";//邮件主题
-	$mailcontent = "$lost_user_id:\n您好！\n您在$OJ_NAME系统选择了找回密码服务,为了验证您的身份,请将下面字串输入口令重置页面以确认身份:".$_SESSION[$OJ_NAME.'_'.'lost_key']."\n 注意：验证通过后，上面这个字符串将成为您的新密码!\n\n\nHUSTOJ在线评测系统";//邮件内容
-	$mailtype = "TXT";//邮件格式（HTML/TXT）,TXT为文本邮件
-	//************************ 配置信息 ****************************
-	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-	$smtp->debug =false;//是否显示发送的调试信息
-	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
+邮箱信息备忘*/
+	 
+        $mailtitle = "OJ系统密码重置激活";//邮件主题
+        $mailcontent = "$lost_user_id:\n您好！\n您在${OJ_NAME}系统选择了找回密码服务,为了验证您的身份,请将下面字串输入口令重置页面以确认身份:".$_SESSION[$OJ_NAME.'_'.'lost_key']."\n 注意：验证通过后，上面这个字符串将成为您的新密码!\n\n\nHUSTOJ在线评测系统";//邮件内容
+
+/* 弃用存留，不做修改
+        $mailtype = "TXT";//邮件格式（HTML/TXT）,TXT为文本邮件
+        //************************ 配置信息 ****************************
+        $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
+        $smtp->debug =false;//是否显示发送的调试信息
+        $state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
+邮箱信息备忘*/
+
+        email($row['email'],$mailtitle,$mailcontent);    
+
         require("template/".$OJ_TEMPLATE."/lostpassword2.php");
 
  }else{

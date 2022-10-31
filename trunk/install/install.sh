c8af2c9ebd50516fd8ce1ddf959be440f04ce0a7
张浩斌 <newsclan@gmail.com>
2022-10-31 15:41:43 +0800

    Create install.sh

---
+++ trunk/install/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IN_SCREEN=no
+if echo "$TERM"|grep "screen" ; then
+        IN_SCREEN=yes;
+fi
+
+if [ "$IN_SCREEN" == "no" ] ;then
+        echo "not in screen";
+        apt update
+        apt install screen
+        screen $0 $*
+else
+        echo "in screen";
+        OSID=`lsb_release -is|tr 'UDC' 'udc'`
+        OSRS=`lsb_release -rs`
+        INSTALL="install-$OSID$OSRS.sh"
+        URL="http://dl.hustoj.com/$INSTALL"
+        wget -O "$INSTALL" "$URL"
+        chmod +x "$INSTALL"
+        "./$INSTALL"
+        sleep 10;
+fi
+

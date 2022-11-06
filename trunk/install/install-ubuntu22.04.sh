1115687ed30a663e889d487c0d77326ca0ff5abf
张浩斌 <newsclan@gmail.com>
2022-11-06 15:19:48 +0800

    Update install-ubuntu22.04.sh

--- trunk/install/install-ubuntu22.04.sh
+++ trunk/install/install-ubuntu22.04.sh
@@ -35,7 +35,7 @@ apt-get install -y libmysqlclient-dev
 apt-get install -y libmysql++-dev 
 PHP_VER=`apt-cache search php-fpm|grep -e '[[:digit:]]\.[[:digit:]]' -o`
 if [ "$PHP_VER" = "" ] ; then PHP_VER="8.1"; fi
-for pkg in net-tools make g++ php$PHP_VER-fpm nginx mysql-server php$PHP_VER-mysql php$PHP_VER-common php$PHP_VER-gd php$PHP_VER-zip php$PHP_VER-mbstring php$PHP_VER-xml php$PHP_VER-curl php$PHP_VER-intl php$PHP_VER-xmlrpc php$PHP_VER-soap tzdata
+for pkg in net-tools make g++ php$PHP_VER-fpm nginx mysql-server php$PHP_VER-mysql php$PHP_VER-common php$PHP_VER-gd php$PHP_VER-zip php$PHP_VER-mbstring php$PHP_VER-xml php$PHP_VER-curl php$PHP_VER-intl php$PHP_VER-xmlrpc php$PHP_VER-soap php-yaml tzdata
 do
 	while ! apt-get install -y "$pkg" 
 	do

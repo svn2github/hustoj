53b2e2ebb95517a40d33951edeb9fcb7723e1f60
张浩斌 <newsclan@gmail.com>
2022-11-19 12:44:40 +0800

    Update bak.sh

--- trunk/install/bak.sh
+++ trunk/install/bak.sh
@@ -7,8 +7,10 @@ SERVER=`cat $config|grep 'OJ_HOST_NAME' |awk -F= '{print $2}'`
 USER=`cat $config|grep 'OJ_USER_NAME' |awk -F= '{print $2}'`
 PASSWORD=`cat $config|grep 'OJ_PASSWORD' |awk -F= '{print $2}'`
 DATABASE=`cat $config|grep 'OJ_DB_NAME' |awk -F= '{print $2}'`
+PORT=`cat $config|grep 'OJ_PORT_NUMBER' |awk -F= '{print $2}'`
 
-mysqldump -h $SERVER  -R $DATABASE -u$USER -p$PASSWORD | bzip2 >/var/backups/db_${DATE}.sql.bz2
+echo "optimize table `compileinfo`,`contest` ,`contest_problem` ,`loginlog`,`news`,`privilege`,`problem` ,`solution`,`source_code`,`users`,`topic`,`reply`,`online`,`sim`,`mail`;"|mysql -h $SERVER -P $PORT -R $DATABASE -u$USER -p$PASSWORD
+mysqldump -h $SERVER -P $PORT -R $DATABASE -u$USER -p$PASSWORD | bzip2 >/var/backups/db_${DATE}.sql.bz2
 if tar cjf /var/backups/hustoj_${DATE}.tar.bz2 /home/judge/data /home/judge/src /home/judge/etc /var/backups/db_${DATE}.sql.bz2 --exclude=/home/judge/src/install/*.bz2 ; then
 	rm /var/backups/hustoj_${OLD3}.tar.bz2
 	rm /var/backups/db_${OLD}.sql.bz2

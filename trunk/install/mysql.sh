f61e9dbae9f2cddc3f9534bee1f146c97c0db217
张浩斌 <newsclan@gmail.com>
2022-11-19 12:47:24 +0800

    Update mysql.sh

--- trunk/install/mysql.sh
+++ trunk/install/mysql.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 config="/home/judge/etc/judge.conf"
 VIRTUAL="/var/www/virtual/"
+SERVER=`cat $config|grep 'OJ_HOST_NAME' |awk -F= '{print $2}'`
 USER=`cat $config|grep 'OJ_USER_NAME' |awk -F= '{print $2}'`
 PASSWORD=`cat $config|grep 'OJ_PASSWORD' |awk -F= '{print $2}'`
-DBNAME=`cat $config|grep 'OJ_DB_NAME' |awk -F= '{print $2}'`
-echo "mysql -u${USER} -p${PASSWORD} ${DBNAME} "
-mysql -u${USER} -p${PASSWORD} ${DBNAME}
+DATABASE=`cat $config|grep 'OJ_DB_NAME' |awk -F= '{print $2}'`
+PORT=`cat $config|grep 'OJ_PORT_NUMBER' |awk -F= '{print $2}'`
+
+echo "mysql -u${USER} -p{PASSWORD_HIDDEN} ${DBNAME} "
+
+mysql -h $SERVER -P $PORT -u$USER -p$PASSWORD $DATABASE 

34cf2224b5720d0a73104d552b501ab72c6506c1
张浩斌 <newsclan@gmail.com>
2022-11-19 15:43:51 +0800

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,7 +9,7 @@ mysql < /trunk/install/db.sql
 mysql -e "insert into jol.privilege ( user_id, rightstr ) values('admin','administrator');"
 mysql -e "insert into jol.problem(problem_id,title,time_limit,memory_limit) values(1,1,1,5);"
 mysql -e "insert into jol.source_code values(1,'#include<stdio.h>\nint main(){\nint a,b;\nscanf(\"%d%d\",&a,&b);\nprintf(\"%d\\\\n\",a+b);\n}\n');"
-mysql -e "insert into jol.solution (solution_id,user_id,problem_id,ip) values(1,1,1,1);"
+mysql -e "insert into jol.solution (solution_id,user_id,problem_id,ip,in_date) values(1,1,1,1,now());"
 
 # Hustoj basic file system
 useradd -m -u 1536 judge

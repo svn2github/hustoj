f584172e95f8fe433cfb3b42fae539a2661624cd
张浩斌 <newsclan@gmail.com>
2022-11-30 16:08:42 +0800

    Add files via upload

--- trunk/web/admin/menu2.php
+++ trunk/web/admin/menu2.php
@@ -122,6 +122,7 @@
 <?php }?>
 
 <?php if (isset($_SESSION[$OJ_NAME.'_'.'administrator'])&&!$OJ_SAE){?>
+  <a href="solution_statistics.php" target="main" title="Create your own data"><font color="eeeeee">SS Report</font></a> <br>
   <a href="problem_copy.php" target="main" title="Create your own data"><font color="eeeeee">CopyProblem</font></a> <br>
   <a href="problem_changeid.php" target="main" title="Danger,Use it on your own risk"><font color="eeeeee">ReOrderProblem</font></a>
 <?php }?>
--- trunk/web/admin/solution_statistics.php
+++ trunk/web/admin/solution_statistics.php
@@ -19,7 +19,8 @@ if(isset($OJ_LANG)){
 <div class='container'>
 
 <?php
-if(isset($_POST['pids'])){
+$cid=intval($_GET['cid']);
+if(isset($_POST['pids'])||$cid>0){
 
   $user_ida = explode("\n", trim($_POST['ulist']));
   $user_ida = array_unique($user_ida);
@@ -34,35 +35,53 @@ if(isset($_POST['pids'])){
   }
   //echo implode(",",$user_ida),"<br>";
   
+	  $sql="select user_id,nick ";
 
+  if($cid>0){
+	  $pida=array();
+	  $result=pdo_query("select problem_id from contest_problem where contest_id=? order by num",$cid);
+	  foreach($result as $row){
+	  	array_push($pida,$row['problem_id']);
+	  }
 
-  $sql="select user_id,nick ";
-  $pida=array_unique(explode(',',$_POST['pids']));
-  $len=count($pida);
-  for($i=0;$i<$len;$i++){
-  	$pida[$i]=intval($pida[$i]);
+  }else{
+	  $pida=array_unique(explode(',',$_POST['pids']));
+	  $len=count($pida);
+	  for($i=0;$i<$len;$i++){
+		$pida[$i]=intval($pida[$i]);
+	  }
   }
+
   $pida=array_unique($pida);
+
   foreach($pida as $pid){
   	$sql.=" ,min(case problem_id when $pid then result else 15 end) P$pid";
   }
   //$user_ids=implode("','",$user_ida);
-  $sql.=" from solution where user_id in ($user_ids) group by user_id,nick ";
-  //echo $sql;
-  $result = pdo_query($sql,$user_ida);
+  if(isset($_GET['cid'])){
+	  $sql.=" from solution where contest_id=? group by user_id,nick order by user_id";
+  	  $result = pdo_query($sql,$cid);
+
+  }else{
+	  $sql.=" from solution where user_id in ($user_ids) group by user_id,nick ";
+  	  $result = pdo_query($sql,$user_ida);
+  }
+//  echo $sql;
 ?>
 
 
 <center>
   <table width=100% border=1 style="text-align:center;">
+    <tr>
     <?php
       echo "<td>USER_ID</td>";
       echo "<td>NICK</td>";
   	foreach($pida as $pid){
-      		echo "<td>P".$pid;
+      		echo "<td class='pid' value='$pid'>P".$pid;
       		echo "</td>";
 	}
-  //var_dump($result);
+      echo "</tr>";
+ // var_dump($result);
     foreach($result as $row){
       echo "<tr>";
       echo "<td>".$row['user_id']."</td>";
@@ -80,7 +99,23 @@ if(isset($_POST['pids'])){
 </table>
 <a href="javascript:history.go(-1);" >Back</a>
 </center>
+<script>
+	function showTitles(){
+		$('.pid').each(function(){
+			let v=$(this).attr('value');
+			let title=$.ajax({url:"ajax.php",method:"post",data:{"pid":v,"m":"problem_get_title"},async:false}).responseText;
+			let html=(v)+"<br><a href='../problem.php?id="+v+"' target='_blank'>"+title+"</a>\n";
+			$(this).html(html);
+			$(this).mouseover=null;
+		});
+		
+	}
+	$(document).ready(function(){
+		showTitles();
+	
+	});
 
+</script>
 <?php
 }else{
 
@@ -89,12 +124,27 @@ if(isset($_POST['pids'])){
 	<center>
 	<form action=solution_statistics.php method='post' class="form-search form-inline">
 	  <input type="text" name=pids class="form-control" placeholder="1000,1001" size=128 >
-              <textarea name='ulist' rows='10' style='width:100%;' placeholder='user1<?php echo "\n"?>user2<?php echo "\n"?>user3<?php echo "\n"?>'></textarea>
+
+              <textarea name='ulist' rows='10' style='width:100%;' placeholder='user1<?php echo "\n"?>user2<?php echo "\n"?>user3<?php echo "\n"?>
+              '> </textarea>
 	  <button type="submit" class="form-control"><?php echo $MSG_SEARCH?></button>
 	</form>
 	</center>
 	</div>
 <?php
-
 }
 ?>
+	<form action=solution_statistics.php method='get' class="form-search form-inline">
+	<select name="cid" >
+<?php
+	$sql="select * from contest order by contest_id desc limit 50 ";
+	$result=pdo_query($sql);
+	foreach($result as $row){
+		echo "<option value='".$row['contest_id']."'";
+		if($row['contest_id']==$cid) echo " selected ";
+		echo ">".$row['title']."</option>";
+	}
+?>
+	</select>
+	  <button type="submit" class="form-control"><?php echo $MSG_SEARCH?></button>
+	</form>

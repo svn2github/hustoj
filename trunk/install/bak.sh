c8d310528afe2fb6177a09eb6b787acfbb1d2e7d
张浩斌 <newsclan@gmail.com>
2022-12-21 10:20:47 +0800

    Update bak.sh

--- trunk/install/bak.sh
+++ trunk/install/bak.sh
@@ -22,7 +22,7 @@ echo "这里有警告是正常现象，请勿担心，下面的打包压缩耗�
 echo "The warning here is normal, don't worry, the following packaging and compression takes a long time, please wait patiently for the backup to end and return to the command line prompt."
 
 mysqldump -h $SERVER -P $PORT -R $DATABASE -u$USER -p$PASSWORD | bzip2 >/var/backups/db_${DATE}.sql.bz2
-if tar cjf /var/backups/hustoj_${DATE}.tar.bz2 /home/judge/data /home/judge/src /home/judge/etc /var/backups/db_${DATE}.sql.bz2 --exclude=/home/judge/src/install/*.bz2 ; then
+if tar cjf /var/backups/hustoj_${DATE}.tar.bz2 /home/judge/data /home/judge/src /home/judge/etc /var/backups/db_${DATE}.sql.bz2; then
 	rm /var/backups/hustoj_${OLD3}.tar.bz2  2> /dev/null
 	rm /var/backups/db_${OLD}.sql.bz2  2> /dev/null
 fi

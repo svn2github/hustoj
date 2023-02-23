7dba9bf3c88cf6e92652d695ee8a732af7f4f1a3
张浩斌 <newsclan@gmail.com>
2023-02-23 16:28:59 +0800

    Update problem_copy.php

--- trunk/web/admin/problem_copy.php
+++ trunk/web/admin/problem_copy.php
@@ -1,7 +1,7 @@
 <?php require_once ("admin-header.php");
 if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']))){
-	echo "<a href='../loginpage.php'>Please Login First!</a>";
-	exit(1);
+        echo "<a href='../loginpage.php'>Please Login First!</a>";
+        exit(1);
 }
 ?>
 <ol>
@@ -12,6 +12,15 @@ Copy from http://uoj.ac
   <input type=submit>
 </form>
 </li>
+<li>
+<!--
+Copy from http://ybt.ssoier.cn:8088/......
+<form method=POST action=problem_add_page_ybt.php>
+  <input name=url type=text size=100  class="input input-xxlarge" value="http://ybt.ssoier.cn:8088/problem_show.php?pid=1000">
+  <input type=submit>
+</form>
+</li>
+-->
 <li>
 Copy from http://hustoj......
 <form method=POST action=problem_add_page_hustoj.php>
@@ -43,7 +52,7 @@ Copy from http://acm.student.cs.uwaterloo.ca/~acm00
 <li>
 Copy from acm.pku.edu.cn
 <form method=POST action=problem_add_page_pku.php>
-  <input name=url type=text size=100>
+  <input name=url class='input input-xxlarge' type=text size=100 value='http://poj.org/problem?id=1000' >
   <input type=submit>
 </form>
 </li>

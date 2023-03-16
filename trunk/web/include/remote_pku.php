1dfed41b42e8e6a2d305146f91310dcac88111dd
张浩斌 <newsclan@gmail.com>
2023-03-16 21:38:39 +0800

    Update remote_pku.php

--- trunk/web/include/remote_pku.php
+++ trunk/web/include/remote_pku.php
@@ -165,7 +165,7 @@ if(isset($_POST[$OJ_NAME.'_refer'])){
 	header("location:".$_SESSION[$OJ_NAME.'_refer']);
 	unset($_SESSION[$OJ_NAME.'_refer']);
 }else{
-	if(time()-filemtime($remote_cookie.".sub")>$remote_delay){
+	if(time()-fileatime($remote_cookie.".sub")>$remote_delay){
 		do_submit($remote_site,$remote_user);
 		touch($remote_cookie.".sub");
 	}

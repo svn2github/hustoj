2d2f6e88f4151be1d994f61d26c0e55a43502bb6
张浩斌 <newsclan@gmail.com>
2023-03-18 11:24:15 +0800

    Update remote_pku.php

--- trunk/web/include/remote_pku.php
+++ trunk/web/include/remote_pku.php
@@ -139,7 +139,11 @@ function do_result_one($remote_site,$sid,$rid){
 	$sql="update solution set result=?,pass_rate=?,time=?,memory=?,judger=?,judgetime=now()  where solution_id=?";
 	pdo_query($sql,$result,$pass_rate,$time,$memory,get_domain($remote_site),$sid);
 	echo $sql,$result,$pass_rate,$time,$memory,get_domain($remote_site),$sid;
-
+        if($result==4){
+		$pid=pdo_query("select problem_id from solution where solution_id=?",$sid)[0][0];
+		$sql="update problem set accepted=(select count(1) from solution where result=4 and problem_id=?) where problem_id=?";
+		pdo_query($sql,$pid,$pid);
+	}
 	return $result;
 }
 function do_result($remote_site){

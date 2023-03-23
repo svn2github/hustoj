77e0d3ca0dff1b7f795f512d37b08e6bc8a78325
张浩斌 <newsclan@gmail.com>
2023-03-23 16:37:02 +0800

    Update login.php

--- trunk/web/login.php
+++ trunk/web/login.php
@@ -54,8 +54,8 @@ if(!$use_cookie){
 if($login){
   	$sql = "SELECT * FROM `privilege` WHERE `user_id`=?";
 	
-        session_id(session_create_id($login));
-        session_start();
+        //session_id(session_create_id($login));
+        //session_start();
  
 	
 	$_SESSION[ $OJ_NAME . '_' . 'user_id' ] = $login;

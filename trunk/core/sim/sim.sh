4198e311ec75b3f502fac7aca4dbc61a56718efd
张浩斌 <newsclan@gmail.com>
2023-03-27 06:57:30 +0800

    Update sim.sh

--- trunk/core/sim/sim.sh
+++ trunk/core/sim/sim.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 EXTENSION=`echo "$1" | cut -d'.' -f2`
-for i in ../data/$2/ac/*.$EXTENSION
-do 
-	if [ ! -e "/usr/bin/sim_$EXTENSION" ]
-	then
-		EXTENSION="text";
-	fi
-	sim=`/usr/bin/sim_$EXTENSION -p $1 $i |grep ^$1|awk '{print $4}'`
-	if [ ! -z $sim ] 
-	then 
-		sim_s_id=`basename $i`
-		echo "$sim $sim_s_id" >sim
-		exit $sim 
-	fi
+FIRST=""
+for i in `ls ../data/$2/ac/*.$EXTENSION`
+do
+        echo "i:$i"
+        if [ ! -e "/usr/bin/sim_$EXTENSION" ]
+        then
+                EXTENSION="text";
+        fi
+        sim=`/usr/bin/sim_$EXTENSION -p $1 $i |grep ^$1|awk '{print $4}'`
+        if [ ! -z "$sim" ] && [ "$sim" -gt 80 ]
+        then
+                sim_s_id=`basename $i`
+                echo "$sim $sim_s_id" >sim
+                exit $sim
+        fi
+        FIRST="false"
 done
+if [ -z "$FIRST" ] ;then
+    echo "first answer"
+    echo "0 0" > sim
+else
+        echo $FIRST
+fi
 exit 0;

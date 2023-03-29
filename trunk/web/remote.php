4630bff249bf5b98d6482e39abb5872e76489364
张浩斌 <newsclan@gmail.com>
2023-03-29 13:27:31 +0800

    Update remote.php

--- trunk/web/remote.php
+++ trunk/web/remote.php
@@ -2,12 +2,25 @@
      require_once "include/db_info.inc.php";
      require_once "include/init.php";
         $remote_ojs=array(
-                "pku","hdu","luogu"
+                "pku","hdu"
         );
+		$sites=array(
+			"http://www.poj.org/","http://acm.hdu.edu.cn/"
+		);
+$i=0;
 foreach($remote_ojs as $remote_oj){
-        $file="include/remote_$remote_oj.php";
-        if(file_exists($file)){
-                echo "<iframe src='$file?check' ></iframe>";
-        }
-
+	$file="include/remote_$remote_oj.php";
+    if(file_exists($file)){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $sites[$i]);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_exec($ch);  
+		$curl_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+		if ($curl_code == 200){
+			echo "<iframe src='$file?check' ></iframe>";
+		}
+    }
+	$i++;
 }

04412d05ea1440034762b1c0134cc8c01a76f02a
张浩斌 <newsclan@gmail.com>
2023-03-31 10:28:40 +0800

    Update header.php
    当 $OJ_BG为空的时候造成重复加载，浪费带宽并造成post_key失效。

--- trunk/web/template/syzoj/header.php
+++ trunk/web/template/syzoj/header.php
@@ -123,7 +123,10 @@
 ?>
    
 <body id="MainBg-C" style="position: relative; margin-top: 49px; height: calc(100% - 49px); overflow-y: overlay; 
-background : url(<?php echo $OJ_BG;?>) no-repeat; background-size: 100%">
+
+<?php if(isset($OJ_BG)&&$OJ_BG!="") echo "background:url($OJ_BG)" ?>
+                           
+no-repeat; background-size: 100%">
     <div id="page-header" class="ui fixed borderless menu" style="position: fixed; height: 49px; z-index:99999">
         <div id="menu" class="ui stackable mobile ui container computer" style="margin-left:calc(10%)!important">
             <a class="header item"  href="/"><span

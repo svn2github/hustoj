f6c6957544511ca07d378dfb020c61844a40e194
张浩斌 <newsclan@gmail.com>
2023-04-11 16:49:42 +0800

    Update curl.php

--- trunk/web/include/curl.php
+++ trunk/web/include/curl.php
@@ -26,6 +26,7 @@ function curl_get($url){
 	$curl = curl_init($url);
 	//curl_setopt($curl, CURLOPT_COOKIE, 'PHPSESSID=buiebpv91e0cdhpmm6a320j1l7; path=/');
         //curl_setopt($curl, CURLOPT_HEADER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:')); 
 	curl_setopt($curl, CURLOPT_COOKIEFILE, $remote_cookie); // use saved cookies
 	curl_setopt($curl, CURLOPT_COOKIEJAR, $remote_cookie);  // save coockies
 	curl_setopt($curl, CURLOPT_REFERER, "$url"); 
@@ -46,7 +47,7 @@ function curl_post_urlencoded($url,$form){
 	curl_setopt($curl, CURLOPT_COOKIEJAR, $remote_cookie);  // save coockies
 	curl_setopt($curl, CURLOPT_REFERER, "$url"); 
 	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"); 
-	curl_setopt($curl, CURLOPT_HTTPHEADER, ["DNT: 1","Upgrade-Insecure-Requests: 1","Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,da;q=0.6,es;q=0.5","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"]);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:')); 
 	curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);  // 不要打印内容
 	//curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);

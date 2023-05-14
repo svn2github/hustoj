c3b1a7d4cd4da2d7961dfa4e7bf534fbfa6de11b
张浩斌 <newsclan@gmail.com>
2023-05-14 10:47:48 +0800

    Update category.php

--- trunk/web/template/mdui/category.php
+++ trunk/web/template/mdui/category.php
@@ -10,29 +10,9 @@
     <?php include('_includes/sidebar.php'); ?>
     <div class="mdui-container">
         <h1>分类</h1>
-        <?php if(!$result) { ?>
-            <div style="font-size: 175%;">暂无分类</div>
-        <?php } else { ?>
-            <?php
-                $colors = [
-                    "red",
-                    "pink",
-                    "purple",
-                    "orange",
-                    "light-blue",
-                    "green",
-                    "teal"
-                    ];
-                foreach($result as $row) { 
-                    $hash_num = hexdec(substr(md5($row["source"]),0,7));
-                    $label_color = $colors[$hash_num%count($colors)];
-                    $label_color = $label_color ? $label_color : "theme";
-                    if($row["source"]) {
-                        echo '<a class="mdui-btn mdui-btn-dense mdui-color-'.$label_color.'-accent mdui-ripple mdui-m-a-2" href="problemset.php?search='.urlencode($row["source"]).'">'.$row["source"].'</a>';
-                    }
-                }
-            ?>
-        <?php } ?>
+         <div class="ui existing segment">
+        <?php echo $view_category?>
+        </div>
     </div>
 </body>
 

3a5966e4a8fd2ec16f7ad100e6b891f08d0f40a6
张浩斌 <newsclan@gmail.com>
2023-06-17 14:21:06 +0800

    Create add_dns_to_docker.sh

---
+++ trunk/install/add_dns_to_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DNS=`cat /etc/resolv.conf |grep nameserver|awk '{print $2}'`
+cd /etc/docker
+if grep 'dns' daemon.json > /dev/null ; then
+        echo "has dns"
+else
+
+        head -`wc -l daemon.json |awk '{print $1-1}'` daemon.json > new.json
+        echo ",    \"dns\": [\"$DNS\", \"8.8.8.8\", \"114.114.114.114\"] " >> new.json
+        echo '}' >> new.json
+
+        mv daemon.json old.json
+        mv new.json daemon.json
+
+fi

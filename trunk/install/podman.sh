2aa822be08e8b505b9e941f86238cb2fcb233ec2
张浩斌 <newsclan@gmail.com>
2023-06-19 19:53:00 +0800

    Update podman.sh

--- trunk/install/podman.sh
+++ trunk/install/podman.sh
@@ -16,3 +16,5 @@ sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
 sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/g" /home/judge/etc/judge.conf
 sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
 sed -i "s|OJ_DOCKER_PATH=/usr/bin/docker|OJ_DOCKER_PATH=/usr/bin/podman|g" /home/judge/etc/judge.conf
+pkill -9 judged
+/usr/bin/judged

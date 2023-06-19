67720921b97b6fd9d01a2d8d6264024039317147
张浩斌 <newsclan@gmail.com>
2023-06-19 16:08:06 +0800

    Update install-ubuntu22.04.sh

--- trunk/install/install-ubuntu22.04.sh
+++ trunk/install/install-ubuntu22.04.sh
@@ -188,10 +188,6 @@ else
         sed -i 's/ubuntu:20/ubuntu:22/g' Dockerfile
         sed -i 's|/usr/include/c++/9|/usr/include/c++/11|g' Dockerfile
         bash podman.sh
-        sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
-        sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/g" /home/judge/etc/judge.conf
-        sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
-        sed -i "s|OJ_DOCKER_PATH=/usr/bin/docker|OJ_DOCKER_PATH=/usr/bin/podman|g" /home/judge/etc/judge.conf
 fi
 clear
 reset

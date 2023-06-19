fa9f31585ca209a8264e3d8891e9b18d7dc74ea5
张浩斌 <newsclan@gmail.com>
2023-06-19 16:07:28 +0800

    Update podman.sh

--- trunk/install/podman.sh
+++ trunk/install/podman.sh
@@ -9,4 +9,8 @@ while ! podman build -t hustoj .
 do
 		echo "Network fail, retry... you might want to make sure podman image source is available"
 done
- 
+
+sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
+sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/g" /home/judge/etc/judge.conf
+sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
+sed -i "s|OJ_DOCKER_PATH=/usr/bin/docker|OJ_DOCKER_PATH=/usr/bin/podman|g" /home/judge/etc/judge.conf

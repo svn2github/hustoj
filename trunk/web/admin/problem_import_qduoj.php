09b2691552049ae12a72abc5b74d90cbbac24498
张浩斌 <newsclan@gmail.com>
2023-07-06 17:10:33 +0800

    Update problem_import_qduoj.php

--- trunk/web/admin/problem_import_qduoj.php
+++ trunk/web/admin/problem_import_qduoj.php
@@ -107,7 +107,7 @@ if ($_FILES ["fps"] ["error"] > 0) {
   echo "&nbsp;&nbsp;- Error: ".$_FILES ["fps"] ["error"]."File size is too big, change in PHP.ini<br />";
 }
 else {
-  $tempdir = sys_get_temp_dir()."/import_qduoj";	
+  $tempdir = sys_get_temp_dir()."/import_qduoj".time();	
   echo  $tempdir ;
   mkdir($tempdir);
   $tempfile = $_FILES ["fps"] ["tmp_name"];

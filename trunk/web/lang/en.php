9cea5d61e09fd464bb8589aab83237121d35307d
张浩斌 <newsclan@gmail.com>
2023-07-24 11:31:21 +0800

    Update en.php

--- trunk/web/lang/en.php
+++ trunk/web/lang/en.php
@@ -182,6 +182,8 @@
 	$MSG_CONTEST_CREATOR="CREATOR";
 	$MSG_CONTEST_PENALTY="TIME PENALTY";
 	$MSG_IP_VERIFICATION="IP VERIFICATION";
+        $MSG_LOG="Log";
+        $MSG_SUSPECT="Audit";
 	$MSG_CONTEST_SUSPECT1="IP addresses with multiple IDs. If multiple IDs are accessed at the same computer during the contest/exam, it logged.";
 	$MSG_CONTEST_SUSPECT2="IDs with multiple IP addresses. If switch to another computer during the contest/exam, it logged.";
         $MSG_REVIEW_CONTESTRANK="RankList of the Reviewers"; // rank of people who reviewed problems of contest after it's over

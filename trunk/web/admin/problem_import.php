e7726a77bc202eb254fe28527042d1adab4dd742
张浩斌 <newsclan@gmail.com>
2023-08-01 11:19:45 +0800

    Update problem_import.php

--- trunk/web/admin/problem_import.php
+++ trunk/web/admin/problem_import.php
@@ -43,11 +43,11 @@
       if (!writable($OJ_DATA)) {
         echo "- You need to add  $OJ_DATA into your open_basedir setting of php.ini,<br>
         or you need to execute:<br>
-        <b>chmod 775 -R $OJ_DATA && chgrp -R www-data $OJ_DATA</b><br>
+        <b>chmod 775 -R $OJ_DATA && chgrp -R ".get_current_user()." $OJ_DATA</b><br>
         you can't use import function at this time.<br>"; 
 
         if($OJ_LANG == "cn")
-          echo "权限异常，请先去执行sudo chmod 775 -R $OJ_DATA <br> 和 sudo chgrp -R www-data $OJ_DATA <br>";
+          echo "权限异常，请先去执行sudo chmod 775 -R $OJ_DATA <br> 和 sudo chgrp -R ".get_current_user()." $OJ_DATA <br>";
 
         $show_form = false;
       }

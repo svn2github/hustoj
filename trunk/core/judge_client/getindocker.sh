f6ce934f013dcd22b68b1824c05b71331a1d9de2
张浩斌 <newsclan@gmail.com>
2023-08-04 09:25:39 +0800

    Update getindocker.sh

--- trunk/core/judge_client/getindocker.sh
+++ trunk/core/judge_client/getindocker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "This script is only for test, all modification will be lost until you add them to the Dockerfile and run docker.sh / podman.sh "
 /usr/bin/docker container run --pids-limit 100 --rm --cap-add SYS_PTRACE --net=host -v /home/judge:/home/judge -it hustoj

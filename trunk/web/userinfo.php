633c46603a14c48923899c71525f3b95ae3a943b
Tiang-88 <51110833+Tiang-88@users.noreply.github.com>
2023-08-29 21:48:19 +0800

    Update userinfo.php
    Fatal error: Uncaught Error: Call to undefined function mysql_query_cache() in /home/judge/src/web/userinfo.php:114 Stack trace: #0 {main} thrown in /home/judge/src/web/userinfo.php on line 114

--- trunk/web/userinfo.php
+++ trunk/web/userinfo.php
@@ -6,6 +6,7 @@
 	require_once('./include/setlang.php');
 	require_once("./include/const.inc.php");
 	require_once("./include/my_func.inc.php");
+	require_once("include/memcache.php");
 	if(isset($OJ_OI_MODE)&&$OJ_OI_MODE&&!isset($_SESSION[$OJ_NAME."_administrator"])){
                 $now = strftime("%Y-%m-%d %H:%M",time());
                 $sql="select count(contest_id) from contest where start_time<'$now' and end_time>'$now' and title like '%$OJ_NOIP_KEYWORD%'";

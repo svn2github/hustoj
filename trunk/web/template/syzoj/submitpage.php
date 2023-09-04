dfa41dba92e848725cea40fba1272190d5f42ea8
Tiang-88 <51110833+Tiang-88@users.noreply.github.com>
2023-09-04 18:01:31 +0800

    Update submitpage.php

--- trunk/web/template/syzoj/submitpage.php
+++ trunk/web/template/syzoj/submitpage.php
@@ -85,7 +85,7 @@ echo"<option value=$i ".( $lastlang==$i?"selected":"").">
 
 			if (isset($OJ_TEST_RUN)&&$OJ_TEST_RUN) $height="400px";else $height="500px";
 	?>
-		        	?>
+		        	
 <div style="position: relative;">
     <div id="source" style="width:100%;" cols=180 rows=16></div>
 	<pre style="width:90%;height:<?php echo $height?>" cols=180 rows=16 id="source"><?php echo htmlentities($view_src,ENT_QUOTES,"UTF-8")?></pre>

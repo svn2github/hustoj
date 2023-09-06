e36ec5cdfd9a729e511701b45451cb7df76a63e7
张浩斌 <newsclan@gmail.com>
2023-09-06 19:46:12 +0800

    Add files via upload

--- trunk/web/register.php
+++ trunk/web/register.php
@@ -2,6 +2,7 @@
 require_once("./include/db_info.inc.php");
 if(isset($OJ_REGISTER)&&!$OJ_REGISTER) exit(0);
 require_once("./include/my_func.inc.php");
+require_once( './include/setlang.php' );
 require_once("./include/email.class.php");     // 新版本的邮件发送信息请填写到这个文件中
 if(isset($OJ_CSRF)&&$OJ_CSRF)require_once("./include/csrf_check.php");
 $err_str="";
@@ -86,11 +87,6 @@ $nick=(htmlentities ($nick,ENT_QUOTES,"UTF-8"));
 $school=(htmlentities ($school,ENT_QUOTES,"UTF-8"));
 $email=(htmlentities ($email,ENT_QUOTES,"UTF-8"));
 $ip = ($_SERVER['REMOTE_ADDR']);
-if(isset($OJ_EMAIL_CONFIRM) && $OJ_EMAIL_CONFIRM) 
-	$_SESSION[$OJ_NAME.'_'.'activecode']=getToken(16);
-else
-	$_SESSION[$OJ_NAME.'_'.'activecode']="";
-
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])&&!empty(trim($_SERVER['HTTP_X_FORWARDED_FOR']))) {
     $REMOTE_ADDR = $_SERVER['HTTP_X_FORWARDED_FOR'];
     $tmp_ip = explode(',', $REMOTE_ADDR);
@@ -100,6 +96,28 @@ if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])&&!empty(trim($_SERVER['HTTP_X_FORWAR
     $tmp_ip = explode(',', $REMOTE_ADDR);
     $ip = (htmlentities($tmp_ip[0], ENT_QUOTES, "UTF-8"));
 }
+
+// 检查IP是否已经注册过
+if (isset($OJ_REG_SPEED) && $OJ_REG_SPEED > 0 ) {
+
+    // 查询最近1小时内该IP地址已经注册的用户数量
+    $sql = "SELECT COUNT(*) FROM `users` WHERE (`ip` = ? or email = ? ) AND `reg_time` > DATE_SUB(NOW(), INTERVAL 1 HOUR)";
+    $result = pdo_query($sql, $ip,$email);
+    $count = intval($result[0][0]);
+    
+    if ($count > $OJ_REG_SPEED ) {
+        // 如果数量大于$OJ_REG_SPEED ，则表示该IP地址在最近1小时内已经注册过$OJ_REG_SPEED个账户
+        print "<script language='javascript'>\n";
+        print "alert('您的IP地址或Email已经注册过".$OJ_REG_SPEED."个账户，请稍后再试。\\n');\n";
+        print "history.go(-1);\n</script>";
+        exit(0);
+    }
+}
+if(isset($OJ_EMAIL_CONFIRM) && $OJ_EMAIL_CONFIRM) 
+	$_SESSION[$OJ_NAME.'_'.'activecode']=getToken(16);
+else
+	$_SESSION[$OJ_NAME.'_'.'activecode']="";
+
 if(isset($OJ_REG_NEED_CONFIRM)&&$OJ_REG_NEED_CONFIRM) $defunct="Y";
 else $defunct="N";
 $sql="INSERT INTO `users`("
@@ -107,11 +125,10 @@ $sql="INSERT INTO `users`("
 ."VALUES(?,?,?,NOW(),?,NOW(),?,?,?,?)";
 $rows=pdo_query($sql,$user_id,$email,$ip,$password,$nick,$school,$defunct,$_SESSION[$OJ_NAME.'_'.'activecode']);// or die("Insert Error!\n");
 //发送激活邮件
-if (isset($OJ_EMAIL_CONFIRM) && $OJ_EMAIL_CONFIRM) {
+if (isset($OJ_EMAIL_CONFIRM) && $OJ_EMAIL_CONFIRM ) {
+	$link= 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['REQUEST_URI'])."active.php?code=".$_SESSION[$OJ_NAME.'_'.'activecode'];
 	email($email,"$MSG_ACTIVE_YOUR_ACCOUNT",
-		"$MSG_CLICK_COPY $MSG_ACTIVE_YOUR_ACCOUNT :\n ".$_SESSION[$OJ_NAME.'_'.'activecode']
-	
-	);
+		"$MSG_CLICK_COPY $MSG_ACTIVE_YOUR_ACCOUNT $user_id :\n ".$link );
 }
 $sql="INSERT INTO `loginlog` VALUES(?,?,?,NOW())";
 pdo_query($sql,$user_id,"no save",$ip);

6960194226905919f4f51a8f6e85c64c2e4e9fc5
张浩斌 <newsclan@gmail.com>
2023-11-09 12:41:43 +0800

    Create faqs.news.php

---
+++ trunk/web/template/syzoj/faqs.news.php
@@ -0,0 +1,10 @@
+<?php $show_title="$MSG_FAQ - $OJ_NAME"; ?>
+<?php include("template/$OJ_TEMPLATE/header.php");?>
+<div class="padding">
+    <h1 class="ui center aligned header">帮助</h1>
+    <div style="font-content">
+    
+    </div>
+</div>
+
+<?php include("template/$OJ_TEMPLATE/footer.php");?>
